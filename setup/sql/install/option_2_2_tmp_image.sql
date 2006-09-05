bbab0eeecd0ba0d37a532b3bc5a517a8b3d420d7
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-09-05 06:49:17 +0000

    setup SQL files for MySQL 4.1
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@110 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/install/option_2_2_tmp_image.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE c_tmp_image (
-  c_tmp_image_id int(11) NOT NULL auto_increment,
-  filename text NOT NULL,
-  bin longblob NOT NULL,
-  r_datetime datetime NOT NULL default '0000-00-00 00:00:00',
-  type text,
-  PRIMARY KEY  (c_tmp_image_id),
-  KEY filename (filename(100))
-) TYPE=MyISAM;
