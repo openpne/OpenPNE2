7d006e98e2c5b08d82fed83167d47600387c1c1b
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-09-10 11:23:04 +0000

    DBインデックスを追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@135 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ setup/sql/update/update04_for2.3.3-add_index.sql
@@ -0,0 +1,5 @@
+ALTER TABLE c_commu_category_parent ADD INDEX sort_order (sort_order);
+ALTER TABLE c_diary_comment ADD INDEX c_member_id_r_datetime_c_diary_id (c_member_id, r_datetime, c_diary_id);
+ALTER TABLE c_profile_pref ADD INDEX sort_order (sort_order);
+ALTER TABLE c_rss_cache ADD INDEX r_datetime (r_datetime);
+ALTER TABLE c_rss_cache ADD INDEX r_datetime_c_member_id (r_datetime, c_member_id);
