fc11025de4f55a5cd146eab0a40f031cbbcb7730
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-09-26 10:19:10 +0000

    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@256 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/cap.php
+++ public_html/cap.php
@@ -2,7 +2,6 @@
 /**
  * @copyright 2005-2006 OpenPNE Project
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
- * @deprecated File deprecated in Release 1.9.0
  */
 
 require_once './config.inc.php';
