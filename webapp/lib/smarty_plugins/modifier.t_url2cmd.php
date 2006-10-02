a171832195af45bb588b4fdae46187550dc58e84
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-10-02 04:49:57 +0000

    URL判定の修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@339 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/smarty_plugins/modifier.t_url2cmd.php
+++ webapp/lib/smarty_plugins/modifier.t_url2cmd.php
@@ -11,7 +11,7 @@ function smarty_modifier_t_url2cmd($string)
     $replace = array('"', "'");
     $string = str_replace($search, $replace, $string);
 
-    $url_pattern = "/https?:\/\/([a-zA-Z0-9-.]+)\/[\w\-.,:;\~\^\/?\@&=+\$%#!()]+/";
+    $url_pattern = "/https?:\/\/([a-zA-Z0-9-.]+)\/?[\w\-.,:;\~\^\/?\@&=+\$%#!()]*/";
     return preg_replace_callback($url_pattern, '_smarty_modifier_t_cmd_make_url_js', $string);
 }
 
