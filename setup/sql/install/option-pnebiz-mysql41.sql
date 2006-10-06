1709c5a07309fd9ab9907fa4f05976c083022056
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-10-06 11:43:52 +0000

    PNEBIZのキャップに入れるHTMLをセットアップSQLに追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@420 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/install/option-pnebiz-mysql41.sql
+++ setup/sql/install/option-pnebiz-mysql41.sql
@@ -79,3 +79,5 @@ CREATE TABLE `biz_todo` (
   `r_datetime` datetime default NULL,
   PRIMARY KEY  (`biz_todo_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+REPLACE INTO `c_siteadmin` VALUES (NULL, 'inc_page_top', '<table width="100%" cellpadding="0" cellspacing="0" border="0" style="margin:0;">\n<tr>\n<td style="padding:3px 9px;" align="right" class="bg_13">\nグループウェア機能はこちら →\n　<a href="?m=biz&a=page_s_list">施設予約</a>\n　<a href="?m=biz&a=page_fh_biz_schedule_add">予定登録</a>\n　<a href="?m=biz&a=page_h_biz_group_find_all">グループ</a></td></tr>\n</table>', NOW());
