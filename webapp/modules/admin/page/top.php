e734e1fa3395b343bf8402889bf416c6f30eebd1
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-10-06 09:49:15 +0000

    OpenPNEInformationの管理画面へのフィード
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@409 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/top.php
+++ webapp/modules/admin/page/top.php
@@ -4,6 +4,8 @@
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 
+require_once 'OpenPNE/RSS.php';
+
 // 管理画面トップページ 認証済み
 class admin_page_top extends OpenPNE_Action
 {
@@ -13,6 +15,17 @@ class admin_page_top extends OpenPNE_Action
 
         $v['SNS_NAME'] = SNS_NAME;
         $v['OPENPNE_VERSION'] = OPENPNE_VERSION;
+
+        if (DISPLAY_OPENPNE_INFO){
+            $rss_url=OPENPNE_INFO_URL;
+            $rss = new OpenPNE_RSS();
+            if (!$items = $rss->fetch($rss_url)) {
+                $v['admin_info'] = array();
+                error_log("error");
+            } else {
+                $v['admin_info'] = $items;
+            }
+        }
 
         $this->set($v);
         return 'success';
--- webapp/modules/admin/templates/top.tpl
+++ webapp/modules/admin/templates/top.tpl
@@ -6,4 +6,19 @@
 <p>左側のメニューから設定したい項目を選択してください。</p>
 ({/if})
 
+<br>
+<br>
+<div class="info" style="margin: 70px 0 100px 0; text-align:  font-weight: bold; font-size: 1.2emx; color: #666; ">
+<dl class="section" id="information">
+<dt style="font-size:11px;">OpenPNE管理者向け情報</dt>
+<dd>
+<ul>
+({foreach from=$admin_info item=item})
+<li><a href="({$item.link})">({$item.title})</a></li>
+</ul>
+({/foreach})
+</dd>
+</dd>
+</div>
+
 ({$inc_footer|smarty:nodefaults})
