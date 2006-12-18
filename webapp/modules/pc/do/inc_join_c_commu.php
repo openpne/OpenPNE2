75453e28a250fe57df2720db7e3befeb4df52daf
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-12-18 15:07:24 +0000

    一括置換でのバグ修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@1281 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/inc_join_c_commu.php
+++ webapp/modules/pc/do/inc_join_c_commu.php
@@ -28,7 +28,7 @@ class pc_do_inc_join_c_commu extends OpenPNE_Action
         //承認必要なし
         case STATUS_C_JOIN_REQUEST_FREE:
             db_commu_join_c_commu($target_c_commu_id, $u);
-            db_commu_join_c_commu_send_mail($target_c_commu_id, $u);
+            do_inc_join_c_commu_send_mail($target_c_commu_id, $u);
             $p = array('target_c_commu_id' => $target_c_commu_id);
             openpne_redirect('pc', 'page_c_join_commu_2', $p);
             break;
