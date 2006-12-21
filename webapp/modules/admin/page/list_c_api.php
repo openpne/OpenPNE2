aeab08cddcde96cf105c838c18aa998aeac5fd77
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-12-21 06:48:29 +0000

    参照渡し修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@1315 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/list_c_api.php
+++ webapp/modules/admin/page/list_c_api.php
@@ -10,7 +10,7 @@ class admin_page_list_c_api extends OpenPNE_Action
     function execute($requests)
     {
         $pager = array();
-        $c_api_list = db_admin_get_c_api_all($requests['page'], $requests['page_size'], &$pager);
+        $c_api_list = db_admin_get_c_api_all($requests['page'], $requests['page_size'], $pager);
 
         $this->set("pager", $pager);
         $this->set("c_api_list", $c_api_list);
