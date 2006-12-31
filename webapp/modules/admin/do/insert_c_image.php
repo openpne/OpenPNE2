25b4e21ae37755a735e125fac5ffb27541c313c7
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2006-12-31 08:25:45 +0000

    管理画面の画像アップロード時のファイル名に制限を追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@1476 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/insert_c_image.php
+++ webapp/modules/admin/do/insert_c_image.php
@@ -7,9 +7,14 @@
 // スキン画像更新
 class admin_do_insert_c_image extends OpenPNE_Action
 {
+    function handleError($errors) 
+    { 
+        admin_client_redirect('edit_c_image', array_shift($errors)); 
+    } 
+
     function execute($requests)
     {
-        if (!empty($_FILES['upfile']) && !empty($requests['filename'])) {
+        if (!empty($_FILES['upfile']) && $_FILES['upfile']['error'] !== UPLOAD_ERR_NO_FILE && !empty($requests['filename'])) {
             image_data_delete($requests['filename']);
             admin_insert_c_image($_FILES['upfile'], $requests['filename']);
 
