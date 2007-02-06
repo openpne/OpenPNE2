747f78dafdb34b658b44b965ef789f7cafbf661c
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-02-06 11:28:19 +0000

    管理画面テンプレート修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2_6@1770 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/modules/admin/default.js
+++ public_html/modules/admin/default.js
@@ -1,74 +1,28 @@
-var menulist_num= 7;//ドロップダウンメニューの数
-var menulist= new Array(menulist_num);
-menulist['adminSiteMember']= {'count_mouseout':0,'display':false};
-menulist['adminImageKakikomi']= {'count_mouseout':0,'display':false};
-menulist['adminStatisticalInformation']= {'count_mouseout':0,'display':false};
-menulist['adminDesign']= {'count_mouseout':0,'display':false};
-menulist['adminSNSConfig']= {'count_mouseout':0,'display':false};
-menulist['adminInfoKiyaku']= {'count_mouseout':0,'display':false};
-menulist['adminAdminConfig']= {'count_mouseout':0,'display':false};
+var menu_info = new Array();
 
-var count= 0;//main関数の実行回数
-var count_limit= 1000000;//main関数の実行回数（100万でリセット）
+function menu(host_object_id, visible_object_id){
+	if(document.all){
+		var visible_object = document.all[visible_object_id];
+		var host_object = document.all[host_object_id];
+	}else if(document.getElementById){
+		var visible_object = document.getElementById(visible_object_id);
+		var host_object = document.getElementById(host_object_id);
+	}
 
-var delay= 1;//ドロップダウンメニューを非表示にするまでのディレイ
-
-function main() {
-
-	//メニューの制御
-	if (count > delay) {
-	
-	for (var key in menulist) {
-		var key_cont = key+"Cont";
-		if(document.getElementById){
-			var host_object = document.getElementById(key);
-			var visible_object = document.getElementById(key_cont);
-		}else if(document.all){
-			var host_object = document.all[key];
-			var visible_object = document.all[key_cont];
-		}
-
-		////メニューを表示する
-		if ((count - menulist[key]['count_mouseout'] < delay) || menulist[key]['display']) {
-			
-			if(document.getElementById){
-				visible_object.style.left = host_object.offsetLeft + "px";
-				visible_object.style.top = host_object.offsetTop + host_object.offsetHeight + "px";
-			}else if(document.all){
-				visible_object.style.left = host_object.offsetLeft;
-				visible_object.style.top = host_object.offsetTop + host_object.offsetHeight;	
-			}
+	if(visible_object.style.visibility == 'visible'){
+		visible_object.style.visibility="hidden";
+	}else{
+		if(document.all){
+			visible_object.style.left = host_object.offsetLeft;
+			visible_object.style.top = host_object.offsetTop + host_object.offsetHeight;	
 			visible_object.style.visibility="visible";
-			
-		////メニューを隠す
-		} else {
-			if (menulist[key]['count_mouseout'] > 0) {
-				if (visible_object.style.visibility == "visible"){
-					visible_object.style.visibility="hidden";
-				}
-			}
-		}
+		}else if(document.getElementById){
+			visible_object.style.left = host_object.offsetLeft + "px";
+			visible_object.style.top = host_object.offsetTop + host_object.offsetHeight + "px";
+			visible_object.style.visibility="visible";
+		}		
 	}
-	
-	}
-	
-	count ++;
-	
 }
-
-function menu(id) {
-	//onmouseover時の処理
-	menulist[id]['display']= true;
-}
-
-function menu_hide(id) {
-	//onmouseout時の処理
-	menulist[id]['count_mouseout']= count;
-	menulist[id]['display']= false;
-}
-
-main_timer_ID = setInterval('main()',100);//main関数の実行
-
 /*----------------------------------------------------
 メンバーリスト
 ----------------------------------------------------*/
@@ -100,3 +54,4 @@ function Link(linkLoc) {
         window.location.href=linkLoc;
     }
 }
+
