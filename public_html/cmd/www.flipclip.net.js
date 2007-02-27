d29beb7ad3ebe76c6ab73af666c5631d7d58d445
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-02-27 11:38:42 +0000

    flipclip小窓更新
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@1963 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/cmd/www.flipclip.net.js
+++ public_html/cmd/www.flipclip.net.js
@@ -1,23 +1,50 @@
 //MASTER:flipclip.net.js SLAVE:www.flipclip.net.js
 function url2cmd(url) {
-    if (!url.match(/^http:\/\/(?:www\.|)flipclip\.net\/clips\/[a-zA-Z0-9_-]+\/([a-zA-Z0-9_-]+)\/?$/)) {
+    if (!url.match(/^http:\/\/(?:www\.|)flipclip\.net\/clips\/([a-zA-Z0-9_-]+)\/([a-zA-Z0-9_-]+)\/?$/)) {
         return;
     }
-    var clipid = RegExp.$1;
-    main(clipid);
+    var userid = RegExp.$1
+    var clipid = RegExp.$2;
+    main(userid,clipid);
 }
 
-function main(clipid) {
-    if (!clipid.match(/^[a-zA-Z0-9_-]+$/)) {
+function main(userid, clipid) {
+    if (!clipid.match(/^[a-zA-Z0-9_-]+$/) || !userid.match(/^[a-zA-Z0-9_-]+$/)) {
         return;
     }
 
-    document.writeln('<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="229" height="163" align="middle">');
+    document.writeln('<object');
+    document.writeln('classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"');
+    document.writeln('codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0"');
+    document.writeln('width="400"');
+    document.writeln('height="300"');
+    document.writeln('id="PlayerCore"');
+    document.writeln('align="middle">');
     document.writeln('<param name="allowScriptAccess" value="sameDomain" />');
-    document.writeln('<param name="movie" value="http://www.flipclip.net/swf/player/15.swf?c='+clipid +'" />');
+    document.writeln('<param name="movie" value="http://www.flipclip.net/static/swf/frames/CompactXLarge.swf" />');
     document.writeln('<param name="quality" value="high" />');
+    document.writeln('<param name="scale" value="noscale" />');
+    document.writeln('<param name="salign" value="lt" />');
     document.writeln('<param name="wmode" value="transparent" />');
     document.writeln('<param name="menu" value="false" />');
-    document.writeln('<param name="flashvars" value="webServiceUrl=http://www.flipclip.net/amf/player&serviceName=FlipClip::AMF::Player&clipKey='+ clipid +'&uid=m3loves55&fid='+clipid+'&flv_url=http://www.flipclip.net/videos/m3loves55/'+clipid+'.flv" />');
-    document.writeln('<embed src="http://www.flipclip.net/swf/player/15.swf?c='+clipid+'" quality="high" bgcolor="#ffffff" wmode="transparent" width="229" height="163" align="middle"flashvars="webServiceUrl=http://www.flipclip.net/amf/player&serviceName=FlipClip::AMF::Player&clipKey=' +clipid+ '&uid=m3loves55&fid=' +clipid+ '&flv_url=http://www.flipclip.net/videos/m3loves55/' +clipid+ '.flv" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></object>');
+    document.writeln('<param name="bgcolor" value="#ffffff" />');
+    document.writeln('<param name="flashvars" value="coreURL=http://www.flipclip.net/static/swf/PlayerCore3/core.swf&seqURL=http://www.flipclip.net/clips/'+userid+'/'+clipid+'/playinfo/400x300&modPath=http://www.flipclip.net/static/swf/PlayerCore3/module/&autoPlay=false" />');
+    document.writeln('<embed');
+    document.writeln('src="http://www.flipclip.net/static/swf/frames/CompactXLarge.swf"');
+    document.writeln('quality="high"');
+    document.writeln('scale="noscale"');
+    document.writeln('salign="lt"');
+    document.writeln('wmode="transparent"');
+    document.writeln('menu="false"');
+    document.writeln('bgcolor="#ffffff"');
+    document.writeln('width="400"');
+    document.writeln('height="300"');
+    document.writeln('name="PlayerCore"');
+    document.writeln('align="middle"');
+    document.writeln('allowScriptAccess="sameDomain"');
+    document.writeln('type="application/x-shockwave-flash"');
+    document.writeln('pluginspage="http://www.macromedia.com/go/getflashplayer"');
+    document.writeln('flashvars="coreURL=http://www.flipclip.net/static/swf/PlayerCore3/core.swf&seqURL=http://www.flipclip.net/clips/'+userid+'/'+clipid+'/playinfo/400x300?modPath=http://www.flipclip.net/static/swf/PlayerCore3/module/&autoPlay=false" />');
+    document.writeln('</object>');
+
 }
