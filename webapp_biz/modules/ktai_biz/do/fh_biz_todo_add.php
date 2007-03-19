9623490e6adbeab4cbfe088a08179a52e138064d
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-03-19 10:35:11 +0000

    #521 携帯版でtodoの新規作成ができていなかったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@2186 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp_biz/modules/ktai_biz/do/fh_biz_todo_add.php
+++ webapp_biz/modules/ktai_biz/do/fh_biz_todo_add.php
@@ -7,6 +7,7 @@ class ktai_biz_do_fh_biz_todo_add extends OpenPNE_Action
         $tail = $GLOBALS['KTAI_URL_TAIL'];
 
         // --- リクエスト変数
+        $target_c_member_id = $requests['target_c_member_id'];
         $value = $requests['value'];
         $due_datetime_y = $requests['due_datetime_y'];
         $due_datetime_m = $requests['due_datetime_m'];
@@ -20,12 +21,14 @@ class ktai_biz_do_fh_biz_todo_add extends OpenPNE_Action
 
         if ($due_datetime_y && $due_datetime_m && $due_datetime_d) {
             $due_datetime = $due_datetime_y . '-' . $due_datetime_m . '-' . $due_datetime_d . ' 00:00:00';            
+        } else {
+            $due_datetime = '0000-00-00 00:00:00';
         }
 
         if (is_null($target_c_member_id)) {
             $target_c_member_id = $u;
         }
-        biz_insertTodo($target_c_member_id, $value, $u, 0, $is_all, $due_datetime);
+        $result = biz_insertTodo($target_c_member_id, $value, $u, 0, $is_all, $due_datetime);
 
         $p = array('target_c_member_id' => $target_c_member_id);
         ktai_biz_openpne_redirect('ktai_biz', 'page_fh_biz_todo_list', $p);
