a8221ce46af13151ebcc982e1745e68593fd4504
takanashi <takanashi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-04-05 06:52:46 +0000

    #524  PNEPOINTのアサイン箇所誤りの修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2_6@2436 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/h_prof.php
+++ webapp/modules/pc/page/h_prof.php
@@ -41,15 +41,15 @@ class pc_page_h_prof extends OpenPNE_Action
         // 誕生日まであと何日？
         $this->set('days_birthday', db_member_count_days_birthday4c_member_id($target_c_member_id));
 
-        // inc_entry_point
-        $this->set('inc_entry_point', fetch_inc_entry_point_f_home($this->getView()));
-
         //PNEPOINT
         $point = db_point_get_point($u);
         $this->set("point", $point);
 
         //rank
         $this->set("rank", db_point_get_rank4point($point));
+
+        // inc_entry_point
+        $this->set('inc_entry_point', fetch_inc_entry_point_f_home($this->getView()));
 
         return 'success';
     }
