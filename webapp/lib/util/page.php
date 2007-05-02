f5c372efc9af62ad81fc5456fd2c4e7888a4fe6a
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-02 08:43:02 +0000

    #684:ファンクションキャッシュ有効の場合にナビゲーションの変更反映が遅延していたのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.8.x@2685 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/util/page.php
+++ webapp/lib/util/page.php
@@ -22,7 +22,7 @@ function fetch_inc_navi($type, $target_id = null)
     if (!$is_recurred) {  //function cacheのために再帰処理を行う
         $is_recurred = true;
         $funcargs = func_get_args();
-        return pne_cache_recursive_call(OPENPNE_FUNCTION_CACHE_LIFETIME_LONG, __FUNCTION__, $funcargs);
+        return pne_cache_recursive_call(OPENPNE_FUNCTION_CACHE_LIFETIME_FAST, __FUNCTION__, $funcargs);
     }
 
     $is_recurred = false;
