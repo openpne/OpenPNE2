20e5f992bd04ca23be2678fbfb48954f830efbe4
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-10 04:52:11 +0000

    #684:fetch_inc_navi()のファンクションキャッシュをh_系のみに限定して行うようにした(r2748) #684:admin_do_delete_c_naviでファンクションキャッシュをドロップしていなかったのを修正(r2749) #684:fetch_inc_navi()のキャッシュ生存時間をFASTからLONGに戻した(r2752)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.6.x@2754 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/util/page.php
+++ webapp/lib/util/page.php
@@ -17,15 +17,18 @@
  */
 function fetch_inc_navi($type, $target_id = null)
 {
-    static $is_recurred = false;  //再帰処理中かどうかの判定フラグ
+    // $type が h の場合のみ function cache
+    if ($type == 'h') {
+        static $is_recurred = false;  //再帰処理中かどうかの判定フラグ
 
-    if (!$is_recurred) {  //function cacheのために再帰処理を行う
-        $is_recurred = true;
-        $funcargs = func_get_args();
-        return pne_cache_recursive_call(OPENPNE_FUNCTION_CACHE_LIFETIME_FAST, __FUNCTION__, $funcargs);
+        if (!$is_recurred) {  //function cacheのために再帰処理を行う
+            $is_recurred = true;
+            $funcargs = func_get_args();
+            return pne_cache_recursive_call(OPENPNE_FUNCTION_CACHE_LIFETIME_LONG, __FUNCTION__, $funcargs);
+        }
+
+        $is_recurred = false;
     }
-
-    $is_recurred = false;
 
     $inc_smarty = new OpenPNE_Smarty($GLOBALS['SMARTY']);
     $inc_smarty->templates_dir = 'pc/templates';
