0f7a1e796ed0ae424f53bac31cc5d1e767f49092
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-12 06:55:54 +0000

    #688:CSVダウンロード関連修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.8.x@2830 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/csv_member.php
+++ webapp/modules/admin/do/csv_member.php
@@ -26,7 +26,7 @@ class admin_do_csv_member extends OpenPNE_Action
 
         $member_key_string = $this->get_key_list();
         $c_member_list = $this->db_get_c_member_list($start_id,$end_id);
-        $member_csv_data = $this->create_csv_data($member_key_string,$c_member_list);
+        $member_csv_data = $this->create_csv_data($member_key_string, $c_member_list);
         
         header("Content-Type: application/octet-stream");
         header("Content-Disposition: attachment; filename=member.csv");
@@ -39,19 +39,19 @@ class admin_do_csv_member extends OpenPNE_Action
      */
     function db_get_c_member_list($start_id,$end_id)
     {
-        $params=array();
+        $params = array();
         $sql = 'SELECT c_member_id FROM c_member ';
         $sql .= ' WHERE 1 ' ;
-        if($start_id!=""){
+        if ($start_id > 1) {
             $sql .= ' AND c_member_id >= ? ';
             $params[] = $start_id;
         }
-        if($end_id!=""){
+        if ($end_id > 0) {
             $sql .= ' AND c_member_id <= ? ';
             $params[] = $end_id;
         }
-        $sql .= ' ORDER BY c_member_id' ;
-        $ids = db_get_col($sql,$params);
+        $sql .= ' ORDER BY c_member_id';
+        $ids = db_get_col($sql, $params);
     
         $c_member_list = array();
         foreach ($ids as $id) {
@@ -72,7 +72,7 @@ class admin_do_csv_member extends OpenPNE_Action
             
             $tmp_profile_list = db_member_c_member_profile_list4c_member_id($id, 'private');
             $c_profile_list = db_member_c_profile_list4null();
-            foreach($c_profile_list as $key=>$tmp_profile ){
+            foreach ($c_profile_list as $key => $tmp_profile) {
                 if (is_array($tmp_profile_list[$tmp_profile['name']]['value'])){
                     foreach ($tmp_profile_list[$tmp_profile['name']]['value'] as $itm){
                         $tmp_c_member[$tmp_profile['name']] .= $itm . " ";
@@ -88,11 +88,12 @@ class admin_do_csv_member extends OpenPNE_Action
             
             $c_member_list[]=$tmp_c_member;
         }
-    
+        
         return $c_member_list;
     }
     
-    function get_key_list(){
+    function get_key_list()
+    {
         $c_profile_list = db_member_c_profile_list4null();
         
         $ley_list[]="メンバーID";
@@ -106,7 +107,7 @@ class admin_do_csv_member extends OpenPNE_Action
         $ley_list[]="誕生年";
         $ley_list[]="誕生月";
         $ley_list[]="誕生日";
-        foreach($c_profile_list as $profile){
+        foreach ($c_profile_list as $profile) {
             $ley_list[]= $profile['caption'];
         }
         $ley_list[]="PCアドレス";
@@ -116,23 +117,26 @@ class admin_do_csv_member extends OpenPNE_Action
         return $ley_list;
     }
     
-    function create_csv_data($key_string,$value_list){
+    function create_csv_data($key_string, $value_list)
+    {
         $csv = "";
-        foreach($key_string as $each_key){
-            if($csv != "")$csv .= ",";
-            $csv .= '"'.mb_convert_encoding($each_key ,"SJIS", "auto").'"';
+        foreach ($key_string as $each_key) {
+            if ($csv != "") {
+                $csv .= ",";
+            }
+            $csv .= '"' . mb_convert_encoding($each_key, 'SJIS', 'auto') . '"';
         }
         $csv .= "\n";
         
-        foreach($value_list as $key => $value){
+        foreach ($value_list as $key => $value) {
             $temp = "";
-            foreach($value as $key2 => $value2){
-                $value2 = mb_convert_encoding($value2 ,"SJIS", "auto");
+            foreach ($value as $key2 => $value2) {
+                $value2 = mb_convert_encoding($value2, 'SJIS', 'auto');
                 if ($value2 != null) $value2 = str_replace('"', '""', $value2);//クォート
                 if ($value2 != null) $value2 = str_replace("\r","",$value2);//改行コードを変換
-                $temp .= "\"".$value2."\",";
+                $temp .= '"' . $value2 . '",';
             }
-            $csv .= $temp."\n";
+            $csv .= $temp . "\n";
         }
         return $csv;
     }
