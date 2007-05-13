cf6117d3a0df25f3542eb0bdf7777ea7367e258a
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-13 02:47:21 +0000

    db/rank.php を db/point.php に統合
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.8.x@2859 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/db/point.php
+++ webapp/lib/db/point.php
@@ -95,4 +95,19 @@ function db_point_get_point($c_member_id)
     return db_get_one($sql, $params);
 }
 
+//ポイントからランクを取得
+function db_point_get_rank4point($point)
+{
+    $sql = "SELECT * FROM c_rank WHERE point <= ? ORDER BY point DESC";
+    $params = array(intval($point));
+    return db_get_row($sql, $params);
+}
+
+//ランク情報を全部取得
+function db_point_get_rank_all()
+{
+    $sql = "SELECT * FROM c_rank ORDER BY point";
+    return db_get_all($sql);
+}
+
 ?>
