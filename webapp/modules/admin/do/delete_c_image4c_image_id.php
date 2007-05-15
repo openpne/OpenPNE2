bb5af921174e0b287ada45fceef663fe64fab7d8
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-15 18:16:05 +0000

    #764:「スキン画像変更」に用いた画像の削除方法が不統一
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.6.x@2914 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/delete_c_image4c_image_id.php
+++ webapp/modules/admin/do/delete_c_image4c_image_id.php
@@ -10,6 +10,11 @@ class admin_do_delete_c_image4c_image_id extends OpenPNE_Action
     function execute($requests)
     {
         $c_image = db_image_c_image4c_image_id($requests['target_c_image_id']);
+
+        if (strpos($c_image['filename'], 'skin_') === 0 || strpos($c_image['filename'], 'no_') === 0) {
+            admin_client_redirect('list_c_image', 'この画像は削除できません');
+        }
+
         image_data_delete($c_image['filename']);
         db_admin_delete_c_image_link4image_filename($c_image['filename']);
 
