d08ce2fff7efa1309300ca39f08c564f3a527ea0
maruyama <maruyama@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-15 09:05:51 +0000

    #691:外部Blog・アクセスブロック設定時にエラーメッセージが出ない
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.6.x@2903 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/do/h_config_access_block_update.php
+++ webapp/modules/ktai/do/h_config_access_block_update.php
@@ -16,6 +16,13 @@ class ktai_do_h_config_access_block_update extends OpenPNE_Action
         $tail = $GLOBALS['KTAI_URL_TAIL'];
         $u = $GLOBALS['KTAI_C_MEMBER_ID'];
 
+        $filtered_id = db_member_filter_c_access_block_id($u, $requests['c_member_id_block']);
+        foreach ($requests['c_member_id_block'] as $each_id) {
+            if (!in_array($each_id,$filtered_id)) {
+                $p = array('msg' => 41);
+                openpne_redirect('ktai', 'page_h_config_access_block', $p);
+            }
+        }
         db_member_insert_c_access_block($u, $requests['c_member_id_block']);
 
         $p = array('msg' => 36);
--- webapp/modules/pc/do/h_config_3.php
+++ webapp/modules/pc/do/h_config_3.php
@@ -34,8 +34,27 @@ class pc_do_h_config_3 extends OpenPNE_Action
         // ----------
 
         include_once 'OpenPNE/RSS.php';
-
-        if ($rss_url = OpenPNE_RSS::auto_discovery($rss)) {
+        
+        $rss_url = OpenPNE_RSS::auto_discovery($rss);
+        
+        $msg_list = array();
+        if ($rss && !$rss_url) $msg_list[] = "BlogのURLが有効な値ではありません";
+        $filtered_id = db_member_filter_c_access_block_id($u, $c_member_id_block);
+        foreach ($c_member_id_block as $each_id) {
+            if (!in_array($each_id,$filtered_id)) {
+            	$msg_list[] = "アクセスブロックIDが有効な値ではありません";
+                break;
+            }
+        }
+        
+        // error
+        if ($msg_list) {
+            $_REQUEST['msg'] = array_shift($msg_list);
+            openpne_forward('pc', 'page', "h_config");
+            exit;
+        }
+        
+        if ($rss_url) {
             $c_member = db_member_c_member4c_member_id($u);
             if ($rss_url != $c_member['rss']) {
                 //異なるBlogを登録すると過去のrssは全て削除する
