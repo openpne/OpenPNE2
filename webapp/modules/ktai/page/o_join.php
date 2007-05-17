3b5e06bf231a3e795270f99913a997d35fbef3bb
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-17 12:41:04 +0000

    #788:携帯アフィリエイトの設定画面がない
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.8.x@2960 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/o_join.php
+++ webapp/modules/ktai/page/o_join.php
@@ -13,6 +13,11 @@ class ktai_page_o_join extends OpenPNE_Action
 
     function execute($requests)
     {
+        // 携帯から新規登録できない場合はログインページへ
+        if (IS_CLOSED_SNS || !((OPENPNE_REGIST_FROM & OPENPNE_REGIST_FROM_KTAI) >> 1)) {
+            openpne_redirect('ktai', 'page_o_login');
+        }
+        
         // --- リクエスト変数
         $aff_id = $requests['aff_id'];
 
