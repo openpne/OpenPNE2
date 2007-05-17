bdd77eed35416e5fadf1bfdd595b682b923f3b3b
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-17 12:58:10 +0000

    #798:存在しない予定の表示を許可していた箇所があったバグを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.8.x@2963 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp_biz/modules/biz/lib/mysql_functions.php
+++ webapp_biz/modules/biz/lib/mysql_functions.php
@@ -148,6 +148,10 @@ function biz_isPermissionSchedule($u, $biz_schedule_id)
     $biz_group_id = $biz_schedule['biz_group_id'];
     $target_c_member_id = $biz_schedule['c_member_id'];
 
+    if (empty($biz_schedule)) {  // 存在しない予定には表示権限がない
+        return false;
+    }
+
     switch ($public_flag) {
     case 'private' :  // 予定参加者にのみ権限が与えられる予定
         if ($target_c_member_id == $u) {  // 自分が予定作成者
