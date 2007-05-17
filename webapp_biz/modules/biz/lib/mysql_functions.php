3c4cabbe4666670804d957062f936de3a60219d8
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-17 05:42:57 +0000

    #793:予定削除関数biz_deleteSchedule()の不要部分を削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.8.x@2941 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp_biz/modules/biz/lib/mysql_functions.php
+++ webapp_biz/modules/biz/lib/mysql_functions.php
@@ -883,38 +883,18 @@ function biz_insertSchedule($title, $c_member_id, $begin_date, $finish_date, $be
 }
 
 //スケジュール削除
-function biz_deleteSchedule($id, $group = false)
+function biz_deleteSchedule($id)
 {
-    if ($group) {
-        $sql = 'SELECT first_id FROM biz_schedule WHERE biz_schedule_id = ?';
-        $params = array(intval($id));
-        $firstid = db_get_one($sql, $params);
-
-        $sql = 'SELECT biz_schedule_id FROM biz_schedule WHERE first_id = ?';
-        $params = array(intval($firstid));
-        $schedules = db_get_col($sql, $params);
-
-        $sql = 'DELETE FROM biz_schedule WHERE first_id = ?';
-        $params = array(intval($firstid));
-        db_query($sql, $params);
-
-        foreach ($schedules as $nowid) {
-            $sql = 'DELETE FROM biz_schedule_member WHERE biz_schedule_id = ?';
-            $params = array(intval($nowid));
-            db_query($sql, $params);
-        }
-    } else {
-        $sql = 'DELETE FROM biz_schedule WHERE biz_schedule_id = ?';
-        $params = array(
-            intval($id),
-        );
-        db_query($sql, $params);
-        $sql = 'DELETE FROM biz_schedule_member WHERE biz_schedule_id = ?';
-        $params = array(
-            intval($id),
-        );
-        db_query($sql, $params);
-    }
+    $sql = 'DELETE FROM biz_schedule WHERE biz_schedule_id = ?';
+    $params = array(
+        intval($id),
+    );
+    db_query($sql, $params);
+    $sql = 'DELETE FROM biz_schedule_member WHERE biz_schedule_id = ?';
+    $params = array(
+        intval($id),
+    );
+    db_query($sql, $params);
 }
 
 //スケジュール編集
