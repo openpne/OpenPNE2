3d4ce9466c10c150e0c221ac95338337230ac109
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-24 03:17:33 +0000

    #854:携帯版で、コミュニティ管理者交代・副管理者の要請ができるメンバーがいない状態でメッセージを送信することができていたのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.6.x@3112 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/c_edit_member.php
+++ webapp/modules/ktai/page/c_edit_member.php
@@ -36,6 +36,28 @@ class ktai_page_c_edit_member extends OpenPNE_Action
         $this->set("is_prev", $list[1]);
         $this->set("is_next", $list[2]);
 
+        // 管理者交代メッセージ送信先メンバーリスト
+        $admin_list = array();
+        foreach ($list[0] as $c_member) {
+            if ($c_member['c_member_id'] != $u
+                && $c_member['c_commu_admin_confirm_id'] <= 0
+                && $c_member['c_commu_sub_admin_confirm_id'] <= 0) {
+                $admin_list[] = $c_member;
+            }
+        }
+        $this->set('admin_list', $admin_list);
+
+        // 副管理者要請メッセージ送信先メンバーリスト
+        $subadmin_list = array();
+        foreach ($list[0] as $c_member) {
+            if ($c_member['c_member_id'] != $u && $c_member['c_member_id'] != $c_commu['c_member_id_sub_admin']
+                && $c_member['c_commu_admin_confirm_id'] <= 0
+                && $c_member['c_commu_sub_admin_confirm_id'] <= 0) {
+                $subadmin_list[] = $c_member;
+            }
+        }
+        $this->set('subadmin_list', $subadmin_list);
+
         //コミュニティのメンバ数
         $this->set("count_member", db_commu_count_c_commu_member_list4c_commu_id($target_c_commu_id));
 
