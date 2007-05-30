540117ca75052e62947fc095298e4c50858554d2
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-05-30 05:30:15 +0000

    r3155を元に戻した
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/2.6.x@3160 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2005-2007 OpenPNE Project
+ * @copyright 2005-2006 OpenPNE Project
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 
@@ -9,7 +9,4 @@ require_once OPENPNE_WEBAPP_DIR . '/init.inc';
 
 openpne_execute();
 
-// test kim
-print("test");
-// test
 ?>
