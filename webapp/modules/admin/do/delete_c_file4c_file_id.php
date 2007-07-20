4c04f3114b03c7eb875ff85f158e6fea20ab583c
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-07-20 03:29:32 +0000

    2.8.2修正をマージ
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3458 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/delete_c_file4c_file_id.php
+++ webapp/modules/admin/do/delete_c_file4c_file_id.php
@@ -14,6 +14,10 @@ class admin_do_delete_c_file4c_file_id extends OpenPNE_Action
         }
 
         $c_file = db_file_c_file4c_file_id($requests['target_c_file_id']);
+        if (!$c_file) {
+            admin_client_redirect('top', '指定されたファイルは存在しません');
+        }
+
         db_file_delete_c_file($c_file['filename']);
         db_admin_delete_c_file_link4filename($c_file['filename']);
 
