56fe1403b6b6e179b8ab87160747a0fcf9788c7d
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-07-31 14:45:04 +0000

    #989:admin：監視機能で各データを削除する際に存在しないIDを指定しても削除が行われてしまう
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3594 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/delete_kakikomi_c_commu_topic_comment.php
+++ webapp/modules/admin/do/delete_kakikomi_c_commu_topic_comment.php
@@ -9,9 +9,15 @@ class admin_do_delete_kakikomi_c_commu_topic_comment extends OpenPNE_Action
 {
     function execute($requests)
     {
+        $topic_comment = db_commu_c_commu_topic_comment4c_commu_topic_comment_id($requests['target_c_commu_topic_comment_id']);
+        
+        if (!$topic_comment) {
+            admin_client_redirect('topic_comment_list', '指定されたトピック・イベントのコメントは存在しません');
+        }
+        
         db_commu_delete_c_commu_topic_comment($requests['target_c_commu_topic_comment_id']);
 
-        admin_client_redirect('topic_comment_list', 'トピックコメントを削除しました');
+        admin_client_redirect('topic_comment_list', 'トピック・イベントのコメントを削除しました');
     }
 }
 
