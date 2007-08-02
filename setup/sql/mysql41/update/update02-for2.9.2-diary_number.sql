b5b1c389b80c28e6b2bd440624ab5ef6b4a8d5b5
maruyama <maruyama@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-08-02 02:22:59 +0000

    #1011:【日記】日記コメントにIDを入れて、レス時に指定しやすくする
    のマージ
    
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3608 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ setup/sql/mysql41/update/update02-for2.9.2-diary_number.sql
@@ -0,0 +1,25 @@
+ALTER TABLE `c_diary_comment` ADD COLUMN `number` int(11) NOT NULL default '0';
+
+CREATE TABLE `tmp_c_diary_comment` (
+  `c_diary_comment_id` int(11) NOT NULL auto_increment,
+  `number` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`c_diary_comment_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+INSERT INTO tmp_c_diary_comment
+(c_diary_comment_id,
+number)
+(SELECT
+dc1.c_diary_comment_id,
+count(*) AS number
+ FROM c_diary_comment AS dc1,c_diary_comment AS dc2
+WHERE dc1.c_diary_id =dc2.c_diary_id
+ AND dc1.c_diary_comment_id >= dc2.c_diary_comment_id
+GROUP BY dc1.c_diary_comment_id
+);
+
+UPDATE c_diary_comment AS dc1,tmp_c_diary_comment AS dc2
+ SET dc1.number=dc2.number
+ WHERE dc1.c_diary_comment_id = dc2.c_diary_comment_id;
+
+DROP TABLE tmp_c_diary_comment;
