479630f3e32a256f170e1b62f3dd31d5fa4abefe
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-09-04 03:01:27 +0000

    #1079:不要な<a name="..">を削除、レイアウト調節 コミュニティからのメッセージ受信設定表示の際、PC版の場合は携帯版登録状況を、携帯版の場合はPC版登録状況を見るように修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3819 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/c_home.php
+++ webapp/modules/ktai/page/c_home.php
@@ -48,12 +48,21 @@ class ktai_page_c_home extends OpenPNE_Action
         $this->set("relation_c_member_and_c_commu",
             k_p_c_home_relationship_between_member_commu($target_c_commu_id, $u));
 
+        $c_member_secure = db_member_c_member_secure4c_member_id($u);
+        if (empty($c_member_secure['ktai_address'])) {
+            $is_registered_ktai_address = false;
+        } else {
+            $is_registered_ktai_address = true;
+        }
+
         //コミュニティメール(ktai)受信設定
         $this->set("is_receive_mail", db_commu_is_receive_mail_ktai($target_c_commu_id, $u));
         //コミュニティメール(pc)受信設定
         $this->set("is_receive_mail_pc", db_commu_is_receive_mail_pc($target_c_commu_id, $u));
         //管理者からのメッセージ受信設定
         $this->set("is_receive_message", db_commu_is_receive_message($target_c_commu_id, $u));
+        // 携帯アドレス登録状況
+        $this->set('is_registered_ktai_address', $is_registered_ktai_address);
 
         $this->set('is_unused_pc_bbs', util_is_unused_mail('m_pc_bbs_info'));
         $this->set('is_unused_ktai_bbs', util_is_unused_mail('m_ktai_bbs_info'));
