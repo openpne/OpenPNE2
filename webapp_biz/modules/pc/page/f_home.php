01652afbd4e31d9d962b2a413f5412fbd2720d85
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-09-05 06:14:57 +0000

    #1107:BIZ有効時でも一言機能が使用できるように修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3847 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp_biz/modules/pc/page/f_home.php
+++ webapp_biz/modules/pc/page/f_home.php
@@ -58,6 +58,8 @@ class pc_page_f_home extends OpenPNE_Action
             $target_c_member['age'] = getAge($target_c_member['birth_year'], $target_c_member['birth_month'], $target_c_member['birth_day']);
         }
         $this->set('target_c_member', $target_c_member);
+        $target_c_member_all = db_member_c_member_with_profile($target_c_member_id, 'private');
+        $this->set('target_c_member_all', $target_c_member_all);
 
         $this->set('c_rss_cache_list', db_rss_c_rss_cache_list4c_member_id($target_c_member_id, 5));
 
