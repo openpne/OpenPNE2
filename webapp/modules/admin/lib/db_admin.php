27cad79854623fb9c3a4f5b78ff840f105da77ad
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-09-05 14:13:19 +0000

    #1113:小窓の管理画面をブラックリスト方式に変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3861 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/lib/db_admin.php
+++ webapp/modules/admin/lib/db_admin.php
@@ -2496,6 +2496,18 @@ function db_admin_get_c_cmd_one($c_cmd_id)
     return db_get_row($sql, $params);
 }
 
+/***
+ * 指定した名称のCMD（小窓）の設定を取得する
+ * 
+ * @param names array 設定を取得したい小窓名（複数指定）
+ * @return array 小窓の設定リスト
+ */
+function db_admin_get_c_cmd_list4name($names)
+{
+    $sql = 'SELECT * FROM c_cmd WHERE name IN ("' . implode('","', $names) . '")';
+    return db_get_all($sql);
+}
+
 /**
  * 祝日のリストを取得
  */
