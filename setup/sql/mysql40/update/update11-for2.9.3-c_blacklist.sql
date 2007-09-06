425b34fc82166557bf7ac91d59cbb5b6cbb48898
maruyama <maruyama@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-09-06 02:44:24 +0000

    SQLのナンバリングを修正 MySQL4.0と4.1で異なっていた点を修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3870 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/mysql40/update/update11-for2.9.3-c_blacklist.sql
+++ setup/sql/mysql40/update/update11-for2.9.3-c_blacklist.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `c_blacklist` (
   `c_blacklist_id` int(11) NOT NULL auto_increment,
-  `c_member_id` int(11) NOT NULL,
+  `easy_access_id` blob NOT NULL,
+  `info` text NOT NULL,
   PRIMARY KEY  (`c_blacklist_id`)
 ) TYPE=MyISAM;
