c6549ac94cdfdbd6e999ff7f3514f6cc727d3d5a
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-09-08 10:18:41 +0000

    デフォルトスキンの説明用HTMLをPHPに変更（Content-Typeヘッダで確実に文字エンコーディングを指定するため）
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3915 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/skin/default/skin.php
+++ public_html/skin/default/skin.php
@@ -1,3 +1,4 @@
+<?php header('Content-Type: text/html; charset=UTF-8'); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -6,6 +7,6 @@
 </head>
 <body>
 <h1>OpenPNEデフォルトスキン</h1>
-<div><img src="img/skin_login.jpg" /></div>
+<div><img src="img/skin_login.jpg" alt="ログイン画面" /></div>
 </body>
 </html>
