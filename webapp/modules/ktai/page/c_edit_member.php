1921da48b7dc92f47f614a330020534c0e30c04e
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-09-13 05:45:06 +0000

    #1198:携帯版の管理者交代要請、副管理者要請の際、メッセージ入力ができなかったのを修正 管理者交代要請、副管理者要請時の文言をPCと携帯で統一
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@3994 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/c_edit_member.php
+++ webapp/modules/ktai/page/c_edit_member.php
@@ -33,36 +33,38 @@ class ktai_page_c_edit_member extends OpenPNE_Action
         //メンバーリスト
         $list = db_commu_c_member_list4c_commu_id($target_c_commu_id, $page_size, $page);
         $total_num = $list[3];
-        $this->set("c_member_list", $list[0]);
+        $c_member_list = $list[0];
+
+        // 管理者交代メッセージ送信先メンバーリスト
+        foreach ($c_member_list as $key => $c_member) {
+            if ($c_member['c_member_id'] != $u
+                && $c_member['c_commu_admin_confirm_id'] <= 0
+                && $c_member['c_commu_sub_admin_confirm_id'] <= 0) {
+                $c_member_list[$key]['is_display_admin'] = true;
+            } else {
+                $c_member_list[$key]['is_display_admin'] = false;
+            }
+        }
+
+        // 副管理者要請メッセージ送信先メンバーリスト
+        foreach ($c_member_list as $key => $c_member) {
+            if ($c_member['c_member_id'] != $u && $c_member['c_member_id'] != $c_commu['c_member_id_sub_admin']
+                && $c_member['c_commu_admin_confirm_id'] <= 0
+                && $c_member['c_commu_sub_admin_confirm_id'] <= 0) {
+                $c_member_list[$key]['is_display_sub_admin'] = true;
+            } else {
+                $c_member_list[$key]['is_display_sub_admin'] = false;
+            }
+        }
+
+        $this->set("c_member_list", $c_member_list);
         $this->set("is_prev", $list[1]);
         $this->set("is_next", $list[2]);
 
         $pager = array();
         $pager['start'] = ($page_size * ($page - 1)) + 1;
-        $pager['end'] = $pager['start'] + count($list[0]) - 1;
+        $pager['end'] = $pager['start'] + count($c_member_list) - 1;
         $this->set('pager', $pager);
-
-        // 管理者交代メッセージ送信先メンバーリスト
-        $admin_list = array();
-        foreach ($list[0] as $c_member) {
-            if ($c_member['c_member_id'] != $u
-                && $c_member['c_commu_admin_confirm_id'] <= 0
-                && $c_member['c_commu_sub_admin_confirm_id'] <= 0) {
-                $admin_list[] = $c_member;
-            }
-        }
-        $this->set('admin_list', $admin_list);
-
-        // 副管理者要請メッセージ送信先メンバーリスト
-        $subadmin_list = array();
-        foreach ($list[0] as $c_member) {
-            if ($c_member['c_member_id'] != $u && $c_member['c_member_id'] != $c_commu['c_member_id_sub_admin']
-                && $c_member['c_commu_admin_confirm_id'] <= 0
-                && $c_member['c_commu_sub_admin_confirm_id'] <= 0) {
-                $subadmin_list[] = $c_member;
-            }
-        }
-        $this->set('subadmin_list', $subadmin_list);
 
         //コミュニティのメンバー数
         $this->set("count_member", db_commu_count_c_commu_member_list4c_commu_id($target_c_commu_id));
