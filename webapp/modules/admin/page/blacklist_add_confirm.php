32a072f93b2f6488884aa19b0fba5f7152c79812
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-09-15 14:41:21 +0000

    ハードタブを4スペースに置換 他気がついたところだけコーディング規約に合わせて修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4035 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/blacklist_add_confirm.php
+++ webapp/modules/admin/page/blacklist_add_confirm.php
@@ -9,7 +9,7 @@ class admin_page_blacklist_add_confirm extends OpenPNE_Action
     function execute($requests)
     {
         if (db_member_easy_access_id_is_blacklist($requests['easy_access_id'])) {
-        	admin_client_redirect('blacklist_add', 'その個体識別番号は既に登録されています');
+            admin_client_redirect('blacklist_add', 'その個体識別番号は既に登録されています');
         }
         $c_member_id = db_member_c_member_id4easy_access_id($requests['easy_access_id'], false);
         $c_member = db_member_c_member4c_member_id($c_member_id, true);
--- webapp/modules/pc/page/f_message_send.php
+++ webapp/modules/pc/page/f_message_send.php
@@ -22,7 +22,7 @@ class pc_page_f_message_send extends OpenPNE_Action
 
         // 権限チェック
         if ($target_c_member_id == $u) {  // 自分にメッセージは送れない
-        	handle_kengen_error();
+            handle_kengen_error();
         }
 
         if ($form_val['target_c_message_id']) {
