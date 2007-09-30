9a00288aba5376b849c28fa7145d8aceb9b7f5ec
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-09-30 14:59:34 +0000

    #1129:携帯個体識別番号必須制の携帯版新規登録時、リクエスト変数 pre_id を使用しないようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4236 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/o_regist_ktai.php
+++ webapp/modules/ktai/page/o_regist_ktai.php
@@ -15,30 +15,14 @@ class ktai_page_o_regist_ktai extends OpenPNE_Action
     {
         // --- リクエスト変数
         $ses = $requests['ses'];
-        $c_member_id = intval($requests['id']);
-        $c_member_pre_id = intval($requests['pre_id']);
         // ----------
 
         $pre = array();
 
-        // セッションが有効かどうか
-        if ((!$pre = db_member_c_ktai_address_pre4session($ses)) && !empty($c_member_id)) {  // 既存メンバー
-            // 無効の場合、login へリダイレクト
-            $c_member_secure = db_member_c_member_secure4c_member_id($c_member_id);
-            $p = array('kad' => t_encrypt($c_member_secure['ktai_address']));
-            openpne_redirect('ktai', 'page_o_login', $p);
-        } elseif (!empty($c_member_id)) {
-        } elseif ((!$pre = db_member_c_member_pre4session($ses)) && !empty($c_member_pre_id)) {  // 新規登録
+        if (!$pre = db_member_c_member_pre4session($ses)) {
             openpne_redirect('ktai', 'page_o_login');
         }
 
-        if (!empty($c_member_id) && ($c_member_id == $pre['c_member_id'])) {
-            $this->set('c_member_id', $c_member_id);
-        } elseif (!empty($c_member_pre_id) && ($c_member_pre_id == $pre['c_member_pre_id'])) {
-            $this->set('c_member_pre_id', $c_member_pre_id);
-        } else {
-            openpne_redirect('ktai', 'page_o_login');
-        }
         $this->set('ses', $ses);
         $this->set('pre', $pre);
 
