f13942fc796e963f88f6ac8f6076c4f3fa53dcf7
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-10-02 11:07:51 +0000

    #1129:アフィリエイト用タグを pc_page_o_regist_ktai_address_end に挿入するようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4266 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/o_regist_ktai_address_end.php
+++ webapp/modules/pc/page/o_regist_ktai_address_end.php
@@ -22,6 +22,11 @@ class pc_page_o_regist_ktai_address_end extends OpenPNE_Action
         //---- inc_ テンプレート用 変数 ----//
         $this->set('inc_page_header', fetch_inc_page_header('regist'));
 
+        //アフィリエイトタグ用変数
+        $aftag = str_replace(array('({$ID})', '({$DATETIME})'),
+            array($requests['c_member_pre_id'], date("YmdHis")), AFFILIATE_TAG);
+        $this->set('aftag', $aftag);
+
         $this->set('login_url', get_login_url());
         return 'success';
     }
