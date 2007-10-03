387440ce494a81e9263a4b6f44b2acc835658c40
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-10-03 18:50:18 +0000

    #1370:setup/script 以下にupdate, upgradeディレクトリ追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4299 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/script/update/config.inc.php
+++ setup/script/update/config.inc.php
@@ -4,7 +4,7 @@
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 
-define('OPENPNE_DIR', realpath('../../'));
+define('OPENPNE_DIR', realpath('../../../'));
 require_once OPENPNE_DIR . '/config.php';
 
 ?>
