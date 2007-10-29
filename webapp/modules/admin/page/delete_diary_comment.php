31e4935f7981f6f59f04e1e047ac661f1a4fa768
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-10-29 16:24:33 +0000

    インデント、空白修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@4529 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/delete_diary_comment.php
+++ webapp/modules/admin/page/delete_diary_comment.php
@@ -23,7 +23,7 @@ class admin_page_delete_diary_comment extends OpenPNE_Action
         
         $diary = db_diary_get_c_diary4id($diary_comment['c_diary_id']);
         $diary_comment['subject'] = $diary['subject'];
-        $diary_comment['count_comments'] = db_diary_count_c_diary_comment4c_diary_id($diary_comment['c_diary_id']); 
+        $diary_comment['count_comments'] = db_diary_count_c_diary_comment4c_diary_id($diary_comment['c_diary_id']);
         $member = db_member_c_member4c_member_id($diary_comment['c_member_id']);
         $diary_comment['c_member'] = $member;
         $this->set('diary_comment', $diary_comment);
