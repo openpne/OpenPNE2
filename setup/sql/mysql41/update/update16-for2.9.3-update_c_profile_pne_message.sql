04709fbb1e5ac690161fa9a9753353e769e05f4f
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-11-02 04:18:34 +0000

    2.9.xのupdate SQLを削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4540 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/mysql41/update/update16-for2.9.3-update_c_profile_pne_message.sql
+++
@@ -1,4 +0,0 @@
-/*!40101 SET NAMES utf8 */;
-
-UPDATE `c_profile` SET `caption` = 'My News' WHERE `name` = 'PNE_MY_NEWS';
-UPDATE `c_profile` SET `caption` = 'My News（更新記録）' WHERE `name` = 'PNE_MY_NEWS_DATETIME';
