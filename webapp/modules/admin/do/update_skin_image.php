5c46f1017a151451a00218d92dc382ffc75b1f2d
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-12-13 08:38:51 +0000

    #1597:携帯版ロゴ画像がアップロードされた場合は、ロゴの非表示設定を解除するようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4686 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/update_skin_image.php
+++ webapp/modules/admin/do/update_skin_image.php
@@ -18,6 +18,16 @@ class admin_do_update_skin_image extends OpenPNE_Action
             if ($old_fn = db_get_c_skin_filename4skinname($requests['skinname'])) {
                 image_data_delete($old_fn);
             }
+
+            // 携帯版ロゴ画像がアップロードされた場合は、ロゴの非表示設定を解除する
+            if ($requests['skinname'] == 'skin_ktai_header') {
+                if (is_null(db_admin_c_admin_config4name('OPENPNE_USE_KTAI_LOGO'))) {
+                    db_admin_insert_c_admin_config('OPENPNE_USE_KTAI_LOGO', true);
+                } else {
+                    db_admin_update_c_admin_config('OPENPNE_USE_KTAI_LOGO', true);
+                }
+            }
+
             db_replace_c_skin_filename($requests['skinname'], $fn);
             pne_cache_drop('fetch_inc_navi', 'h');
             admin_client_redirect('edit_skin_image', 'スキン画像を変更しました');
