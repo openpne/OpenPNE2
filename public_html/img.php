edfac3a0b0aab329c6df492fc4327549d56b21d2
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-12-18 10:55:52 +0000

    #1639:img.phpにリダイレクトするためのpc_page_h_toimgアクションを追加し、認証時に$_REQUEST['target_script']を使用しないようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4738 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/img.php
+++ public_html/img.php
@@ -12,6 +12,8 @@ if (defined('CHECK_IMG_AUTH') && CHECK_IMG_AUTH) {
     require_once OPENPNE_WEBAPP_DIR . '/init.inc';
     
     $module = $_GET['m'];
+    $_GET['a'] = 'page_h_toimg';
+
     // init
     if ($init = openpne_ext_search("{$module}/init.inc")) {
         require_once $init;
