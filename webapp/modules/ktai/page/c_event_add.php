c4b9b88c9c19531db9a2159fa856e2fa01af373d
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-12-20 11:12:35 +0000

    #1663:トピック作成権限によるトピック作成リンクの表示・トピック作成アクションの挙動を統一したのを取り消し（r4803）
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@4810 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/c_event_add.php
+++ webapp/modules/ktai/page/c_event_add.php
@@ -18,13 +18,17 @@ class ktai_page_c_event_add extends OpenPNE_Action
         //--- 権限チェック
         //コミュニティメンバー
         if (!db_commu_is_c_commu_member($c_commu_id, $u)) {
-            handle_kengen_error();
+            $_REQUEST['target_c_commu_id'] = $c_commu_id;
+            $_REQUEST['msg'] = "イベント作成をおこなうにはコミュニティに参加する必要があります";
+            openpne_forward('ktai', 'page', "c_home");
+            exit;
         }
 
         $c_commu = db_commu_c_commu4c_commu_id2($c_commu_id);
+
         //トピック作成権限チェック
         if ($c_commu['topic_authority'] == 'admin_only' && !db_commu_is_c_commu_admin($c_commu_id, $u)) {
-            handle_kengen_error();
+            ktai_display_error("イベントは管理者だけが作成できます");
         }
         //---
 
