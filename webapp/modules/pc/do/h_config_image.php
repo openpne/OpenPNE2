584da999a635a0ef17a2996fc9c6704171f73502
yamaji <yamaji@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-12-21 07:00:46 +0000

    http://trac.openpne.jp/ticket/1520  プロフィール写真の連投により２個のメイン画像が見かけ上出てくる。この問題をFIX
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4841 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_config_image.php
+++ webapp/modules/pc/do/h_config_image.php
@@ -41,15 +41,21 @@ class pc_do_h_config_image extends OpenPNE_Action
         $image_filename = image_insert_c_image($upfile_obj, "m_{$u}");
 
         if ($image_filename) {
-            //c_memberのフィールドに登録
-            db_member_config_image_new($u, $image_filename, $img_num);
-
-            //画像1の時（最初の画像）メイン画像に
-            if ($img_num == 1) {
-                db_member_change_c_member_main_image($u, 1);
-            }
+        	if ( !(
+	        	$image_filename == $c_member['image_filename'] or 
+        		$image_filename == $c_member['image_filename_1'] or 
+	        	$image_filename == $c_member['image_filename_2'] or
+        		$image_filename == $c_member['image_filename_3']
+        	)) {
+	            //c_memberのフィールドに登録
+	            db_member_config_image_new($u, $image_filename, $img_num);
+	
+	            //画像1の時（最初の画像）メイン画像に
+	            if ($img_num == 1) {
+	                db_member_change_c_member_main_image($u, 1);
+	            }
+	    	}
         }
-
         openpne_redirect('pc', 'page_h_config_image');
     }
 }
