5d41eae431b970f4b7e42900e9e4069de3a4e000
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-12-26 01:34:57 +0000

    #1705:htmlspecialcharsを使わないようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4845 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/cmd/amazon.co.jp.js
+++ public_html/cmd/amazon.co.jp.js
@@ -1,12 +1,13 @@
- /**
-  * Amazon review CMD
-  *
-  * @license http://www.php.net/license/3_0.txt PHP License ver3.0
-  * @copylight 2007-2007 imoto<imoto@tejimaya.com>
-  * @copylight 2007-2007 Tejimaya .inc
-  * @author Imoto<imoto@tejimaya.com>
-  * @author Mamoru Tejima<tejima@tejimaya.com>
-  */
+/**
+ * Amazon review CMD
+ *
+ * @license http://www.php.net/license/3_0.txt PHP License ver3.0
+ * @copylight 2007-2007 imoto<imoto@tejimaya.com>
+ * @copylight 2007-2007 Tejimaya .inc
+ * @author Imoto<imoto@tejimaya.com>
+ * @author Mamoru Tejima<tejima@tejimaya.com>
+ */
+
 function url2cmd(url)
 {
     var match = url.match(/^.*(?:ASIN|product|dp)\/([^\/]+)(?:\/.*)*$/);
@@ -21,7 +22,6 @@ function url2cmd(url)
 
 function main(id)
 {
-    id = htmlSpecialChars(id);
     var url = "http://amazon.openpne.jp/?id=" + id ;
     var html = ''
     + '<iframe MARGINWIDTH="0" MARGINHEIGHT="0" HSPACE="0" VSPACE="0" FRAMEBORDER="0" SCROLLING="no" BORDERCOLOR="#000000" src="' + url + '" name="sample" width="420" height="320">'
@@ -29,11 +29,4 @@ function main(id)
     + '</iframe>';
 
     document.write(html);
-}
-
-function htmlSpecialChars(value)
-{
-    value = value.replace(/\</g, "&lt;").replace(/\>/g, "&gt;").replace(/\&/g, "&amp;").replace(/\"/g, "&quot;");
-    //value = value.replace(/\'/g, "&#039;");
-    return value;
 }
