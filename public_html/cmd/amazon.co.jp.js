f3ab2d470baabb9915317333209d158d5c79f7ec
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-12-26 01:19:30 +0000

    #1705:PEARコーディング規約に沿う形に修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4844 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/cmd/amazon.co.jp.js
+++ public_html/cmd/amazon.co.jp.js
@@ -7,30 +7,33 @@
   * @author Imoto<imoto@tejimaya.com>
   * @author Mamoru Tejima<tejima@tejimaya.com>
   */
-function url2cmd(url) {
-	var match = url.match(/^.*(?:ASIN|product|dp)\/([^\/]+)(?:\/.*)*$/);
-		
-	if (match) {
-	   var id = RegExp.$1;
-	   main(id);
+function url2cmd(url)
+{
+    var match = url.match(/^.*(?:ASIN|product|dp)\/([^\/]+)(?:\/.*)*$/);
+
+    if (match) {
+       var id = RegExp.$1;
+       main(id);
     } else {
        pne_url2a(url);
     }
 }
 
-function main(id) {
-	id = htmlSpecialChars(id);
-	var url = "http://amazon.openpne.jp/?id=" + id ;
+function main(id)
+{
+    id = htmlSpecialChars(id);
+    var url = "http://amazon.openpne.jp/?id=" + id ;
     var html = ''
-	+ '<iframe MARGINWIDTH="0" MARGINHEIGHT="0" HSPACE="0" VSPACE="0" FRAMEBORDER="0" SCROLLING="no" BORDERCOLOR="#000000" src="' + url + '" name="sample" width="420" height="320">'
-	+ 'この部分はインラインフレームを使用しています。'
-	+ '</iframe>';
-	
+    + '<iframe MARGINWIDTH="0" MARGINHEIGHT="0" HSPACE="0" VSPACE="0" FRAMEBORDER="0" SCROLLING="no" BORDERCOLOR="#000000" src="' + url + '" name="sample" width="420" height="320">'
+    + 'この部分はインラインフレームを使用しています。'
+    + '</iframe>';
+
     document.write(html);
 }
 
-function htmlSpecialChars(value) {
-	value = value.replace(/\</g, "&lt;").replace(/\>/g, "&gt;").replace(/\&/g, "&amp;").replace(/\"/g, "&quot;");
-	//value = value.replace(/\'/g, "&#039;");
-	return value;
+function htmlSpecialChars(value)
+{
+    value = value.replace(/\</g, "&lt;").replace(/\>/g, "&gt;").replace(/\&/g, "&amp;").replace(/\"/g, "&quot;");
+    //value = value.replace(/\'/g, "&#039;");
+    return value;
 }
