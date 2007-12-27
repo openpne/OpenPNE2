d6c082a61b0466c0869eacf73dc4f268b47269af
yamaji <yamaji@8bbc783d-d51a-0410-adab-8fa69d43f207>
2007-12-27 05:10:58 +0000

    #1650 イベントの「募集人数」の数値入力に"-"（マイナス）を入力しても登録が可能となっている。このバグを訂正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4858 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/c_event_add_insert_c_commu_topic.php
+++ webapp/modules/pc/do/c_event_add_insert_c_commu_topic.php
@@ -60,7 +60,9 @@ class pc_do_c_event_add_insert_c_commu_topic extends OpenPNE_Action
                 $err_msg[] = "募集期限は開催日時より未来に指定できません";
             }
         }
-
+        if ( $event['capacity'] != "" && 1*$event['capacity'] < 1 )
+        	$err_msg[] = "募集人数は１以上の数値を指定してください";
+        
         if ($err_msg) {
             $_REQUEST = $event;
             $_REQUEST['target_c_commu_id'] = $event['c_commu_id'];
