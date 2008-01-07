2d67c18c5b4d655ee4fb37e455a6fb4ffad33b7b
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-01-07 12:26:49 +0000

    #1748:PEAR::DBのバージョンを1.7.6から1.7.13に上げた
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4880 8bbc783d-d51a-0410-adab-8fa69d43f207

--- lib/include/DB/odbc.php
+++ lib/include/DB/odbc.php
@@ -18,9 +18,9 @@
  * @package    DB
  * @author     Stig Bakken <ssb@php.net>
  * @author     Daniel Convissor <danielc@php.net>
- * @copyright  1997-2005 The PHP Group
+ * @copyright  1997-2007 The PHP Group
  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    CVS: $Id: odbc.php,v 1.78 2005/02/28 01:42:17 danielc Exp $
+ * @version    CVS: $Id: odbc.php,v 1.81 2007/07/06 05:19:21 aharvey Exp $
  * @link       http://pear.php.net/package/DB
  */
 
@@ -42,9 +42,9 @@ require_once 'DB/common.php';
  * @package    DB
  * @author     Stig Bakken <ssb@php.net>
  * @author     Daniel Convissor <danielc@php.net>
- * @copyright  1997-2005 The PHP Group
+ * @copyright  1997-2007 The PHP Group
  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    Release: 1.7.6
+ * @version    Release: 1.7.13
  * @link       http://pear.php.net/package/DB
  */
 class DB_odbc extends DB_common
@@ -266,7 +266,7 @@ class DB_odbc extends DB_common
         }
         // Determine which queries that should return data, and which
         // should return an error code only.
-        if (DB::isManip($query)) {
+        if ($this->_checkManip($query)) {
             $this->affected = $result; // For affectedRows()
             return DB_OK;
         }
@@ -367,7 +367,7 @@ class DB_odbc extends DB_common
      */
     function freeResult($result)
     {
-        return @odbc_free_result($result);
+        return is_resource($result) ? odbc_free_result($result) : false;
     }
 
     // }}}
