d99adbaa862b09f3f0b265c0da7d9550c023d429
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-01-10 07:13:28 +0000

    #1765:$_SESSIONのキー名をc_member_idにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4895 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/do/o_easy_login.php
+++ webapp/modules/ktai/do/o_easy_login.php
@@ -60,7 +60,6 @@ class ktai_do_o_easy_login extends OpenPNE_Action
         $auth->auth->setAuth($username);
         $auth->auth->setAuthData('OPENPNE_URL', OPENPNE_URL);
         
-        $_SESSION['c_member_id'] = $c_member_id;
         db_member_do_access($c_member_id);
 
         $p = array();
