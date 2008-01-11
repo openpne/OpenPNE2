103957d5143a041d3000202146da2e072020d423
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-01-11 18:18:27 +0000

    #1747:カテゴリをSearchIndexに置き換え
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4922 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ setup/sql/postgres74/update/update01-for2.11.3-alter_c_review_category.sql
@@ -0,0 +1,12 @@
+UPDATE c_review_category SET category = 'Books' WHERE c_review_category_id = 1;
+UPDATE c_review_category SET category = 'ForeignBooks' WHERE c_review_category_id = 2;
+UPDATE c_review_category SET category = 'Music' WHERE c_review_category_id = 3;
+UPDATE c_review_category SET category = 'Classical' WHERE c_review_category_id = 4;
+UPDATE c_review_category SET category = 'DVD' WHERE c_review_category_id = 5;
+UPDATE c_review_category SET category = 'VideoGames' WHERE c_review_category_id = 6;
+UPDATE c_review_category SET category = 'Software' WHERE c_review_category_id = 7;
+UPDATE c_review_category SET category = 'Electronics' WHERE c_review_category_id = 8;
+UPDATE c_review_category SET category = 'Kitchen' WHERE c_review_category_id = 9;
+UPDATE c_review_category SET category = 'Toys' WHERE c_review_category_id = 10;
+UPDATE c_review_category SET category = 'SportingGoods' WHERE c_review_category_id = 11;
+UPDATE c_review_category SET category = 'HealthPersonalCare' WHERE c_review_category_id = 12;
