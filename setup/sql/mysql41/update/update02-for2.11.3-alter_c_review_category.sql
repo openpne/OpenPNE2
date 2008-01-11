bfed3a50e11b31756fab4d1f535eedbe2dd43a81
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-01-11 18:39:10 +0000

    #1774:レビューのカテゴリに「時計」「ベビー＆マタニティ」「アパレル＆シューズ」を追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@4923 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/mysql41/update/update02-for2.11.3-alter_c_review_category.sql
+++ setup/sql/mysql41/update/update02-for2.11.3-alter_c_review_category.sql
@@ -1,3 +1,5 @@
+/*!40101 SET NAMES utf8 */;
+
 UPDATE `c_review_category` SET `category` = 'Books' WHERE `c_review_category_id` = 1;
 UPDATE `c_review_category` SET `category` = 'ForeignBooks' WHERE `c_review_category_id` = 2;
 UPDATE `c_review_category` SET `category` = 'Music' WHERE `c_review_category_id` = 3;
@@ -10,3 +12,6 @@ UPDATE `c_review_category` SET `category` = 'Kitchen' WHERE `c_review_category_i
 UPDATE `c_review_category` SET `category` = 'Toys' WHERE `c_review_category_id` = 10;
 UPDATE `c_review_category` SET `category` = 'SportingGoods' WHERE `c_review_category_id` = 11;
 UPDATE `c_review_category` SET `category` = 'HealthPersonalCare' WHERE `c_review_category_id` = 12;
+INSERT INTO `c_review_category` VALUES (NULL,'Watches','時計',13);
+INSERT INTO `c_review_category` VALUES (NULL,'Baby','ベビー＆マタニティ',14);
+INSERT INTO `c_review_category` VALUES (NULL,'Apparel','アパレル＆シューズ',15);
