e98a01a2e05bb3ad7b2e2b97501600a018f8d237
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-01-21 14:21:01 +0000

    #1663:トピック作成リンクの表示・トピック作成アクションの実行権限の統一（r4804の安定版への再度取り込み）
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@4987 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/c_event_add_confirm.php
+++ webapp/modules/pc/page/c_event_add_confirm.php
@@ -17,20 +17,13 @@ class pc_page_c_event_add_confirm extends OpenPNE_Action
         //--- 権限チェック
         //コミュニティメンバー
         if (!db_commu_is_c_commu_member($target_c_commu_id, $u)) {
-            $_REQUEST['target_c_commu_id'] = $target_c_commu_id;
-            $_REQUEST['msg'] = "イベント作成をおこなうにはコミュニティに参加する必要があります";
-            openpne_forward('pc', 'page', "c_home");
-            exit;
+            handle_kengen_error();
         }
 
         $c_commu = db_commu_c_commu4c_commu_id2($target_c_commu_id);
-
         //トピック作成権限チェック
         if ($c_commu['topic_authority'] == 'admin_only' && !db_commu_is_c_commu_admin($target_c_commu_id, $u)) {
-            $_REQUEST['target_c_commu_id'] = $target_c_commu_id;
-            $_REQUEST['msg'] = "イベントは管理者だけが作成できます";
-            openpne_forward('pc', 'page', "c_home");
-            exit;
+            handle_kengen_error();
         }
         //---
 
