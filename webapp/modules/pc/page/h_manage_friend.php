e20f7e5ca937f90a28aca8fdb77f5ee16a9de24f
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-02-22 17:27:53 +0000

    #1548:pc_page_h_manage_friendにページャをつけた
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@5311 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/h_manage_friend.php
+++ webapp/modules/pc/page/h_manage_friend.php
@@ -9,9 +9,25 @@ class pc_page_h_manage_friend extends OpenPNE_Action
     function execute($requests)
     {
         $u = $GLOBALS['AUTH']->uid();
+        $page = $requests['page'];
+        $size = 20;
+
+        list($c_friend_list, $prev, $next, $total_num) = db_friend_c_friend_list_disp4c_member_id($u, $page, $size);
+
+        $pager = array(
+            'prev' => $prev,
+            'next' => $next,
+            'total_num' => $total_num,
+        );
+        $pager['start'] = $size * ($page - 1) + 1;
+        $pager['end'] = $size * $page;
+        if ($pager['end'] > $total_num) {
+            $pager['end'] = $total_num;
+        }
 
         $this->set('inc_navi', fetch_inc_navi('h'));
-        $this->set('c_friend_list', db_friend_c_friend_list_disp4c_member_id($u));
+        $this->set('c_friend_list', $c_friend_list);
+        $this->set('pager', $pager);
 
         return 'success';
     }
