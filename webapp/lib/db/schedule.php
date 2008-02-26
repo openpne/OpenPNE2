5aefee76e3ee515e382301943662bf272d043c12
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-02-26 11:49:45 +0000

    #1919:１日のイベントが月間カレンダーに反映されない
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@5371 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/db/schedule.php
+++ webapp/lib/db/schedule.php
@@ -51,7 +51,7 @@ function db_schedule_event4c_member_id($year, $month, $c_member_id)
     }
 
     $sql = 'SELECT * FROM c_commu_topic WHERE c_commu_id IN ('.$ids.')' .
-            ' AND event_flag = 1 AND open_date > ? AND open_date <= ?';
+            ' AND event_flag = 1 AND open_date >= ? AND open_date <= ?';
 
     $max_day = date("t", mktime(0,0,0,intval($month),1,intval($year) ));
     $params = array(
