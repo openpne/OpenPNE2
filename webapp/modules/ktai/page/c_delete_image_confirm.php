29f02c9e8a9278c9c6e150ce41b99ec5c33c78cf
kiwa <kiwa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-03-20 15:49:40 +0000

    #2085 携帯：副管理者がコミュニティ画像を変更できるようにした(2.10)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@5904 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/c_delete_image_confirm.php
+++ webapp/modules/ktai/page/c_delete_image_confirm.php
@@ -18,10 +18,7 @@ class ktai_page_c_delete_image_confirm extends OpenPNE_Action
 
         //--- 権限チェック
         $status = db_common_commu_status($u, $target_c_commu_id);
-        if (!$status['is_commu_admin']) {
-            handle_kengen_error();
-        }
-        if ($status['is_commu_sub_admin']) {
+        if (!$status['is_commu_admin'] && !$status['is_commu_sub_admin']) {
             handle_kengen_error();
         }
         //---
