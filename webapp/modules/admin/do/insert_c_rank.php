26e8ad429105b289bcfd5c42ca1ed2db718ebd00
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-03-24 14:58:26 +0000

    #2044:存在しない画像を指定してもランクの追加ができていたのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6047 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/insert_c_rank.php
+++ webapp/modules/admin/do/insert_c_rank.php
@@ -14,6 +14,7 @@ class admin_do_insert_c_rank extends OpenPNE_Action
 
     function execute($requests)
     {
+        // ランク機能が無効
         if (!OPENPNE_USE_POINT_RANK) {
             admin_client_redirect('top', '指定されたページにはアクセスできません');
         }
@@ -22,14 +23,33 @@ class admin_do_insert_c_rank extends OpenPNE_Action
             admin_client_redirect('list_c_rank', '同一の到達ポイントのランクが既に存在します');
         }
 
-        $c_rank_id = db_admin_insert_c_rank($requests['name'], '', $requests['point']);
-        if ($_FILES['image_upfile']['name']) {
-            $ext = t_check_image_format($_FILES['image_upfile']);
-            $image_filename = "r_{$c_rank_id}_".time().".{$ext}";
-            admin_insert_c_image($_FILES['image_upfile'], $image_filename);
+        $upfile_obj = $_FILES['image_upfile'];
 
-            db_admin_update_c_rank($c_rank_id, $requests['name'], $image_filename, $requests['point']);
+        // ランク画像が指定されていない
+        if (empty($upfile_obj) || $upfile_obj['error'] === UPLOAD_ERR_NO_FILE) {
+            admin_client_redirect('list_c_rank', 'ランク画像を指定してください');
         }
+
+        // ランク画像の形式チェック
+        if (!empty($upfile_obj) && $upfile_obj['error'] !== UPLOAD_ERR_NO_FILE) {
+            if (!($image = t_check_image($upfile_obj))) {
+                admin_client_redirect('list_c_rank', '画像は' . IMAGE_MAX_FILESIZE . 'KB以内のGIF・JPEG・PNGにしてください');
+            }
+        }
+
+        // ランクを追加
+        $c_rank_id = db_admin_insert_c_rank($requests['name'], '', $requests['point']);
+        if (!$c_rank_id) {
+            admin_client_redirect('list_c_rank', 'ランクが追加できませんでした');
+        }
+
+        // ランク画像挿入
+        $ext = t_check_image_format($upfile_obj);
+        $image_filename = "r_{$c_rank_id}_".time().".{$ext}";
+        admin_insert_c_image($upfile_obj, $image_filename);
+
+        // ランクとランク画像の関連付け
+        db_admin_update_c_rank($c_rank_id, $requests['name'], $image_filename, $requests['point']);
 
         admin_client_redirect('list_c_rank', 'ランクを追加しました');
     }
