1772c830e1eb3a33a8ec8af698b95b1236ddea48
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-09 01:42:11 +0000

    #2059:プロフィールのバリデーションルール生成部分の共通化
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6439 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_regist_prof.php
+++ webapp/modules/pc/do/h_regist_prof.php
@@ -35,7 +35,7 @@ class pc_do_h_regist_prof extends OpenPNE_Action
         } else {
             $validator->addRequests($_REQUEST['profile']);
         }
-        $validator->addRules($this->_getValidateRulesProfile());
+        $validator->addRules(util_get_validate_rules_profile('regist'));
         if (!$validator->validate()) {
             $errors = array_merge($errors, $validator->getErrors());
         }
@@ -97,8 +97,7 @@ class pc_do_h_regist_prof extends OpenPNE_Action
             break;
         }
     }
-    
-    
+
     function _getValidateRules()
     {
         return array(
@@ -144,36 +143,6 @@ class pc_do_h_regist_prof extends OpenPNE_Action
                 'caption' => '秘密の質問の答え',
             ),
         );
-    }
-
-    function _getValidateRulesProfile()
-    {
-        $rules = array();
-        $profile_list = db_member_c_profile_list4null();
-        foreach ($profile_list as $profile) {
-            if ($profile['disp_regist']) {
-                $rule = array(
-                    'type' => 'int',
-                    'required' => $profile['is_required'],
-                    'caption' => $profile['caption'],
-                );
-                switch ($profile['form_type']) {
-                case 'text':
-                case 'textlong':
-                case 'textarea':
-                    $rule['type'] = $profile['val_type'];
-                    $rule['regexp'] = $profile['val_regexp'];
-                    $rule['min'] = $profile['val_min'];
-                    ($profile['val_max']) and $rule['max'] = $profile['val_max'];
-                    break;
-                case 'checkbox':
-                    $rule['is_array'] = '1';
-                    break;
-                }
-                $rules[$profile['name']] = $rule;
-            }
-        }
-        return $rules;
     }
 }
 
