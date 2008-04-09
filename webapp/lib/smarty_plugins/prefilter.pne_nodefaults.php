f2e50f54aa53e25ea8c6d39237bb596cdb4a8064
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-09 10:12:48 +0000

    #2187:default_modifiers対象外にt_loopを追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6455 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/smarty_plugins/prefilter.pne_nodefaults.php
+++ webapp/lib/smarty_plugins/prefilter.pne_nodefaults.php
@@ -9,6 +9,7 @@ function smarty_prefilter_pne_nodefaults($tpl_source, &$smarty)
     $tpl_source = preg_replace_callback('/\(\{if .*?\}\)/', '_smarty_prefilter_pne_nodefaults_callback', $tpl_source);
     $tpl_source = preg_replace_callback('/\(\{elseif .*?\}\)/', '_smarty_prefilter_pne_nodefaults_callback', $tpl_source);
     $tpl_source = preg_replace_callback('/\(\{foreach .*?\}\)/', '_smarty_prefilter_pne_nodefaults_callback', $tpl_source);
+    $tpl_source = preg_replace_callback('/\(\{t_loop .*?\}\)/', '_smarty_prefilter_pne_nodefaults_callback', $tpl_source);
     return $tpl_source;
 }
 
