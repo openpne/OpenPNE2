dcebe4e4ed6a5bc487b0b0966ebe05f76762a569
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-16 11:42:18 +0000

    #135:各アクションでアルバム使用設定を見るようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6572 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/delete_album_image_confirm.php
+++ webapp/modules/admin/page/delete_album_image_confirm.php
@@ -8,6 +8,10 @@ class admin_page_delete_album_image_confirm extends OpenPNE_Action
 {
     function execute($requests)
     {
+        if (!OPENPNE_USE_ALBUM) {
+            handle_kengen_error();
+        }
+
         $v = array();
         $target_c_album_image_id = $requests['target_c_album_image_id'];
         $album_image = db_album_image_get_c_album_image4id($target_c_album_image_id);
