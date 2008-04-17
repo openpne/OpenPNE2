4baf256d0e9ec9da604cb16ee5d0ce5dd35929f9
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-17 07:59:54 +0000

    #135:PostgreSQLのupdateSQLを追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6609 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ setup/sql/postgres74/update/update10-for2.11.7-install_album.sql
@@ -0,0 +1,22 @@
+CREATE TABLE c_album (
+  c_album_id serial NOT NULL,
+  c_member_id int4 NOT NULL default '0',
+  subject text NOT NULL,
+  description text NOT NULL,
+  album_cover_image text NOT NULL,
+  r_datetime timestamp NOT NULL default '0000-01-01 00:00:00',
+  u_datetime timestamp NOT NULL default '0000-01-01 00:00:00',
+  public_flag varchar(20) NOT NULL default 'public',
+  PRIMARY KEY (c_album_id)
+);
+
+CREATE TABLE c_album_image (
+  c_album_image_id serial NOT NULL,
+  c_album_id int4 NOT NULL default '0',
+  c_member_id int4 NOT NULL default '0',
+  image_filename text NOT NULL,
+  image_description text NOT NULL,
+  filesize int4 NOT NULL default '0',
+  r_datetime timestamp NOT NULL default '0000-01-01 00:00:00',
+  PRIMARY KEY (c_album_image_id)
+);
