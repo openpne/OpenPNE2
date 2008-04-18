fb6f923815cfa8228a41bd032e71a0f4d983f6be
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-18 09:53:25 +0000

    #135:アルバム機能関連アクションの余計なコメントを削除（コメントを削除することで処理がつかみにくくなる箇所は、併せて軽いリファクタリングも実施）
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6628 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_album_image_edit_insert_c_album_image.php
+++ webapp/modules/pc/do/h_album_image_edit_insert_c_album_image.php
@@ -31,31 +31,24 @@ class pc_do_h_album_image_edit_insert_c_album_image extends OpenPNE_Action
         $tmpfile_1 = $requests['tmpfile_1'];
         // ----------
         
-        //アルバム画像登録処理
-        /*
-         * ※tmpfile_1にファイル画像がある場合
-         * 1.既存の変更する画像データを削除
-         *   削除対象テーブル c_image_album
-         * 2.tmpfile_1の画像データ登録
-         *   登録対象テーブル c_image_album
-         * 3.登録した画像ファイル名をc_album_imageデータに更新
-         */
         $filename = '';
         $c_album_image = db_album_image_get_c_album_image4id($target_c_album_image_id);
         $img_tmp_dir_path = OPENPNE_VAR_DIR . '/tmp/';
-        if($tmpfile_1){
+
+        // アルバム画像登録処理
+        if ($tmpfile_1) {
             db_album_image_data_delete($c_album_image['image_filename']);
             $filename = image_insert_c_image_album4tmp("a_{$target_c_album_id}_1", $tmpfile_1);
             $filesize_1 = filesize($img_tmp_dir_path . basename($tmpfile_1));
         }
         
-        //画像データ一時ファイル削除
+        // 画像データ一時ファイル削除
         $sessid = session_id();
         t_image_clear_tmp($sessid);
 
         db_album_update_c_album_image($target_c_album_image_id,  $filename, $image_description,$filesize_1);
 
-        //c_albumの更新時間UPDATE
+        // c_albumの更新時間UPDATE
         db_album_update_c_album_u_datetime($target_c_album_id);
         
         $p = array('target_c_album_id' => $target_c_album_id);
