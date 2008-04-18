8968ae986d4d7a37ab28e11671091f7126459d9c
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-18 08:52:19 +0000

    #135:画像挿入時にPostgreSQL環境でエラーが出ることがあったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6626 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_album_image_add_insert_c_album_image.php
+++ webapp/modules/pc/do/h_album_image_add_insert_c_album_image.php
@@ -26,44 +26,39 @@ class pc_do_h_album_image_add_insert_c_album_image extends OpenPNE_Action
 
         // --- リクエスト変数
         $target_c_album_id = $requests['target_c_album_id'];
-        $image_description1 = $requests['image_description1'];
-        $image_description2 = $requests['image_description2'];
-        $image_description3 = $requests['image_description3'];
-        $image_description4 = $requests['image_description4'];
-        $image_description5 = $requests['image_description5'];
-        $tmpfile_1 = $requests['tmpfile_1'];
-        $tmpfile_2 = $requests['tmpfile_2'];
-        $tmpfile_3 = $requests['tmpfile_3'];
-        $tmpfile_4 = $requests['tmpfile_4'];
-        $tmpfile_5 = $requests['tmpfile_5'];
         // ----------
 
         $img_tmp_dir_path = OPENPNE_VAR_DIR . '/tmp/';
-        if($tmpfile_1){
-            $filename_1 = image_insert_c_image_album4tmp("a_{$target_c_album_id}_1", $tmpfile_1);
-            $filesize_1 = filesize($img_tmp_dir_path . basename($tmpfile_1));
-        }
-        if($tmpfile_2){
-            $filename_2 = image_insert_c_image_album4tmp("a_{$target_c_album_id}_2", $tmpfile_2);
-            $filesize_2 = filesize($img_tmp_dir_path . basename($tmpfile_2));
-        }
-        if($tmpfile_3){
-            $filename_3 = image_insert_c_image_album4tmp("a_{$target_c_album_id}_3", $tmpfile_3);
-            $filesize_3 = filesize($img_tmp_dir_path . basename($tmpfile_3));
-        }
-        if($tmpfile_4){
-            $filename_4 = image_insert_c_image_album4tmp("a_{$target_c_album_id}_4", $tmpfile_4);
-            $filesize_4 = filesize($img_tmp_dir_path . basename($tmpfile_4));
-        }
-        if($tmpfile_5){
-            $filename_5 = image_insert_c_image_album4tmp("a_{$target_c_album_id}_5", $tmpfile_5);
-            $filesize_5 = filesize($img_tmp_dir_path . basename($tmpfile_5));
+        $insert_data = array();
+        $filesize_all = 0;
+
+        // 画像登録用の連想配列を作成
+        for ($i = 1; $i <= 5; $i++) {
+            $tmpfile = $requests['tmpfile_' . $i];
+
+            if ($tmpfile) {
+                $description = $requests['image_description' . $i];
+                $filename = image_insert_c_image_album4tmp("a_{$target_c_album_id}_{$i}", $tmpfile);
+                $filesize = filesize($img_tmp_dir_path . basename($tmpfile));
+
+                // 説明文が空の場合はファイル名を挿入する
+                if (!$description) {
+                    $description = $filename;
+                }
+
+                $insert_data[] = array(
+                    'filename' => $filename,
+                    'description' => $description,
+                    'filesize' => $filesize,
+                );
+
+                $filesize_all += $filesize;
+            }
         }
 
-        $sessid = session_id();
-        t_image_clear_tmp($sessid);
+        t_image_clear_tmp(session_id());
 
-        $filesize_all = $filesize_1 + $filesize_2 + $filesize_3 + $filesize_4 + $filesize_5; 
+        // 容量制限
         if (!db_album_is_insertable4c_member_id($u, $filesize_all)) {
             $msg = 'これ以上画像を投稿することができません。';
             if (!db_album_is_insertable4c_member_id($u)) {
@@ -77,41 +72,16 @@ class pc_do_h_album_image_add_insert_c_album_image extends OpenPNE_Action
             );
             openpne_redirect('pc', 'page_h_album_image_add', $p);
         }
-        
-        // 説明がなければ画像の名前を設定
-        if(!$image_description1){
-            $image_description1 = $filename_1;
-        }
-        if(!$image_description2){
-            $image_description2 = $filename_2;
-        }
-        if(!$image_description3){
-            $image_description3 = $filename_3;
-        }
-        if(!$image_description4){
-            $image_description4 = $filename_4;
-        }
-        if(!$image_description5){
-            $image_description5 = $filename_5;
-        }
-        
-        $insert_data = array(
-                            0=>array('image_description'=>$image_description1,'image_filename'=>$filename_1,'image_size'=>$filesize_1),
-                            1=>array('image_description'=>$image_description2,'image_filename'=>$filename_2,'image_size'=>$filesize_2),
-                            2=>array('image_description'=>$image_description3,'image_filename'=>$filename_3,'image_size'=>$filesize_3),
-                            3=>array('image_description'=>$image_description4,'image_filename'=>$filename_4,'image_size'=>$filesize_4),
-                            4=>array('image_description'=>$image_description5,'image_filename'=>$filename_5,'image_size'=>$filesize_5),
-        );
-        
-        foreach ($insert_data as $value) {
-            db_insert_c_album_image($target_c_album_id, $u, $value['image_filename'], $value['image_description'], $value['image_size']);
-        }
-        
-        //c_albumの更新日時UPDATE
-        db_album_update_c_album_u_datetime($target_c_album_id);
-        
-        $p = array('target_c_album_id' => $target_c_album_id);
 
+        // 画像を登録する
+        foreach ($insert_data as $value) {
+            db_insert_c_album_image($target_c_album_id, $u, $value['filename'], $value['description'], $value['filesize']);
+        }
+
+        // 画像が登録されたので、アルバムの更新日時も更新する
+        db_album_update_c_album_u_datetime($target_c_album_id);
+
+        $p = array('target_c_album_id' => $target_c_album_id);
         openpne_redirect('pc', 'page_fh_album', $p);
     }
 }
