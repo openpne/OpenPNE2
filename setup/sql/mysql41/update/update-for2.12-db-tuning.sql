d5f842907729a3945dcf92f11391d75e0cacad5f
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-18 10:48:31 +0000

    #2090:r_datetime_comment->u_datetime
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6632 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/mysql41/update/update-for2.12-db-tuning.sql
+++ setup/sql/mysql41/update/update-for2.12-db-tuning.sql
@@ -1,9 +1,9 @@
 /* コメント記入履歴時間  */
-ALTER TABLE c_commu_topic ADD COLUMN r_datetime_comment datetime NOT NULL default '0000-00-00 00:00:00';
+ALTER TABLE c_commu_topic ADD COLUMN u_datetime datetime NOT NULL default '0000-00-00 00:00:00';
 
 /* Index 変更*/
 DROP INDEX r_datetime_c_commu_id ON c_commu_topic;
-CREATE INDEX r_datetime_c_commu_id ON c_commu_topic(c_commu_id,r_datetime_comment);
+CREATE INDEX r_datetime_c_commu_id ON c_commu_topic(c_commu_id,u_datetime);
 
 CREATE INDEX c_commu_id_c_member_id ON c_commu_member(c_commu_id,c_member_id);
 
@@ -26,18 +26,18 @@ CREATE INDEX `c_member_id_c_diary_id` ON `c_diary_comment_summary`(c_member_id,c
 DROP TABLE IF EXISTS `tmp_c_commu_topic`;
 CREATE TABLE `tmp_c_commu_topic` (
   `c_commu_topic_id` int(11) NOT NULL default '0',
-  `r_datetime_comment` datetime NOT NULL default '0000-00-00 00:00:00',
+  `u_datetime` datetime NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`c_commu_topic_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO tmp_c_commu_topic (c_commu_topic_id, r_datetime_comment)
-  (SELECT cct.c_commu_topic_id, MAX(cctc.r_datetime) as r_datetime_comment
+INSERT INTO tmp_c_commu_topic (c_commu_topic_id, u_datetime)
+  (SELECT cct.c_commu_topic_id, MAX(cctc.r_datetime) as u_datetime
      FROM c_commu_topic_comment AS cctc ,c_commu_topic AS cct
      WHERE cctc.c_commu_topic_id = cct.c_commu_topic_id 
      GROUP BY cct.c_commu_topic_id);
 
 UPDATE c_commu_topic INNER JOIN tmp_c_commu_topic USING (c_commu_topic_id)
- SET c_commu_topic.r_datetime_comment = tmp_c_commu_topic.r_datetime_comment;
+ SET c_commu_topic.u_datetime = tmp_c_commu_topic.u_datetime;
 
 DROP TABLE tmp_c_commu_topic;
 
