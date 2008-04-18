57c5815c6104519160f2a558d6895d0a325ac19a
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-18 11:54:24 +0000

    #2090:日記コメント記入履歴の既存データのコンバータを作成した
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6634 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/mysql41/update/update-for2.12-db-tuning.sql
+++ setup/sql/mysql41/update/update-for2.12-db-tuning.sql
@@ -1,13 +1,10 @@
-/* コメント記入履歴時間  */
 ALTER TABLE c_commu_topic ADD COLUMN u_datetime datetime NOT NULL default '0000-00-00 00:00:00';
 
-/* Index 変更*/
 DROP INDEX r_datetime_c_commu_id ON c_commu_topic;
 CREATE INDEX r_datetime_c_commu_id ON c_commu_topic(c_commu_id,u_datetime);
 
 CREATE INDEX c_commu_id_c_member_id ON c_commu_member(c_commu_id,c_member_id);
 
-/* 日記コメント記入履歴Table */
 CREATE TABLE `c_diary_comment_log` (
   `c_diary_comment_log_id` int(11) NOT NULL auto_increment,
   `c_member_id` int(11) NOT NULL default '0',
@@ -21,8 +18,6 @@ CREATE INDEX `c_diary_id` ON `c_diary_comment_log`(c_diary_id);
 CREATE INDEX `c_diary_id_r_datetime` ON `c_diary_comment_log`(c_diary_id,r_datetime);
 CREATE INDEX `c_member_id_c_diary_id` ON `c_diary_comment_log`(c_member_id,c_diary_id);
 
-/* 既存データのコンバート */
-/* コメント記入履歴時間  */
 DROP TABLE IF EXISTS `tmp_c_commu_topic`;
 CREATE TABLE `tmp_c_commu_topic` (
   `c_commu_topic_id` int(11) NOT NULL default '0',
@@ -40,4 +35,11 @@ UPDATE c_commu_topic INNER JOIN tmp_c_commu_topic USING (c_commu_topic_id)
  SET c_commu_topic.u_datetime = tmp_c_commu_topic.u_datetime;
 
 DROP TABLE tmp_c_commu_topic;
+
+INSERT INTO c_diary_comment_log (NULL, c_member_id, c_diary_id, r_datetime)
+  (SELECT NULL, c_member_id, c_diary_id, MAX(r_datetime) AS r_datetime
+    FROM c_diary_comment
+    WHERE TO_DAYS(NOW()) - TO_DAYS(r_datetime) <= 15
+    GROUP BY c_member_id, c_diary_id
+    ORDER BY r_datetime);
 
