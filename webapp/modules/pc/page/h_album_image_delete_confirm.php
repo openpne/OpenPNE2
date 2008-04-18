a2a2779d1a6d631febbff363b766af6f6a426719
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-18 09:53:25 +0000

    #135:アルバム機能関連アクションの余計なコメントを削除（コメントを削除することで処理がつかみにくくなる箇所は、併せて軽いリファクタリングも実施）
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6628 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/h_album_image_delete_confirm.php
+++ webapp/modules/pc/page/h_album_image_delete_confirm.php
@@ -39,42 +39,6 @@ class pc_page_h_album_image_delete_confirm extends OpenPNE_Action
         $this->set("form_val",$form_val);
         
         return 'success';
-
-        /*
-        $target_album = db_album_get_c_album4id($target_c_album_id);
-
-        // 削除するコメントがが指定されていない
-        if (!$target_c_album_image_id) {
-            $p = array('target_c_album_id' => $target_c_album_id);
-            openpne_redirect('pc', 'page_fh_album', $p);
-        }
-        
-
-        // オブジェクトの振り分け用
-        $target_c_member_id = $target_album[0]['c_member_id'];
-
-        // inc_navi.tpl
-        if ($target_c_member_id == $u) {
-            $type = 'h';
-            $is_album_admin = true;
-        } else {
-            $type = 'f';
-            $is_album_admin = false;
-        }
-        $this->set('inc_navi', fetch_inc_navi($type, $target_c_member_id));
-        $this->set('is_album_admin', $is_album_admin);
-
-        $this->set('member', db_member_c_member4c_member_id($u));
-        $this->set('target_member', db_member_c_member4c_member_id($target_c_member_id));
-        $this->set('target_album', $target_album);
-
-        //削除するコメント一覧
-        $list = db_album_get_c_album_comment_list4id_list($target_c_album_comment_id);
-        $this->set('target_album_comment_list', array_shift($list));
-
-        //削除するコメントID
-        $this->set('target_c_album_comment_id', $target_c_album_comment_id);
-        */
     }
 }
 
