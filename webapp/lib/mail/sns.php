f14d9d959b8923052532d81f5f48f56720eb88f1
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-18 14:44:06 +0000

    #2090:c_diary_comment_summaryが残っていた箇所があったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6641 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/mail/sns.php
+++ webapp/lib/mail/sns.php
@@ -427,10 +427,10 @@ class mail_sns
 
         //日記コメント記入履歴追加
         if ($u != $target_c_member_id) {
-            db_diary_insert_c_diary_comment_summary($u,$target_c_diary_id);
+            db_diary_insert_c_diary_comment_log($u,$target_c_diary_id);
         }
         //日記コメント記入履歴更新
-        db_diary_update_c_diary_comment_summary($target_c_diary_id);
+        db_diary_update_c_diary_comment_log($target_c_diary_id);
 
         // 写真登録
         $images = $this->decoder->get_images();
