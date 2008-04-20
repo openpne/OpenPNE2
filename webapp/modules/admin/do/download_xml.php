3b5f8db25fa091b6532e5b91f74c71cb35a605f3
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-20 08:56:45 +0000

    LICENSEの年号が古くなっていたものを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6677 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/download_xml.php
+++ webapp/modules/admin/do/download_xml.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2005-2007 OpenPNE Project
+ * @copyright 2005-2008 OpenPNE Project
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 require_once 'XML/Serializer.php';
