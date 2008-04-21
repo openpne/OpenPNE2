c84c5e3689de313a98c2d627980319aba7c03e7f
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-21 12:19:20 +0000

    #2271:アルバム小窓を追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6688 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ public_html/cmd/openpne/pc_page_fh_album.js
@@ -0,0 +1,50 @@
+function url2cmd(url)
+{
+    if (!url.match(/(https?:\/\/[a-zA-Z0-9_\-\/.,:;~@=+$%#!\()]+)\/?\?(?:[a-zA-Z0-9_\-\/.,:;~?@=+$%#!\()]|&amp;)*&amp;target_c_album_id=([a-zA-Z0-9_\-]+)/)) {
+        pne_url2a(url);
+        return;
+    }
+    var openpne_url = RegExp.$1;
+    var target_c_album_id = RegExp.$2;
+
+    var xml_url = openpne_url + "?m=pc&a=page_fh_album_output_xml&target_c_album_id=" + target_c_album_id;
+    new Ajax.Request(xml_url, {
+        asynchronous : false ,
+        method: "get",
+
+        onSuccess: function(obj) {
+            var c_album = obj.responseXML.firstChild;
+            var c_member = c_album.getElementsByTagName('c_member')[0];
+
+            var subject = c_album.getElementsByTagName('subject')[0].firstChild.nodeValue;
+            var nickname = c_member.getElementsByTagName('nickname')[0].firstChild.nodeValue;
+            var album_cover_image = c_album.getElementsByTagName('album_cover_image')[0].firstChild.nodeValue;
+            var public_flag = '';
+            switch (c_album.getElementsByTagName('public_flag')[0].firstChild.nodeValue) {
+            case 'public' :
+                public_flag = "(全員に公開)";
+                break;
+            case 'friend' :
+                public_flag += "(マイフレンドに公開)";
+                break;
+            case 'private' :
+                public_flag += "(公開しない)";
+                break;
+            default :
+            }
+
+            var str = '<div class="dparts" style="width: 160px;"><div class="parts">'
+                    + '<div class="partsHeading"><h3>' + subject + '</h3></div>'
+                    + '<div style="text-align:center; padding: 10px;"><img src="' + album_cover_image + '" alt="' + subject + '" style=" margin-left: auto; margin-right: auto;" /></div>'
+                    + '<div style="text-align: center; padding: 5px;">' + public_flag + '</div>'
+                    + '<div style="text-align: center; padding: 5px;"><a href="' + url + '">アルバムを見る</a></div>'
+                    + '</div></div>';
+
+            document.write(str);
+        },
+
+        onFailure: function(obj) {
+            pne_url2a(url);
+        }
+    });
+}
