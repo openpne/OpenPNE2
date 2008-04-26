c3fc4295735aa2ad2a5629e392d91f657e9bf2fe
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-26 14:27:38 +0000

    #2307:フォトアルバム携帯版のアルバム詳細、アルバム写真一覧、アルバム写真詳細ページ作成
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6759 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ webapp/modules/ktai/page/h_err_fh_album.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright 2005-2008 OpenPNE Project
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ */
+
+class ktai_page_h_err_fh_album extends OpenPNE_Action
+{
+    function execute($requests)
+    {
+        return 'success';
+    }
+}
+
+?>
