214d5028533d8c93c343058b58446af0246dcd74
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-26 08:38:51 +0000

    #2305:c_member_config_option を使わないようにするPostgreSQL用SQLを追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6744 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ setup/sql/postgres74/update/update15-for2.12beta1-drop_c_member_config_option.sql
@@ -0,0 +1,9 @@
+ALTER TABLE c_member_config ADD COLUMN name varchar(64) NOT NULL default '';
+
+UPDATE c_member_config SET name = c_member_config_option.name
+    FROM c_member_config_option
+    WHERE c_member_config.c_member_config_option_id = c_member_config_option.c_member_config_option_id;
+
+DROP TABLE c_member_config_option;
+ALTER TABLE c_member_config DROP COLUMN c_member_config_option_id;
+
