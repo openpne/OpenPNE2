6e3370263b7d2e6fdff49b1a6d108a03faf55be7
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-26 09:02:47 +0000

    #2279:公開範囲設定を無視してアルバム写真が表示できてしまっていたのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6745 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/fh_album_image_show.php
+++ webapp/modules/pc/page/fh_album_image_show.php
@@ -6,8 +6,6 @@
 
 class pc_page_fh_album_image_show extends OpenPNE_Action
 {
-
-
     function execute($requests)
     {
         if (!OPENPNE_USE_ALBUM) {
@@ -17,22 +15,24 @@ class pc_page_fh_album_image_show extends OpenPNE_Action
         $u = $GLOBALS['AUTH']->uid();
         
         // --- リクエスト変数
-        $target_c_album_id       = $requests['target_c_album_id'];
         $target_c_album_image_id = $requests['target_c_album_image_id'];
         // ----------
         
         //targetが指定されていない
-        if(!$target_c_album_id || !$target_c_album_image_id){
+        if (!$target_c_album_image_id) {
             openpne_redirect('pc', 'page_h_err_fh_album');
         }
+
+        //targetのアルバム写真が存在しない
+        if (!p_common_is_active_c_album_image_id($target_c_album_image_id)) {
+            openpne_redirect('pc', 'page_h_err_fh_album');
+        }
+        
+        $target_c_album_image = db_album_image_get_c_album_image4id($target_c_album_image_id);
+        $target_c_album_id = $target_c_album_image['c_album_id'];
         
         //targetのアルバムが存在しない
-        if(!p_common_is_active_c_album_id($target_c_album_id)) {
-            openpne_redirect('pc', 'page_h_err_fh_album');
-        }
-        
-        //targetのアルバム写真が存在しない
-        if(!p_common_is_active_c_album_image_id($target_c_album_image_id)) {
+        if (!p_common_is_active_c_album_id($target_c_album_id)) {
             openpne_redirect('pc', 'page_h_err_fh_album');
         }
         
@@ -57,12 +57,9 @@ class pc_page_fh_album_image_show extends OpenPNE_Action
             db_ashiato_insert_c_ashiato($target_c_member_id, $u);
         }
         
-        $this->set("type", $type);
+        $this->set('type', $type);
         $this->set('inc_navi', fetch_inc_navi($type, $target_c_member_id));
-        
-        $target_c_album_image = db_album_image_get_c_album_image4id($target_c_album_image_id);
-        $this->set('c_album_image',$target_c_album_image);
-        
+        $this->set('c_album_image', $target_c_album_image);
         $this->set('c_album_iamge_id_prev', db_album_image_c_album_image_id_prev4c_album_id($target_c_member_id, $target_c_album_id, $target_c_album_image_id, $u));
         $this->set('c_album_iamge_id_next', db_album_image_c_album_image_id_next4c_album_id($target_c_member_id, $target_c_album_id, $target_c_album_image_id, $u));
 
