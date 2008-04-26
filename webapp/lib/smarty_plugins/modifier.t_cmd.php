8bc3439f0121b8dd632629abad73fd597474f846
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-26 13:21:17 +0000

    #2027:小窓ファイルが見つからない場合、小窓キャスターのJavascriptを読み込むようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6752 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/smarty_plugins/modifier.t_cmd.php
+++ webapp/lib/smarty_plugins/modifier.t_cmd.php
@@ -21,7 +21,14 @@ function _smarty_modifier_t_cmd_make_js($matches)
     $src  = $matches[2];
     $args = $matches[4];
 
-    if (!db_is_use_cmd($src, $GLOBALS['_CMD']['type'])) {
+    $path = 'cmd/' . $src . '.js';
+
+    // ファイルが読み込めない場合は、小窓キャスターのJavaScriptを読み込む
+    if (!is_readable($path)) {
+        $path = db_etc_c_cmd_url4name($src);
+    }
+
+    if (!db_is_use_cmd($src, $GLOBALS['_CMD']['type']) || !$path) {
         return $matches[0];
     }
 
@@ -29,7 +36,7 @@ function _smarty_modifier_t_cmd_make_js($matches)
     $arg_str = "'" . implode("','", $_args) . "'";
 
     $result = <<<EOD
-<script type="text/javascript" src="cmd/{$src}.js"></script>
+<script type="text/javascript" src="{$path}"></script>
 <script type="text/javascript">
 <!--
 main({$arg_str});
