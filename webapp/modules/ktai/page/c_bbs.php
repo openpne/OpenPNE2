cbdd79a4de15e7cbe616c0ac21e6df8c5ef4cb45
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-27 16:38:48 +0000

    #2058:コミュニティトピック、イベントのページャを変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6779 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/c_bbs.php
+++ webapp/modules/ktai/page/c_bbs.php
@@ -11,43 +11,34 @@ class ktai_page_c_bbs extends OpenPNE_Action
         $u  = $GLOBALS['KTAI_C_MEMBER_ID'];
 
         // --- リクエスト変数
-        $target_c_commu_topic_id = $requests['target_c_commu_topic_id'];
+        $c_commu_topic_id = $requests['target_c_commu_topic_id'];
         $page = $requests['page'];
         // ----------
 
         $page_size = 5;
 
-        //ページ
-        $this->set("page", $page);
+        // トピックのコメントリスト
+        if ($requests['order'] === 'asc') {
+            $desc = false;
+        } else {
+            $desc = true;
+        }
 
-        //トピックのコメントリスト
-        $list = db_commu_c_commu_topic_comment_list4c_c_commu_topic_id($target_c_commu_topic_id, $u, $page_size, $page);
-        $total_num = $list[3];
-        $this->set("is_prev", $list[1]);
-        $this->set("is_next", $list[2]);
-        $this->set('total_num', $total_num);
+        list($list, $pager) = db_commu_c_topic_write4c_commu_topic_id($c_commu_topic_id, $page, $page_size, $desc);
 
-        $pager = array();
-        // number ベースにする
-        $top = reset($list[0]);
-        $bottom = end($list[0]);
-        $pager['end'] = (int)$top['number'];
-        $pager['start'] = (int)$bottom['number'];
+        $this->set('c_commu_topic_comment_list', $list);
         $this->set('pager', $pager);
 
-        $c_commu_topic_comment_list = array_reverse($list[0]);
-        $this->set("c_commu_topic_comment_list", $c_commu_topic_comment_list);
-
         //トピック名
-        $this->set("c_commu_topic_name", db_commu_c_commu_topic_name4c_commu_topic_id($target_c_commu_topic_id));
+        $this->set('c_commu_topic_name', db_commu_c_commu_topic_name4c_commu_topic_id($c_commu_topic_id));
         //トピックID,トピック
-        $this->set("c_commu_topic_id", $target_c_commu_topic_id);
-        $this->set("c_commu_topic", db_commu_c_topic4c_commu_topic_id_2($target_c_commu_topic_id));
+        $this->set('c_commu_topic_id', $c_commu_topic_id);
+        $this->set('c_commu_topic', db_commu_c_topic4c_commu_topic_id_2($c_commu_topic_id));
 
         //コミュニティ
-        $c_commu = db_commu_c_commu4c_commu_topic_id($target_c_commu_topic_id);
+        $c_commu = db_commu_c_commu4c_commu_topic_id($c_commu_topic_id);
         $c_commu_id = $c_commu['c_commu_id'];
-        $this->set("c_commu", $c_commu);
+        $this->set('c_commu', $c_commu);
 
         //--- 権限チェック
         //コミュニティの存在の有無
@@ -61,24 +52,24 @@ class ktai_page_c_bbs extends OpenPNE_Action
         }
 
         //掲示板の閲覧権限 tplでやっている
-        $this->set("is_c_commu_view", db_commu_is_c_commu_view4c_commu_idAc_member_id($c_commu['c_commu_id'], $u));
-        $this->set("is_c_commu_member", db_commu_is_c_commu_member($c_commu['c_commu_id'], $u));
-        $this->set("is_c_event_member", db_commu_is_c_event_member($target_c_commu_topic_id, $u));
-        $this->set("is_c_event_admin", db_commu_is_c_event_admin($target_c_commu_topic_id, $u));
-        $this->set('is_event_join_date', db_commu_is_event_join_date($target_c_commu_topic_id));
+        $this->set('is_c_commu_view', db_commu_is_c_commu_view4c_commu_idAc_member_id($c_commu['c_commu_id'], $u));
+        $this->set('is_c_commu_member', db_commu_is_c_commu_member($c_commu['c_commu_id'], $u));
+        $this->set('is_c_event_member', db_commu_is_c_event_member($c_commu_topic_id, $u));
+        $this->set('is_c_event_admin', db_commu_is_c_event_admin($c_commu_topic_id, $u));
+        $this->set('is_event_join_date', db_commu_is_event_join_date($c_commu_topic_id));
 
         //メンバーがコミュニティ管理者かどうか
-        $this->set("is_admin", db_commu_is_admin4c_member_id_c_commu_topic_id($u, $target_c_commu_topic_id));
+        $this->set('is_admin', db_commu_is_admin4c_member_id_c_commu_topic_id($u, $c_commu_topic_id));
         //コミュニティ管理者
-        $this->set("c_member_admin", db_commu_c_member_admin4c_commu_topic_id($target_c_commu_topic_id));
+        $this->set('c_member_admin', db_commu_c_member_admin4c_commu_topic_id($c_commu_topic_id));
 
         if (MAIL_ADDRESS_HASHED) {
-            $mail_address = "t{$target_c_commu_topic_id}-".t_get_user_hash($u)."@".MAIL_SERVER_DOMAIN;
+            $mail_address = "t{$c_commu_topic_id}-".t_get_user_hash($u).'@'.MAIL_SERVER_DOMAIN;
         } else {
-            $mail_address = "t{$target_c_commu_topic_id}"."@".MAIL_SERVER_DOMAIN;
+            $mail_address = "t{$c_commu_topic_id}".'@'.MAIL_SERVER_DOMAIN;
         }
         $mail_address = MAIL_ADDRESS_PREFIX . $mail_address;
-        $this->set("mail_address", $mail_address);
+        $this->set('mail_address', $mail_address);
 
         return 'success';
     }
