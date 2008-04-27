b615448fcbbe6d599d48846f86ef0c5c2dc900bf
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-27 18:34:56 +0000

    #2181:merged r6397:6400, r6556 (thanks to toraneko)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6781 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/do/h_member_config.php
+++ webapp/modules/ktai/do/h_member_config.php
@@ -17,6 +17,10 @@ class ktai_do_h_member_config extends OpenPNE_Action
         // コメントメール通知
 
         db_member_update_c_member_config($u, 'SEND_DIARY_COMMENT_MAIL_KTAI', $requests['SEND_DIARY_COMMENT_MAIL_KTAI']);
+        if (DISPLAY_NEWDIARYTOPIC_HOME){
+            db_member_update_c_member_config($u, 'DISPLAY_CHANGE_NEWDIARY_HOME_KTAI', $requests['DISPLAY_CHANGE_NEWDIARY_HOME_KTAI']);
+            db_member_update_c_member_config($u, 'DISPLAY_CHANGE_NEWTOPIC_HOME_KTAI', $requests['DISPLAY_CHANGE_NEWTOPIC_HOME_KTAI']);
+        }
 
         $p = array('msg' => 32);
         openpne_redirect('ktai', 'page_h_config', $p);
