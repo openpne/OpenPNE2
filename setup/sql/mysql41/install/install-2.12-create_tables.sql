5ca99d40ab019484d2e32cc5c8d8ffb05302c7dd
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-28 10:58:08 +0000

    #2305:c_member_config.nameのインデックスを削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6797 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/mysql41/install/install-2.12-create_tables.sql
+++ setup/sql/mysql41/install/install-2.12-create_tables.sql
@@ -726,8 +726,7 @@ CREATE TABLE `c_member_config` (
   `name` varchar(64) NOT NULL default '',
   PRIMARY KEY  (`c_member_config_id`),
   KEY `c_member_id` (`c_member_id`),
-  KEY `c_member_config_option_id_c_member_id` (`c_member_id`),
-  KEY `name` (`name`)
+  KEY `c_member_config_option_id_c_member_id` (`c_member_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 CREATE TABLE `c_member_ktai_pre` (
