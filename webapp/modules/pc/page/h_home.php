114acf6e43b7b012de2f1ab59774f42f3e995f9b
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-04-28 13:20:05 +0000

    #2181:DISPLAY_OFF*->IS_DISPLAY*
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6802 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/h_home.php
+++ webapp/modules/pc/page/h_home.php
@@ -70,7 +70,7 @@ class pc_page_h_home extends OpenPNE_Action
         /// 最新情報 ///
 
         // 最新日記
-        if(DISPLAY_NEWDIARYTOPIC_HOME && !$OPTION['DISPLAY_OFF_NEWDIARY_HOME']){
+        if(DISPLAY_NEWDIARYTOPIC_HOME && $OPTION['IS_DISPLAY_NEWDIARY_HOME']){
             $this->set('c_diary_list_all', p_h_home_c_diary_all_list(5));
         }
         // フレンド最新日記
@@ -82,7 +82,7 @@ class pc_page_h_home extends OpenPNE_Action
         $c_diary_my_comment_list = p_h_home_c_diary_my_comment_list4c_member_id($u, 5);
         $this->set('c_diary_my_comment_list', $c_diary_my_comment_list);
         // コミュニティ全ての新着書き込み
-        if(DISPLAY_NEWDIARYTOPIC_HOME && !$OPTION['DISPLAY_OFF_NEWTOPIC_HOME']){
+        if(DISPLAY_NEWDIARYTOPIC_HOME && $OPTION['IS_DISPLAY_NEWTOPIC_HOME']){
             $this->set('c_topic_list_all', p_h_home_c_topic_all_list(5));
         }
         // 参加コミュニティの新着書き込み
