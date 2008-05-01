d75807b1760fcb9e9f0112e1088f1092df973ff9
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-05-01 11:02:36 +0000

    #1968:head要素では絵文字を変換しないようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@6901 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/smarty_plugins/outputfilter.pne_display_emoji.php
+++ webapp/lib/smarty_plugins/outputfilter.pne_display_emoji.php
@@ -8,11 +8,11 @@ function smarty_outputfilter_pne_display_emoji($tpl_output, &$smarty)
     $list = array();
 
     if (empty($GLOBALS['__Framework']['carrier'])) {
-        // input, textarea, title を退避
+        // input, textarea, head を退避
         $patterns = array(
             '/<input[^>]+>/is',
             '/<textarea[^>]+>.*?<\/textarea>/is',
-            '/<title.*?<\/title>/is',
+            '/<head.*?<\/head>/is',
         );
 
         list ($list, $tpl_output) = _smarty_outputfilter_pne_display_emoji_replace($patterns, $tpl_output);
@@ -22,7 +22,7 @@ function smarty_outputfilter_pne_display_emoji($tpl_output, &$smarty)
     $tpl_output = emoji_convert($tpl_output);
 
     if (empty($GLOBALS['__Framework']['carrier'])) {
-        // input, textarea, title を元に戻す
+        // input, textarea, head を元に戻す
         $tpl_output = str_replace(array_keys($list), array_values($list), $tpl_output);
     }
 
