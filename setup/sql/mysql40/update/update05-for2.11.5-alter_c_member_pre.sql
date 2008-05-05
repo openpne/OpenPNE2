dafebd25290eadf2d38b42dfffc0bd88a5fa46d5
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-05-05 12:34:14 +0000

    #2336:2.13の開発用に2.11用のアップデートSQLを削除する
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6967 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/mysql40/update/update05-for2.11.5-alter_c_member_pre.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE c_member_pre ADD COLUMN login_id varchar(255) NOT NULL default '';
-
--- setup/sql/postgres74/update/update05-for2.11.5-create_c_module.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE c_module (
-  c_module_id serial NOT NULL,
-  name text NOT NULL,
-  is_enabled smallint NOT NULL default '0',
-  PRIMARY KEY  (c_module_id),
-  UNIQUE (name)
-);
-
