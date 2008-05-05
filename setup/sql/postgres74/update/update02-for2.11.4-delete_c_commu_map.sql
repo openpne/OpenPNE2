1e4be0b86d7d4c9102ebceb31a5c547bb7eb87ca
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-05-05 12:34:14 +0000

    #2336:2.13の開発用に2.11用のアップデートSQLを削除する
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@6967 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/postgres74/update/update02-for2.11.4-delete_c_commu_map.sql
+++
@@ -1,6 +0,0 @@
-UPDATE c_commu SET info = info || 'http://maps.google.co.jp/maps?ll=' || map_latitude || ',' || map_longitude || '&z=' || map_zoom WHERE is_display_map = 1;
-
-ALTER TABLE c_commu DROP COLUMN is_display_map;
-ALTER TABLE c_commu DROP COLUMN map_latitude;
-ALTER TABLE c_commu DROP COLUMN map_longitude;
-ALTER TABLE c_commu DROP COLUMN map_zoom;
