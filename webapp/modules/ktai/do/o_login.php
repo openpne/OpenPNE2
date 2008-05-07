e76d78c8d57b662eb0ba497558d8c6fec72e575f
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-05-07 06:49:01 +0000

    #1506:閉じ括弧の記述漏れを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.8.x@7007 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/do/o_login.php
+++ webapp/modules/ktai/do/o_login.php
@@ -66,7 +66,7 @@ class ktai_do_o_login extends OpenPNE_Action
             parse_str($requests['login_params'], $p);
         }
 
-        if (!empty($p['a']) {
+        if (!empty($p['a'])) {
             $a = $p['a'];
         }
 
