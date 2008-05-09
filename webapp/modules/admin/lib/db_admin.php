f9746cb592f36c645a43de06378c0ed3857bd20a
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-05-09 11:49:07 +0000

    #2038:db_point_get_point4pre_rank_id->db_point_get_next_rank_point4rank_id
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7089 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/lib/db_admin.php
+++ webapp/modules/admin/lib/db_admin.php
@@ -707,7 +707,7 @@ function db_admin_c_member_id_list4cond_pne_point($ids, $cond_list)
     }
 
     if ($cond_list['e_rank']) {
-        $e_point = db_point_get_point4pre_rank_id($cond_list['e_rank']);
+        $e_point = db_point_get_next_rank_point4rank_id($cond_list['e_rank']);
         if (!is_null($e_point)) {
             $sql .= ' AND value < ?';
             $params[] = (int)$e_point;
