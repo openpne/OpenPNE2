ca212013246786b7bf6e5aae23669965a1ac52c1
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-05-21 18:24:14 +0000

    #2500:PC版「その他設定」を廃止
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7305 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_member_config_update_display_home.php
+++ webapp/modules/pc/do/h_member_config_update_display_home.php
@@ -4,10 +4,7 @@
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 
-/**
- * 設定変更
- */
-class pc_do_h_member_config extends OpenPNE_Action
+class pc_do_h_member_config_update_display_home extends OpenPNE_Action
 {
     function handleError($errors)
     {
@@ -22,6 +19,7 @@ class pc_do_h_member_config extends OpenPNE_Action
 
         db_member_update_c_member_config($u, 'IS_DISPLAY_NEWDIARY_HOME', $requests['IS_DISPLAY_NEWDIARY_HOME']);
         db_member_update_c_member_config($u, 'IS_DISPLAY_NEWTOPIC_HOME', $requests['IS_DISPLAY_NEWTOPIC_HOME']);
+
         openpne_redirect('pc', 'page_h_home');
     }
 }
