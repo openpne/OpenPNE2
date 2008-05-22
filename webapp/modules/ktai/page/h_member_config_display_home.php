d0bedaf3772d3990455cade96064ae1f67eaa1cb
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-05-22 07:14:57 +0000

    #2500:携帯版の「その他設定」を廃止
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@7310 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/h_member_config_display_home.php
+++ webapp/modules/ktai/page/h_member_config_display_home.php
@@ -4,10 +4,14 @@
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 
-class ktai_page_h_member_config extends OpenPNE_Action
+class ktai_page_h_member_config_display_home extends OpenPNE_Action
 {
     function execute($requests)
     {
+        if (!DISPLAY_NEWDIARYTOPIC_HOME) {
+            handle_kengen_error();
+        }
+
         $u  = $GLOBALS['KTAI_C_MEMBER_ID'];
 
         return 'success';
