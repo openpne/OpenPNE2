7d2786c8af388f593e7457ed67b9c17b86a70026
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-05-22 06:48:40 +0000

    #2500:携帯版「その他設定」から日記コメントお知らせメール設定を移動
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7308 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/do/h_member_config_update_diary_comment_mail.php
+++ webapp/modules/ktai/do/h_member_config_update_diary_comment_mail.php
@@ -7,17 +7,16 @@
 /**
  * 設定変更
  */
-class ktai_do_h_member_config extends OpenPNE_Action
+class ktai_do_h_member_config_update_diary_comment_mail extends OpenPNE_Action
 {
     function execute($requests)
     {
         $tail = $GLOBALS['KTAI_URL_TAIL'];
         $u = $GLOBALS['KTAI_C_MEMBER_ID'];
 
-        if (DISPLAY_NEWDIARYTOPIC_HOME){
-            db_member_update_c_member_config($u, 'DISPLAY_CHANGE_NEWDIARY_HOME_KTAI', $requests['DISPLAY_CHANGE_NEWDIARY_HOME_KTAI']);
-            db_member_update_c_member_config($u, 'DISPLAY_CHANGE_NEWTOPIC_HOME_KTAI', $requests['DISPLAY_CHANGE_NEWTOPIC_HOME_KTAI']);
-        }
+        // コメントメール通知
+
+        db_member_update_c_member_config($u, 'SEND_DIARY_COMMENT_MAIL_KTAI', $requests['SEND_DIARY_COMMENT_MAIL_KTAI']);
 
         $p = array('msg' => 48);
         openpne_redirect('ktai', 'page_h_config', $p);
