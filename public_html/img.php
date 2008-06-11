9dd9a60cf915f25e0d7bfb60d4b960ea2fa62047
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-06-11 13:43:19 +0000

    #2655:defined('CHECK_IMG_AUTH')を追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@7577 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/img.php
+++ public_html/img.php
@@ -11,7 +11,7 @@ ini_set('display_errors', false);
 ob_start();
 
 // モジュール毎に決められた認証をおこなっているかどうかのチェック
-if (CHECK_IMG_AUTH) {
+if (defined('CHECK_IMG_AUTH') && CHECK_IMG_AUTH) {
     require_once OPENPNE_WEBAPP_DIR . '/init.inc';
 
     $module = '';
