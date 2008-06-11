4fc86b578e6582ffa91c2deca03457940ab21daf
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-06-11 14:09:32 +0000

    #2634:定義していない$hostを使っていたのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@7580 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/smarty_plugins/modifier.t_url2a_ktai.php
+++ webapp/lib/smarty_plugins/modifier.t_url2a_ktai.php
@@ -49,7 +49,7 @@ function smarty_modifier_t_url2a_ktai_callback($matches)
     $param = str_replace('&amp;', '&', $param);
 
     // 携帯用URLに置換、ksid 追加
-    $ktai_url = $host . '?m=ktai&a=' . $converted_action . $param . '&' . $GLOBALS['KTAI_URL_TAIL'];
+    $ktai_url = $openpne_url . '?m=ktai&a=' . $converted_action . $param . '&' . $GLOBALS['KTAI_URL_TAIL'];
 
     // page_ より後ろを最大50文字に縮めて表示
     $urlstr = $converted_action . $param;
