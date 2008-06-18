04290f6b8000131a318a3a1c54c4824410d75c31
ogawa <ogawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-06-18 09:31:26 +0000

    #1502:スレーブPNEで使われるdb_member_create_member()でc_member_idを返すようにした、またINSERT失敗時に不要なデータを削除する処理を加えた
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@7610 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/o_login.php
+++ webapp/modules/pc/do/o_login.php
@@ -46,12 +46,11 @@ class pc_do_o_login extends OpenPNE_Action
         }
 
         $c_member_id = db_member_c_member_id4username_encrypted($auth->getUsername(), false);
+        if (IS_SLAVEPNE && !$c_member_id) {
+            $c_member_id = db_member_create_member($_POST['username']);
+        }
         if (!$c_member_id) {
-            if (IS_SLAVEPNE) {
-                db_member_create_member($_POST['username']);
-            } else {
-                $this->_fail_login();
-            }
+            $this->_fail_login();
         }
 
         db_api_update_token($c_member_id);
