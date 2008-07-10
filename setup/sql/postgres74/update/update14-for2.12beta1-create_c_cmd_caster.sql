bfdd4ef7d57fa43e431b19055fc74c7c8af40d1c
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-10 08:31:45 +0000

    #2755:DROPするインデックスの指定に誤りがあったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7678 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/postgres74/update/update14-for2.12beta1-create_c_cmd_caster.sql
+++ setup/sql/postgres74/update/update14-for2.12beta1-create_c_cmd_caster.sql
@@ -12,7 +12,7 @@ CREATE TABLE c_cmd_caster (
 ALTER TABLE c_cmd ADD COLUMN url varchar(128) NOT NULL default '';
 
 ALTER TABLE c_cmd DROP CONSTRAINT "c_cmd_name_key";
-DROP INDEX c_cmd_name_key;
+DROP INDEX c_cmd_name_2;
 
 ALTER TABLE c_cmd ADD COLUMN c_cmd_caster_id int4 NOT NULL default '0';
 ALTER TABLE c_cmd ADD COLUMN r_datetime timestamp NOT NULL default '0000-01-01 00:00:00';
