1499ccc63d31001b24660b6d950cd00e8f2c9fbd
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-10 10:57:06 +0000

    #2807:PostgreSQL 版セットアップSQLで、 ALTER TABLE ... ADD COLUMN 時にデフォルト値と NOT NULL 制約を追加してしまっている箇所があったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7688 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/postgres74/update/update14-for2.12beta1-create_c_cmd_caster.sql
+++ setup/sql/postgres74/update/update14-for2.12beta1-create_c_cmd_caster.sql
@@ -9,12 +9,25 @@ CREATE TABLE c_cmd_caster (
   UNIQUE (url)
 );
 
-ALTER TABLE c_cmd ADD COLUMN url varchar(128) NOT NULL default '';
+ALTER TABLE c_cmd ADD COLUMN url varchar(128);
+ALTER TABLE c_cmd ALTER COLUMN url SET DEFAULT '';
+UPDATE c_cmd SET url = '';
+ALTER TABLE c_cmd ALTER COLUMN url SET NOT NULL;
 
 ALTER TABLE c_cmd DROP CONSTRAINT "c_cmd_name_key";
 DROP INDEX c_cmd_name_2;
 
-ALTER TABLE c_cmd ADD COLUMN c_cmd_caster_id int4 NOT NULL default '0';
-ALTER TABLE c_cmd ADD COLUMN r_datetime timestamp NOT NULL default '0000-01-01 00:00:00';
-ALTER TABLE c_cmd ADD COLUMN u_datetime timestamp NOT NULL default '0000-01-01 00:00:00';
+ALTER TABLE c_cmd ADD COLUMN c_cmd_caster_id int4;
+ALTER TABLE c_cmd ALTER COLUMN c_cmd_caster_id SET DEFAULT '0';
+UPDATE c_cmd SET c_cmd_caster_id = '0';
+ALTER TABLE c_cmd ALTER COLUMN c_cmd_caster_id SET NOT NULL;
 
+ALTER TABLE c_cmd ADD COLUMN r_datetime timestamp;
+ALTER TABLE c_cmd ALTER COLUMN r_datetime SET DEFAULT '0000-01-01 00:00:00';
+UPDATE c_cmd SET r_datetime = '0000-01-01 00:00:00';
+ALTER TABLE c_cmd ALTER COLUMN r_datetime SET NOT NULL;
+
+ALTER TABLE c_cmd ADD COLUMN u_datetime timestamp;
+ALTER TABLE c_cmd ALTER COLUMN u_datetime SET DEFAULT '0000-01-01 00:00:00';
+UPDATE c_cmd SET u_datetime = '0000-01-01 00:00:00';
+ALTER TABLE c_cmd ALTER COLUMN u_datetime SET NOT NULL;
