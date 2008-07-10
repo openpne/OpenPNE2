ca3536606937815d5a02e3ea3f23873f984f2e1c
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-10 07:40:11 +0000

    #2755:PostgreSQL版セットアップ用SQLで、誤って存在しないインデックスを削除しようとしている箇所があったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7676 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/postgres74/update/update14-for2.12beta1-create_c_cmd_caster.sql
+++ setup/sql/postgres74/update/update14-for2.12beta1-create_c_cmd_caster.sql
@@ -10,8 +10,10 @@ CREATE TABLE c_cmd_caster (
 );
 
 ALTER TABLE c_cmd ADD COLUMN url varchar(128) NOT NULL default '';
-DROP INDEX name;
-DROP INDEX name_2;
+
+ALTER TABLE c_cmd DROP CONSTRAINT "c_cmd_name_key";
+DROP INDEX c_cmd_name_key;
+
 ALTER TABLE c_cmd ADD COLUMN c_cmd_caster_id int4 NOT NULL default '0';
 ALTER TABLE c_cmd ADD COLUMN r_datetime timestamp NOT NULL default '0000-01-01 00:00:00';
 ALTER TABLE c_cmd ADD COLUMN u_datetime timestamp NOT NULL default '0000-01-01 00:00:00';
