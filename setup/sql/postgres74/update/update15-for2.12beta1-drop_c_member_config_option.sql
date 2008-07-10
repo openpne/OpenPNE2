4181f1dce83ce67ad4809422397564b1b17eb6fe
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-10 10:57:06 +0000

    #2807:PostgreSQL 版セットアップSQLで、 ALTER TABLE ... ADD COLUMN 時にデフォルト値と NOT NULL 制約を追加してしまっている箇所があったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7688 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/postgres74/update/update15-for2.12beta1-drop_c_member_config_option.sql
+++ setup/sql/postgres74/update/update15-for2.12beta1-drop_c_member_config_option.sql
@@ -1,4 +1,7 @@
-ALTER TABLE c_member_config ADD COLUMN name varchar(64) NOT NULL default '';
+ALTER TABLE c_member_config ADD COLUMN name varchar(64);
+ALTER TABLE c_member_config ALTER COLUMN name SET DEFAULT '';
+UPDATE c_member_config SET name = '';
+ALTER TABLE c_member_config ALTER COLUMN name SET NOT NULL;
 
 UPDATE c_member_config SET name = c_member_config_option.name
     FROM c_member_config_option
