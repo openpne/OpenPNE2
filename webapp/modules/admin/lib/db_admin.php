9b2f1d6a5d8a14b475a5edbb35d64b2a1b95b461
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-07-18 07:14:29 +0000

    #2689:冗長・不要な記述を削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7752 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/lib/db_admin.php
+++ webapp/modules/admin/lib/db_admin.php
@@ -2458,48 +2458,25 @@ function monitor_topic_list4target_c_commu_topic_id($c_commu_topic_id, $page_siz
     return array($list , $prev , $next, $total_num, $total_page_num);
 }
 
-function monitor_new_topic_list($page_size, $page)
+function monitor_new_topic_list($limit)
 {
-    $page = intval($page);
-    $page_size = intval($page_size);
-    if ($wheres) { 
-        $where = ' WHERE ' . implode(' AND ', $wheres); 
-    } else { 
-        $where = ''; 
-    } 
+    $limit = intval($limit);
 
-    $select = 'SELECT c.name AS commu_name, c.image_filename AS commu_image, ct.*';
-    $from   = ' FROM c_commu AS c, c_commu_topic AS ct, c_commu_topic_comment AS ctc';
-    $params = array();
-    $where  = ' WHERE ct.c_commu_topic_id = ctc.c_commu_topic_id'
-            . ' AND c.c_commu_id = ct.c_commu_id';
+    $select = 'SELECT c.name AS commu_name, ct.*';
+    $from   = ' FROM c_commu AS c, c_commu_topic AS ct';
+    $where  = ' WHERE c.c_commu_id = ct.c_commu_id';
     $group  = ' GROUP BY ct.c_commu_topic_id';
     $order  = ' ORDER BY ct.u_datetime DESC';
 
     $sql = $select . $from . $where . $group . $order;
 
-    $list = db_get_all_limit($sql,($page-1)*$page_size,$page_size,$params);
+    $list = db_get_all_limit($sql, 0, $limit);
 
     foreach ($list as $key => $value) {
         $list[$key]['count_comments'] = _db_count_c_commu_topic_comments4c_commu_topic_id($value['c_commu_topic_id']);
-        $c_member = db_member_c_member4c_member_id_LIGHT($value['c_member_id']);
-        $list[$key]['nickname'] = $c_member['nickname'];
-        if (!empty($value['filename'])) {
-            $list[$key]['original_filename'] = db_file_original_filename4filename($value['filename']);
-        }
     }
 
-    $sql =
-        "SELECT COUNT(*) "
-        . $from
-        . $where ;
-    $total_num = db_get_one($sql, $params);
-
-    $total_page_num =  ceil($total_num / $page_size);
-    $next = ($page < $total_page_num);
-    $prev = ($page > 1);
-
-    return array($list , $prev , $next, $total_num, $total_page_num);
+    return $list;
 }
 
 function monitor_review_list($keyword, $page_size, $page)
