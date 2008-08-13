a6fa610cf2a8294b1c7a528eebf9f4c75b8550f0
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-13 06:00:51 +0000

    #2933:f系の携帯版アルバム関連アクションにおいてメンバーの存在チェックをおこなうようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@7995 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/fh_album_image_show.php
+++ webapp/modules/ktai/page/fh_album_image_show.php
@@ -18,27 +18,33 @@ class ktai_page_fh_album_image_show extends OpenPNE_Action
         }
 
         $u = $GLOBALS['AUTH']->uid();
-        
+
         // --- リクエスト変数
         $target_c_album_image_id = $requests['target_c_album_image_id'];
         // ----------
-        
+
         //targetのアルバム写真が存在しない
         if (!p_common_is_active_c_album_image_id($target_c_album_image_id)) {
             $this->handleError();
         }
-        
+
         $target_c_album_image = db_album_image_get_c_album_image4id($target_c_album_image_id);
         $target_c_album_id = $target_c_album_image['c_album_id'];
-        
+
         //targetのアルバムが存在しない
         if (!p_common_is_active_c_album_id($target_c_album_id)) {
             $this->handleError();
         }
-        
+
         $target_c_album = db_album_get_c_album4c_album_id($target_c_album_id);
         $target_c_member_id = $target_c_album['c_member_id']; 
-        
+        $target_c_member = db_member_c_member4c_member_id($target_c_member_id);
+
+        // メンバーが存在しない
+        if (!$target_c_member) {
+            $this->handleError();
+        }
+
         if ($target_c_member_id == $u) {
             $type = 'h';
         } else {
@@ -56,14 +62,14 @@ class ktai_page_fh_album_image_show extends OpenPNE_Action
             // あしあとをつける
             db_ashiato_insert_c_ashiato($target_c_member_id, $u);
         }
-        
+
         $this->set('type', $type);
         $this->set('c_album_image', $target_c_album_image);
         $this->set('c_album_image_id_prev', db_album_image_c_album_image_id_prev4c_album_id($target_c_album_id, $target_c_album_image_id));
         $this->set('c_album_image_id_next', db_album_image_c_album_image_id_next4c_album_id($target_c_album_id, $target_c_album_image_id));
 
         $this->set('album_info', $target_c_album);
-        $this->set('target_member', db_member_c_member4c_member_id($target_c_member_id));
+        $this->set('target_member', $target_c_member);
 
         return 'success';
     }
