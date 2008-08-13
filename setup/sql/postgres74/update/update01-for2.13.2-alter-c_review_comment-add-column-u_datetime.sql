6d1a71cf4f7b2747dcaf23b6a9ecd308e853977f
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-13 16:31:17 +0000

    #2840:PostgreSQL版update用SQLが正常に実行できなかったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8014 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/postgres74/update/update01-for2.13.2-alter-c_review_comment-add-column-u_datetime.sql
+++ setup/sql/postgres74/update/update01-for2.13.2-alter-c_review_comment-add-column-u_datetime.sql
@@ -1,2 +1,4 @@
-ALTER TABLE c_review_comment ADD COLUMN u_datetime datetime NOT NULL default '0000-00-00 00:00:00';
-
+ALTER TABLE c_review_comment ADD COLUMN u_datetime timestamp;
+ALTER TABLE c_review_comment ALTER COLUMN u_datetime SET DEFAULT '0000-01-01 00:00:00';
+UPDATE c_review_comment SET u_datetime = '0000-01-01 00:00:00';
+ALTER TABLE c_review_comment ALTER COLUMN u_datetime SET NOT NULL;
