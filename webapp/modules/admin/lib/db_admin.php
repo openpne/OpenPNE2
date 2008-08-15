f47f1339824bfdcafc053366f2e34ba43628fe50
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-15 02:14:48 +0000

    #2411:e_rankの到達ポイントが0未満の場合を考慮していなかったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8028 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/lib/db_admin.php
+++ webapp/modules/admin/lib/db_admin.php
@@ -694,7 +694,8 @@ function db_admin_c_member_id_list4cond_c_member($cond_list, $type = array())
  */
 function db_admin_c_member_id_list4cond_pne_point($ids, $cond_list)
 {
-    $is_contain_empty_point_member = false;
+    $s_point = 0;
+    $e_point = 0;
 
     $sql = 'SELECT c_member_id'.
            ' FROM c_member_profile '.
@@ -706,11 +707,6 @@ function db_admin_c_member_id_list4cond_pne_point($ids, $cond_list)
         $s_point = db_point_get_rank_point4rank_id($cond_list['s_rank']);
         $sql .= ' AND value >= ?';
         $params[] = (int)$s_point;
-
-        // s_point が 0 以下の場合は、c_member_profile に PNE_POINT が存在しないメンバーも結果に含める
-        if ($s_point <= 0) {
-            $is_contain_empty_point_member = true;
-        }
     }
 
     if ($cond_list['e_rank']) {
@@ -723,8 +719,8 @@ function db_admin_c_member_id_list4cond_pne_point($ids, $cond_list)
 
     $point_ids = db_get_col($sql, $params);
 
-    // c_member_profile に結果が存在しないメンバーを結果に含める
-    if ($is_contain_empty_point_member) {
+    // s_point が 0 以下であり、 e_point が 0 を超過する場合、 c_member_profile に PNE_POINT が存在しないメンバーも結果に含める
+    if ($s_point <= 0 && $e_point > 0) {
         $sql = 'SELECT c_member_id FROM c_member_profile '
              . ' INNER JOIN c_profile USING (c_profile_id) '
              . ' WHERE name = ? ';
