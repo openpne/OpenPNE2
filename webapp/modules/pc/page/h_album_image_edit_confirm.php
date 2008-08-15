22f8fee513e3d386c1702b0948093a5e018332e0
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-15 02:45:59 +0000

    #2847:配列として定義する必要のない $upfiles, $tmpfiles を削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8032 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/h_album_image_edit_confirm.php
+++ webapp/modules/pc/page/h_album_image_edit_confirm.php
@@ -40,23 +40,19 @@ class pc_page_h_album_image_edit_confirm extends OpenPNE_Action
             handle_kengen_error();
         }
 
-        $upfiles = array(
-            1 => $_FILES['upfile_1'],
-        );
-        $tmpfiles = array(
-            1 => '',
-        );
+        $upfile = $_FILES['upfile_1'];
+        $tmpfile = '';
 
-        if ($upfiles[1]['error'] !== UPLOAD_ERR_NO_FILE) {
-            if (!($image = t_check_image($upfiles[1]))) {
+        if ($upfile['error'] !== UPLOAD_ERR_NO_FILE) {
+            if (!($image = t_check_image($upfile))) {
                 $_REQUEST['msg'] = '写真は'.IMAGE_MAX_FILESIZE.'KB以内のGIF・JPEG・PNGにしてください';
                 openpne_forward('pc', 'page', 'h_album_image_edit');
                 exit;
             }
-            $tmpfiles[1] = t_image_save2tmp($upfiles[1], $sessid, "a_{$target_c_album_id}_1", $image['format']);
+            $tmpfile = t_image_save2tmp($upfile, $sessid, "a_{$target_c_album_id}_1", $image['format']);
 
             // 置き換えたときのファイルサイズを出すために、追加ファイルサイズから置き換わるファイルサイズを減算
-            $filesize = $upfiles[1]['size'] - $c_album_image['filesize'];
+            $filesize = $upfile['size'] - $c_album_image['filesize'];
         }
 
         if (!db_album_is_insertable4c_member_id($u, $filesize)) {
@@ -81,8 +77,8 @@ class pc_page_h_album_image_edit_confirm extends OpenPNE_Action
             "target_c_album_id" => $target_c_album_id,
             "target_c_album_image_id" => $target_c_album_image_id,
             "image_description" => $image_description,
-            "upfile_1" => $_FILES['upfile_1'],
-            "tmpfile_1" => $tmpfiles[1],
+            "upfile_1" => $upfile,
+            "tmpfile_1" => $tmpfile,
         );
 
         $this->set("form_val", $form_val);
