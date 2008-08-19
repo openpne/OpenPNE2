785681500c350da1b5be7be575aa87129a732f92
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-19 04:48:24 +0000

    #2411:不要な空白文字の削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8048 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/lib/db_admin.php
+++ webapp/modules/admin/lib/db_admin.php
@@ -697,10 +697,10 @@ function db_admin_c_member_id_list4cond_pne_point($ids, $cond_list)
     $s_point = 0;
     $e_point = 0;
 
-    $sql = 'SELECT c_member_id'.
-           ' FROM c_member_profile '.
-           ' INNER JOIN c_profile USING (c_profile_id) '.
-           ' WHERE name = ? ';
+    $sql = 'SELECT c_member_id'
+         . ' FROM c_member_profile'
+         . ' INNER JOIN c_profile USING (c_profile_id)'
+         . ' WHERE name = ?';
     $params = array('PNE_POINT');
 
     if ($cond_list['s_rank']) {
@@ -721,15 +721,14 @@ function db_admin_c_member_id_list4cond_pne_point($ids, $cond_list)
 
     // s_point が 0 以下であり、 e_point が 0 を超過する場合、 c_member_profile に PNE_POINT が存在しないメンバーも結果に含める
     if ($s_point <= 0 && $e_point > 0) {
-        $sql = 'SELECT c_member_id FROM c_member_profile '
-             . ' INNER JOIN c_profile USING (c_profile_id) '
-             . ' WHERE name = ? ';
+        $sql = 'SELECT c_member_id FROM c_member_profile'
+             . ' INNER JOIN c_profile USING (c_profile_id)'
+             . ' WHERE name = ?';
         $params = array('PNE_POINT');
         $have_point_ids = db_get_col($sql, $params);
 
         // この関数に渡されたメンバーIDの配列と、PNE_POINTが存在するすべてのメンバーIDの配列の差分を結果に追加する
         $point_ids = array_merge($point_ids, array_diff($ids, $have_point_ids));
-
     }
 
     return array_intersect($ids, $point_ids);
