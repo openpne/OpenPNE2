0fdb73d2cd8c5b2ad9b70f2508e9f80dbfe8a628
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-20 14:46:30 +0000

    #2866:不要な session_start(), session_regenerate_id() を削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@8086 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/do/o_easy_login.php
+++ webapp/modules/ktai/do/o_easy_login.php
@@ -26,8 +26,6 @@ class ktai_do_o_easy_login extends OpenPNE_Action
         $c_member = db_member_c_member4c_member_id($c_member_id, true);
 
         @session_name('OpenPNEktai');
-        @session_start();
-        @session_regenerate_id();
 
         $config = get_auth_config(true);
         $auth = new OpenPNE_Auth($config);
