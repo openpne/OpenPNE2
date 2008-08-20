37064dabeff14a4092cbd35c4a9251ab31a230b7
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-20 05:43:51 +0000

    #2866:ブラックリストに登録されているメンバーがログインを試みた際に最終ログイン時間が更新されていたのを修正し、ログイン停止状態にあるメンバーとブラックリストに登録されているメンバーがログインもしくは認証をおこなった場合の処理を統一した
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@8066 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/do/o_login.php
+++ webapp/modules/ktai/do/o_login.php
@@ -61,9 +61,12 @@ class ktai_do_o_login extends OpenPNE_Action
             $p = array('msg' => '0', 'kad' => t_encrypt($ktai_address), 'login_params' => $requests['login_params']);
             openpne_redirect('ktai', 'page_o_login', $p);
         }
+
         if (db_member_is_login_rejected($c_member_id)) {
-            $p = array('msg' => '0', 'kad' => t_encrypt($ktai_address), 'login_params' => $requests['login_params']);
-            openpne_redirect('ktai', 'page_o_login', $p);
+            ktai_display_error('ﾛｸﾞｲﾝできませんでした。');
+        }
+        if (db_member_is_blacklist($c_member_id)) {
+            ktai_display_error('ﾛｸﾞｲﾝできませんでした。');
         }
 
         db_member_do_access($c_member_id);
