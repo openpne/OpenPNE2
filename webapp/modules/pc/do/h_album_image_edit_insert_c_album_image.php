de4628fa8d4774e740e6b3678a985b64faeb0786
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-08-20 12:17:41 +0000

    #2847:容量制限チェックのために画像を挿入し、容量制限に引っかかった場合にその画像を削除する形に修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@8075 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_album_image_edit_insert_c_album_image.php
+++ webapp/modules/pc/do/h_album_image_edit_insert_c_album_image.php
@@ -43,9 +43,15 @@ class pc_do_h_album_image_edit_insert_c_album_image extends OpenPNE_Action
 
         // アルバム写真登録処理
         if ($tmpfile) {
+            if (!list($filename, $filesize) = image_insert_c_image_album4tmp("a_{$target_c_album_id}_1", $tmpfile)) {
+                $this->handleError(array('写真が登録できませんでした'));
+            }
+
+            $sessid = session_id();
+            t_image_clear_tmp($sessid);
+
             if (!db_album_is_insertable4c_member_id($u, $filesize - $c_album_image['filesize'])) {
-                $sessid = session_id();
-                t_image_clear_tmp($sessid);
+                db_album_image_data_delete($filename);
 
                 $msg = 'これ以上写真を投稿することができません。';
                 if (!db_album_is_insertable4c_member_id($u)) {
@@ -62,12 +68,6 @@ class pc_do_h_album_image_edit_insert_c_album_image extends OpenPNE_Action
             }
 
             db_album_image_data_delete($c_album_image['image_filename']);
-            if (!list($filename, $filesize) = image_insert_c_image_album4tmp("a_{$target_c_album_id}_1", $tmpfile)) {
-                $this->handleError(array('写真が登録できませんでした'));
-            }
-
-            $sessid = session_id();
-            t_image_clear_tmp($sessid);
         }
 
         db_album_update_c_album_image($target_c_album_image_id, $filename, $image_description, $filesize);
