d5d3ab3b7a6de91977423bbddd79fa5fc36e82fe
kiwa <kiwa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-09-17 11:15:35 +0000

    #3009:DoCoMoIPアドレス追加対応(2008年10月10日)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8366 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/ktaiIP.php
+++ webapp/lib/ktaiIP.php
@@ -9,6 +9,8 @@ $GLOBALS['_OPENPNE_KTAI_IP_LIST'] = array(
 '210.153.84.0/24',
 '210.136.161.0/24',
 '210.153.86.0/24',
+'124.146.174.0/24',
+'124.146.175.0/24',
 
 // au (2008/07/10更新)
 // http://www.au.kddi.com/ezfactory/tec/spec/ezsava_ip.html
