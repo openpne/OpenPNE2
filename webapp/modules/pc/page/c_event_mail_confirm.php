e85a97deb6c4f64750b206ac07631c46a4d6f68f
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-09-21 13:51:55 +0000

    #3005:コミュニティ管理者がイベント一括メッセージを送れるように修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@8418 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/c_event_mail_confirm.php
+++ webapp/modules/pc/page/c_event_mail_confirm.php
@@ -29,7 +29,8 @@ class pc_page_c_event_mail_confirm extends OpenPNE_Action
         if (!db_commu_is_c_commu_view4c_commu_idAc_member_id($c_commu_id, $u)) {
             handle_kengen_error();
         }
-        if (!db_commu_is_c_event_admin($c_commu_topic_id, $u)) {
+        if (!(db_commu_is_c_event_admin($c_commu_topic_id, $u) ||
+            db_commu_is_c_commu_admin($c_commu_id, $u))) {
             handle_kengen_error();
         }
         if (in_array($u, $c_member_ids)) {
