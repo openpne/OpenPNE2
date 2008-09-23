542b77904fd8779788a3a559b02d0fd54b1b6b84
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-09-23 17:30:57 +0000

    #2769:simplepie.inc で定義されている各クラスを、それぞれ単体のファイルで定義するよう分割
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8436 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ lib/include/SimplePie/Category.php
@@ -0,0 +1,58 @@
+<?php
+
+class SimplePie_Category
+{
+	var $term;
+	var $scheme;
+	var $label;
+
+	// Constructor, used to input the data
+	function SimplePie_Category($term = null, $scheme = null, $label = null)
+	{
+		$this->term = $term;
+		$this->scheme = $scheme;
+		$this->label = $label;
+	}
+
+	function __toString()
+	{
+		// There is no $this->data here
+		return md5(serialize($this));
+	}
+
+	function get_term()
+	{
+		if ($this->term !== null)
+		{
+			return $this->term;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	function get_scheme()
+	{
+		if ($this->scheme !== null)
+		{
+			return $this->scheme;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	function get_label()
+	{
+		if ($this->label !== null)
+		{
+			return $this->label;
+		}
+		else
+		{
+			return $this->get_term();
+		}
+	}
+}
