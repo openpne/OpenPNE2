75ae0701592adf1b894be82844a352896563aec3
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-09-25 05:16:49 +0000

    #2991:OPENPNE_REGIST_FROM_PC による制限を削除（追加修正）
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8458 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/o_regist_pc_address.php
+++ webapp/modules/pc/page/o_regist_pc_address.php
@@ -13,7 +13,7 @@ class pc_page_o_regist_pc_address extends OpenPNE_Action
 
     function execute($requests)
     {
-        if (OPENPNE_AUTH_MODE == 'slavepne' || !(OPENPNE_REGIST_FROM & OPENPNE_REGIST_FROM_PC)) {
+        if (OPENPNE_AUTH_MODE == 'slavepne') {
             client_redirect_login();
         }
 
