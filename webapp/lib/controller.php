c2d768291e1ae5ac5ef2c1173cb0ce7630c87b06
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-06 10:20:55 +0000

    #2842:ファイルダウンロード時には常にキャッシュをさせないヘッダを出力するように修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@8578 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/controller.php
+++ webapp/lib/controller.php
@@ -348,11 +348,11 @@ function _check_action($action)
     return $action;
 }
 
-function send_nocache_headers()
+function send_nocache_headers($is_force = false)
 {
     if (!headers_sent()) {
         if ((!empty($GLOBALS['__Framework']['carrier']) && $GLOBALS['__Framework']['carrier'] === 'e')  // au の場合は常に no-cache ヘッダを送信
-            || OPENPNE_SEND_NO_CACHE_HEADER) {
+            || OPENPNE_SEND_NO_CACHE_HEADER || $is_force) {
             // no-cache
             // 日付が過去
             header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
