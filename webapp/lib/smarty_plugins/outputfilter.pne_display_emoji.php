ed2aa71abb783541ecd98cbf6e388ec0e25093a6
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-08 14:34:40 +0000

    #2768:entry_point の場合のみ、t_url2cmd 実行時に textarea など「小窓に変換するべきでない箇所」であるかどうかを考慮するようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8619 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/smarty_plugins/outputfilter.pne_display_emoji.php
+++ webapp/lib/smarty_plugins/outputfilter.pne_display_emoji.php
@@ -12,47 +12,19 @@ function smarty_outputfilter_pne_display_emoji($tpl_output, &$smarty)
     $list = array();
 
     if (empty($GLOBALS['__Framework']['carrier'])) {
-        // input, textarea, option, img, head を退避
-        $patterns = array(
-            '/<input[^>]+>/is',
-            '/<textarea.*?<\/textarea>/is',
-            '/<option.*?<\/option>/is',
-            '/<img[^>]+>/is',
-            '/<head.*?<\/head>/is',
-        );
-
-        list ($list, $tpl_output) = _smarty_outputfilter_pne_display_emoji_replace($patterns, $tpl_output);
+        // 置換用に文字列を退避
+        list ($list, $tpl_output) = util_replace_patterns_to_marker($tpl_output);
     }
 
     // 絵文字変換
     $tpl_output = emoji_convert($tpl_output);
 
     if (empty($GLOBALS['__Framework']['carrier'])) {
-        // input, textarea, option, img, head を元に戻す
+        // 退避した文字列を元に戻す
         $tpl_output = str_replace(array_keys($list), array_values($list), $tpl_output);
     }
 
     return $tpl_output;
-}
-
-function _smarty_outputfilter_pne_display_emoji_replace($patterns, $subject)
-{
-    $i = 0;
-    $list = array();
-
-    foreach ($patterns as $pattern) {
-        if (preg_match_all($pattern, $subject, $matches)) {
-            foreach ($matches[0] as $match) {
-                $replacement = '<<<NOEMOJI:'.$i.'>>>';
-                $list[$replacement] = $match;
-                $i++;
-            }
-        }
-    }
-
-    $subject = str_replace(array_values($list), array_keys($list), $subject);
-
-    return array($list, $subject);
 }
 
 ?>
