53a7474662b758b10104f0cac317b0ee895ec053
ebihara <ebihara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-09 04:01:06 +0000

    #2768:list() のコール部分で、括弧の直前にスペースが入っているのを削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@8626 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/smarty_plugins/outputfilter.pne_display_emoji.php
+++ webapp/lib/smarty_plugins/outputfilter.pne_display_emoji.php
@@ -9,7 +9,7 @@ function smarty_outputfilter_pne_display_emoji($tpl_output, &$smarty)
 
     if (empty($GLOBALS['__Framework']['carrier'])) {
         // 置換用に文字列を退避
-        list ($list, $tpl_output) = util_replace_patterns_to_marker($tpl_output);
+        list($list, $tpl_output) = util_replace_patterns_to_marker($tpl_output);
     }
 
     // 絵文字変換
