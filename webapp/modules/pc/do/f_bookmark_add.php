fd54a8bc46554b854d05f3439e905ae4c5e31944
mori <mori@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-20 05:48:59 +0000

    #3056 :お気に入り追加時の入力チェックを再修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8718 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/f_bookmark_add.php
+++ webapp/modules/pc/do/f_bookmark_add.php
@@ -6,6 +6,14 @@
 
 class pc_do_f_bookmark_add extends OpenPNE_Action
 {
+
+    function handleError($errors)
+    {
+        $_REQUEST['msg'] = $errors[c_member_id];
+        openpne_forward('pc', 'page', 'h_bookmark_list');
+        exit;
+    }
+
     function execute($requests)
     {
         $u = $GLOBALS['AUTH']->uid();
@@ -14,13 +22,6 @@ class pc_do_f_bookmark_add extends OpenPNE_Action
         $c_member_id_to = $requests['c_member_id'];
         // ----------
 
-        //--- 入力チェック（負数を認めない）
-        if ($c_member_id_to <= 0) {
-            $_REQUEST['msg'] = 'お気に入り追加メンバーのIDが不正です。';
-            openpne_forward('pc', 'page', 'h_bookmark_list');
-            exit;
-        }
-
         //--- 権限チェック
         //自分以外 and ブックマーク登録されていない
 
@@ -28,6 +29,14 @@ class pc_do_f_bookmark_add extends OpenPNE_Action
             handle_kengen_error();
         }
 
+        //アクティブメンバーか
+        if (!db_member_is_active_c_member_id($c_member_id_to)) {
+            $_REQUEST['msg'] = '存在しないメンバーIDです。';
+            openpne_forward('pc', 'page', 'h_bookmark_list');
+            exit;
+        }
+
+        //お気に入り登録済みメンバーか
         if (db_bookmark_is_bookmark($u, $c_member_id_to)) {
             $_REQUEST['msg'] = '既にお気に入りに登録済みです。';
             openpne_forward('pc', 'page', 'h_bookmark_list');
