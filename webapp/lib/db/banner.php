afbcd2abfe37fefe94bf0d9a602089deff706607
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-22 03:17:45 +0000

    変数名をわかりやすい表現に変更・不要な空白を削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8738 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/db/banner.php
+++ webapp/lib/db/banner.php
@@ -63,7 +63,7 @@ function db_banner_is_after_auth_banner($c_banner_id)
 {
     $sql = 'SELECT is_hidden_before, is_hidden_after FROM c_banner WHERE c_banner_id = ?';
     $params = array($c_banner_id);
-    $c_banner =  db_get_row($sql, $params);
+    $c_banner = db_get_row($sql, $params);
     if ($c_banner['is_hidden_before'] && !$c_banner['is_hidden_after']) {
         return true;
     } else {
