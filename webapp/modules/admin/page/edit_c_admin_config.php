26646d9e5dbbc52ee0106797c64e1c777d2d5aa0
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-10-30 11:03:06 +0000

    #1867 各外部ログインページのurlの生成方法を修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@8893 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/edit_c_admin_config.php
+++ webapp/modules/admin/page/edit_c_admin_config.php
@@ -12,6 +12,18 @@ class admin_page_edit_c_admin_config extends OpenPNE_Action
         $v = array();
         $v['DAILY_NEWS_DAY'] = explode(',', DAILY_NEWS_DAY);
         $this->set($v);
+
+	//外部ログインフォーム用HTML内のアドレス生成
+        //PC版ログインフォーム 
+        $absolute = true ;
+        $login_url = openpne_gen_url_head('pc', 'do_o_login', $absolute);
+        $this->set('login_url', $login_url);
+
+        //携帯版ログインフォーム
+        $ktai_login_url = openpne_gen_url_head('ktai', 'do_o_login', $absolute);
+        $ktai_easy_login_url = openpne_gen_url_head('ktai', 'do_o_easy_login', $absolute);
+        $this->set('ktai_login_url', $ktai_login_url);
+        $this->set('ktai_easy_login_url', $ktai_easy_login_url);
         return 'success';
     }
 }
