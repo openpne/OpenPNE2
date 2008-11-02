822e649ae729def45f7399390896147eb2ac5b97
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-02 20:38:52 +0000

    #3107 h_confirm_list5.での処理が出来たので一旦コミット
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@8928 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/h_confirm_list.php
+++ webapp/modules/ktai/page/h_confirm_list.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright 2005-2007 OpenPNE Project
+ * @copyright 2005-2008 OpenPNE Project
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 
@@ -10,34 +10,53 @@ class ktai_page_h_confirm_list extends OpenPNE_Action
     {
         $u  = $GLOBALS['KTAI_C_MEMBER_ID'];
 
-        //リンク承認待ちリスト
-        $this->set("anatani_c_friend_confirm_list", db_friend_ktai_anatani_c_friend_confirm_list4c_member_id($u));
+        //あなたにフレンド認証を求めいるメンバーリスト
+        $from_f_confirm_list = db_friend_anatani_c_friend_confirm_list4c_member_id($u);
+        $this->set("from_f_confirm_list", $from_f_confirm_list);
+        //そのメンバーの人数
+        $this->set("num_from_f_confirm_list", count($from_f_confirm_list));
 
-        //コミュニティ参加承認待ちリスト
-        $this->set("anatani_c_commu_member_confirm_list", db_commu_anatani_c_commu_member_confirm_list4c_member_id($u));
+        //コミュニティ承認を求めているメンバーリスト
+        $from_h_confirm_list = db_commu_anatani_c_commu_member_confirm_list4c_member_id($u);
+        $this->set("from_h_confirm_list", $from_h_confirm_list);
+        //そのメンバーの人数
+        $this->set("num_from_h_confirm_list", count($from_h_confirm_list));
 
-        // あなたにコミュニティ管理者交代を希望しているメンバー
-        $this->set("anatani_c_commu_admin_confirm_list",
-            p_h_confirm_list_anatani_c_commu_admin_confirm_list4c_member_id($u));
+        //あなたにコミュニティ管理者交代を希望しているメンバー
+        $from_c_commu_admin_confirm_list = db_commu_anatani_c_commu_admin_confirm_list4c_member_id($u);
+        $this->set("from_c_commu_admin_confirm_list", $from_c_commu_admin_confirm_list);
+        //そのメンバーの人数
+        $this->set("num_from_c_commu_admin_confirm_list", count($from_c_commu_admin_confirm_list));
 
-        // あなたにコミュニティ副管理者を希望しているメンバー
-        $this->set("anatani_c_commu_sub_admin_confirm_list",
-            db_commu_anatani_c_commu_sub_admin_confirm_list4c_member_id($u));
+        //あなたにコミュニティ副管理者を希望しているメンバー
+        $from_c_commu_sub_admin_confirm_list = db_commu_anatani_c_commu_sub_admin_confirm_list4c_member_id($u);
+        $this->set("from_c_commu_sub_admin_confirm_list", $from_c_commu_sub_admin_confirm_list);
+        //そのメンバーの人数
+        $this->set("num_from_c_commu_sub_admin_confirm_list", count($from_c_commu_sub_admin_confirm_list));
 
         //リンク申請出した人のリスト
-        $this->set("anataga_c_friend_confirm_list", db_friend_ktai_anataga_c_friend_confirm_list4c_member_id($u));
+        $to_f_confirm_list = db_friend_ktai_anataga_c_friend_confirm_list4c_member_id($u);
+        $this->set("to_f_confirm_list", $to_f_confirm_list);
+        //そのメンバーの人数
+        $this->set("num_to_f_confirm_list", count($to_f_confirm_list));
 
         //参加申請出したコミュニティに関するリスト
-        $this->set("anataga_c_commu_member_confirm_list", db_commu_anataga_c_commu_member_confirm_list4c_member_id($u));
+        $to_h_confirm_list = db_commu_anataga_c_commu_member_confirm_list4c_member_id($u);
+        $this->set("to_h_confirm_list", $to_h_confirm_list);
+        //そのメンバーの人数
+        $this->set("num_to_h_confirm_list", count($to_h_confirm_list));
 
-        // あなたがコミュニティ管理者交代を要請しているメンバー
-        $this->set("anataga_c_commu_admin_confirm_list",
-            p_h_confirm_list_anataga_c_commu_admin_confirm_list4c_member_id($u));
+        //あなたがコミュニティ管理者交代を要請しているメンバー
+        $to_c_commu_admin_confirm_list = db_commu_anataga_c_commu_admin_confirm_list4c_member_id($u);
+        $this->set("to_c_commu_admin_confirm_list", $to_c_commu_admin_confirm_list);
+        //そのメンバーの人数
+        $this->set("num_to_c_commu_admin_confirm_list", count($to_c_commu_admin_confirm_list));
 
-        // あなたがコミュニティ副管理者を要請しているメンバー
-        $this->set("anataga_c_commu_sub_admin_confirm_list",
-            db_commu_anataga_c_commu_sub_admin_confirm_list4c_member_id($u));
-
+        //あなたがコミュニティ副管理者を要請しているメンバー
+        $to_c_commu_sub_admin_confirm_list = db_commu_anataga_c_commu_sub_admin_confirm_list4c_member_id($u);
+        $this->set("to_c_commu_sub_admin_confirm_list", $to_c_commu_sub_admin_confirm_list);
+        //そのメンバーの人数
+        $this->set("num_to_c_commu_sub_admin_confirm_list", count($to_c_commu_sub_admin_confirm_list));
 
         return 'success';
     }
--- webapp/modules/ktai/templates/h_confirm_list5.tpl
+++ webapp/modules/ktai/templates/h_confirm_list5.tpl
@@ -1,39 +1,49 @@
 ({$inc_ktai_header|smarty:nodefaults})
-<table width="100%">
-<tr><td align="center" bgcolor="#({$ktai_color_config.bg_02})">
-<font color="#({$ktai_color_config.font_05})"><a name="top">承認待ちﾘｽﾄ</a></font><br>
+
+<table width="100%"><tr><td align="center" bgcolor="#({$ktai_color_config.bg_02})">
+<font color="#({$ktai_color_config.font_05})"><a name="top">({$target_c_member.nickname})</a></font><br>
+</td></tr>
+<tr><td bgcolor="#({$ktai_color_config.bg_03})" align="center">
+<font color="#({$ktai_color_config.color_24})">承認待ちﾘｽﾄ(({$total_num}))</font><br>
 </td></tr></table>
 
-({if $msg})
-<font color="#({$ktai_color_config.font_09})">({$msg})</font><br>
-({/if})
+({if !$c_friend_confirm_list})
+({$WORD_FRIEND_HALF})ﾘﾝｸ要請中はありません。
+({else})
+<center>
+({$pager.start})～({$pager.end})件目を表示<br>
+</center>
 
+<hr color="#({$ktai_color_config.border_02})">
 <table width="100%">
-
-<tr><td bgcolor="#({$ktai_color_config.bg_08})">
-<font color="#({$ktai_color_config.color_26})">({$WORD_FRIEND_HALF})ﾘﾝｸ要請中</font><br>
-</td></tr>
-
-({if $anataga_c_friend_confirm_list})
-({foreach from=$anataga_c_friend_confirm_list item=anataga_c_friend_confirm name=anataga_c_friend_confirm_list_name})
-<tr><td bgcolor="#({$ktai_color_config.bg_09})">
+({foreach from=$c_friend_confirm_list item=c_friend_confirm name=c_friend_confirm_list_name})
+<tr><td bgcolor="#({cycle values="`$ktai_color_config.bg_06`,`$ktai_color_config.bg_07`"})">
 <font color="#({$ktai_color_config.font_06})">({$WORD_NICKNAME_HALF})：</font><br>
-<a href="({t_url m=ktai a=page_f_home})&amp;target_c_member_id=({$anataga_c_friend_confirm.c_member_id_to})&amp;({$tail})">({$anataga_c_friend_confirm.nickname})</a><br>
+<a href="({t_url m=ktai a=page_f_home})&amp;target_c_member_id=({$c_friend_confirm.c_member_id_to})&amp;({$tail})">({$c_friend_confirm.nickname})</a><br>
 <br>
-({if $anataga_c_friend_confirm.message})
+({if $c_friend_confirm.message})
 <font color="#({$ktai_color_config.font_06})">ﾒｯｾｰｼﾞ：</font><br>
-({$anataga_c_friend_confirm.message|nl2br})<br>
+({$c_friend_confirm.message|nl2br})<br>
 ({/if})
 <center>
-[<a href="({t_url m=ktai a=do_h_confirm_list_delete_c_friend_confirm})&amp;target_c_friend_confirm_id=({$anataga_c_friend_confirm.c_friend_confirm_id})&amp;({$tail})">依頼削除</a>]<br>
+[<a href="({t_url m=ktai a=do_h_confirm_list_delete_c_friend_confirm})&amp;target_c_friend_confirm_id=({$c_friend_confirm.c_friend_confirm_id})&amp;({$tail})">依頼削除</a>]<br>
 </center>
 </td></tr>
-<tr><td bgcolor="#({$ktai_color_config.bg_10})">
-<hr color="#({$ktai_color_config.border_03})">
+<tr><td bgcolor="#({$ktai_color_config.bg_07})">
+<hr color="#({$ktai_color_config.border_02})">
 </td></tr>
 ({/foreach})
 ({/if})
 </table>
+
+({if $is_prev || $is_next})
+<center>
+({if $is_prev})<a href="({t_url m=ktai a=page_h_confirm_list5})&amp;target_c_member_id=({$target_c_member.c_member_id})&amp;page=({$page-1})&amp;({$tail})" accesskey="4">[i:128]前を表示</a>({/if})
+({if $is_prev && $is_next})&nbsp;({/if})
+({if $is_next})<a href="({t_url m=ktai a=page_h_confirm_list5})&amp;target_c_member_id=({$target_c_member.c_member_id})&amp;page=({$page+1})&amp;({$tail})" accesskey="6">[i:130]次を表示</a>({/if})
 <br>
+</center>
+({/if})
+<hr color="#({$ktai_color_config.border_01})">
 
 ({$inc_ktai_footer|smarty:nodefaults})
