416a196004d9665e30dbde3a7922e70d6b968863
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-05 10:18:44 +0000

    #3060:docomo端末の簡単ログイン時のユーザエージェント情報が異なる為、ログアウトされるバグに対応
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8980 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/OpenPNE/Auth.php
+++ webapp/lib/OpenPNE/Auth.php
@@ -306,8 +306,21 @@ class OpenPNE_Auth
             }
         }
         if ($this->is_check_user_agent) {
-            $user_agent = $this->auth->getAuthData('USER_AGENT');
-            if ($user_agent !== $_SERVER['HTTP_USER_AGENT']) {
+            $login_user_agent = $this->auth->getAuthData('USER_AGENT');
+            $now_user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+            // ドコモ端末からのアクセスかどうか
+            if (strpos($now_user_agent, 'DoCoMo') >= 0) {
+                $is_docomo = true;
+            } else {
+                $is_docomo = false;
+            }
+            if ($is_docomo) {
+                $login_user_agent = substr($login_user_agent, 0, strpos($login_user_agent, '('));
+                $now_user_agent = substr($now_user_agent, 0, strpos($now_user_agent, '('));
+            }
+
+            if ($now_user_agent !== $login_user_agent) {
                 return false;
             }
         }
