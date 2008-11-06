80bce9e26d5a45d3c6d31fe748de91eeec8ca41d
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-06 05:41:27 +0000

    #1867 edit_c_admin_config.tplとedit_c_admin_config.phpを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@9001 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/edit_c_admin_config.php
+++ webapp/modules/admin/page/edit_c_admin_config.php
@@ -15,13 +15,12 @@ class admin_page_edit_c_admin_config extends OpenPNE_Action
 
         //外部ログインフォーム用HTML内のアドレス生成
         //PC版ログインフォーム
-        $absolute = true;
-        $login_url = openpne_gen_url_head('pc', 'do_o_login', $absolute);
+        $login_url = openpne_gen_url_head('pc', 'do_o_login');
         $this->set('login_url', $login_url);
 
         //携帯版ログインフォーム
-        $ktai_login_url = openpne_gen_url_head('ktai', 'do_o_login', $absolute);
-        $ktai_easy_login_url = openpne_gen_url_head('ktai', 'do_o_easy_login', $absolute);
+        $ktai_login_url = openpne_gen_url_head('ktai', 'do_o_login');
+        $ktai_easy_login_url = openpne_gen_url_head('ktai', 'do_o_easy_login');
         $this->set('ktai_login_url', $ktai_login_url);
         $this->set('ktai_easy_login_url', $ktai_easy_login_url);
         return 'success';
