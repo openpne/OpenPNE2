c296c5e785e91329473d7ed5dbb5812735010f04
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-06 04:41:58 +0000

    #3086 db_friend_c_friend_list4c_member_id2を修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@8994 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/db/friend.php
+++ webapp/lib/db/friend.php
@@ -445,7 +445,8 @@ function db_friend_c_friend_list4c_member_id2($c_member_id)
 {
     $sql = "SELECT c_member.* FROM c_friend, c_member" .
         " WHERE c_friend.c_member_id_from = ?".
-        " AND c_member.c_member_id=c_friend.c_member_id_to";
+        " AND c_member.c_member_id=c_friend.c_member_id_to".
+        " ORDER BY r_date ASC";
     $params = array(intval($c_member_id));
     return db_get_all($sql, $params);
 }
