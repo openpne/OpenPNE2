5334085f6a740fd9ff80b241ce3eb5dec44fc4b8
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-11 10:51:31 +0000

    #3135:コミュニティ編集画面にて重複したコミュニティ名を登録できないように修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@9078 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/c_edit_update_c_commu.php
+++ webapp/modules/pc/do/c_edit_update_c_commu.php
@@ -38,6 +38,10 @@ class pc_do_c_edit_update_c_commu extends OpenPNE_Action
         if (!$name) $err_msg[] = WORD_COMMUNITY . "名を入力してください";
         if (!$info) $err_msg[] = WORD_COMMUNITY . "の説明を入力してください";
 
+        if (db_commu_is_commu4c_commu_name($name)) {
+            $err_msg[] = 'その' . WORD_COMMUNITY . 'はすでに存在します';
+        }
+
         if (!db_commu_c_commu_category_is_create_flag($c_commu_category_id)) {
             $err_msg[] = 'そのカテゴリに新規に' . WORD_COMMUNITY . 'を作ることはできません';
         }
