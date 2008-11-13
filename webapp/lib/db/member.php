933effaaf8a7ac5126c15ad07f8d34142b4d442e
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-13 08:49:11 +0000

    #2292 データベースのc_member_profileへの書き込み方法を修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@9131 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/db/member.php
+++ webapp/lib/db/member.php
@@ -1639,14 +1639,15 @@ function db_member_update_c_member_profile($c_member_id, $c_member_profile_list)
         $params = array(intval($c_member_id), intval($item['c_profile_id']));
         db_query($sql, $params);
 
-        if (!(is_null($item['value']) || $item['value'] === '')) {
-            if (is_array($item['value'])) {
-                foreach ($item['value'] as $key => $value) {
-                    db_member_insert_c_member_profile($c_member_id, $item['c_profile_id'], $key, $value, $item['public_flag']);
-                }
-            } else {
-                db_member_insert_c_member_profile($c_member_id, $item['c_profile_id'], $item['c_profile_option_id'], $item['value'], $item['public_flag']);
+        if (is_null($item['value'])) {
+            $item['value'] = "";
+        }
+        if (is_array($item['value'])) {
+            foreach ($item['value'] as $key => $value) {
+                db_member_insert_c_member_profile($c_member_id, $item['c_profile_id'], $key, $value, $item['public_flag']);
             }
+        } else {
+            db_member_insert_c_member_profile($c_member_id, $item['c_profile_id'], $item['c_profile_option_id'], $item['value'], $item['public_flag']);
         }
     }
 }
