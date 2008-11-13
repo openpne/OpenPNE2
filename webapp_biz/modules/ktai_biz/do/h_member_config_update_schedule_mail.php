ddf54a7f265d67bcf3808c82ed529163faf9618b
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-13 17:17:40 +0000

    #2151 PEAR標準コーティング規約通りに修正とPCに設定が反映されないのを修正と一部変数名を小文字に修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@9141 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp_biz/modules/ktai_biz/do/h_member_config_update_schedule_mail.php
+++ webapp_biz/modules/ktai_biz/do/h_member_config_update_schedule_mail.php
@@ -14,7 +14,7 @@ class ktai_biz_do_h_member_config_update_schedule_mail extends OpenPNE_Action
         $tail = $GLOBALS['KTAI_URL_TAIL'];
         $u = $GLOBALS['KTAI_C_MEMBER_ID'];
 
-        // ランクアップメール通知
+        // スケジュールお知らせメール通知
 
         db_member_update_c_member_config($u, 'SEND_SCHEDULE_MAIL_KTAI', $requests['SEND_SCHEDULE_MAIL_KTAI']);
 
