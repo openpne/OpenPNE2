13f91cac85259865964060e4931ec3426f680ae6
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-17 16:09:26 +0000

    #3071 食べログ用JSを再度変更(APIの判定を修正)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@9205 8bbc783d-d51a-0410-adab-8fa69d43f207

--- public_html/cmd/r.tabelog.com.js
+++ public_html/cmd/r.tabelog.com.js
@@ -1,17 +1,37 @@
-function url2cmd(url) {
-    if (!url.match(/^http:\/\/r\.tabelog\.com\/[a-z]+\/A[0-9]+\/A[0-9]+\/([0-9]+)\/?$/)) {
-        pne_url2a(url);
-        return;
+function url2cmd(url)
+{
+    var rcd;
+    var tag;
+    var match_rcd = url.match(/[a-z]+\/A[0-9]+\/A[0-9]+\/([0-9]+)\/?/);
+    if (match_rcd) {
+        rcd = RegExp.$1;
     }
-    var rcd = RegExp.$1;
-    main(rcd);
+
+    var match_tag = url.match(/([a-zA-Z0-9_\-]+)/);
+    if (match_tag) {
+        tag = RegExp.$1;
+    } else {
+        tag = '';
+    }
+
+    if (rcd) {
+        main(rcd, tag);
+    } else {
+        pne_url2a(url);
+    }
 }
 
-function main(rcd) {
-    if (!rcd.match(/^[0-9]+$/)) {
-        return;
+function main(rcd, tag)
+{
+    var url = 'http://api.tabelog.com/RstPNEParts/?RestaurantCD=' + rcd;
+    if (tag) {
+        url += '' + tag;
     }
-    document.write('<iframe WIDTH="420" HEIGHT="280" MARGINWIDTH="0" MARGINHEIGHT="0" HSPACE="0" VSPACE="0" FRAMEBORDER="0" SCROLLING="no" BORDERCOLOR="#000000" src="http://api.tabelog.com/RstPNEParts/?RestaurantCD=' + rcd + '">');
-    document.write('</iframe>');
+
+    var html = ''
+    + '<iframe WIDTH="420" HEIGHT="280" MARGINWIDTH="0" MARGINHEIGHT="0" HSPACE="0" VSPACE="0" FRAMEBORDER="0" SCROLLING="no" BORDERCOLOR="#000000" src="' + url + '">'
+    + '</iframe>';
+
+    document.write(html);
 }
 
