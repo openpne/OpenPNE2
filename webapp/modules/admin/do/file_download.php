c0800cbc3ed1cbc3e6188770528693b9bea6ae8a
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-18 07:51:21 +0000

    #3113 ヘッダの追加条件をIEのみに変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.10.x@9224 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/file_download.php
+++ webapp/modules/admin/do/file_download.php
@@ -44,8 +44,10 @@ class admin_do_file_download extends OpenPNE_Action
         header('Content-Disposition: attachment; filename="' . $original_filename . '"');
         header('Content-Length: '. strlen($file['bin']));
         header('Content-Type: application/octet-stream');
-        header('Cache-Control: public');
-        header('Pragma: public');
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
+            header("Cache-Control: public");
+            header("Pragma: public");
+        }
         echo $file['bin'];
         exit;
     }
