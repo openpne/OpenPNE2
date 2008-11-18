f4eb96ad7f971b67e5928948dd4ee4c0ddba390b
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-18 07:44:26 +0000

    #3113 ヘッダの追加条件をIEのみに変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@9222 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/download_xml.php
+++ webapp/modules/admin/do/download_xml.php
@@ -72,8 +72,10 @@ class admin_do_download_xml extends OpenPNE_Action
             send_nocache_headers(true);
             header("Content-Type: application/octet-stream");
             header("Content-Disposition: attachment; filename=download.rdf");
-            header("Cache-Control: public");
-            header("Pragma: public");
+            if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
+                header("Cache-Control: public");
+                header("Pragma: public");
+            }
             echo $xml;
             //echo "<pre>".htmlspecialchars($xml)."</pre>";
         }
