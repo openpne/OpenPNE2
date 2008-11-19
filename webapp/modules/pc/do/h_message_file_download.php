29e807ca9f5a9ce8ddc7a7bef7a16b0cdf83486e
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-19 05:06:53 +0000

    # IEの場合キャッシュをさせないヘッダを出力しないよう修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@9250 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_message_file_download.php
+++ webapp/modules/pc/do/h_message_file_download.php
@@ -40,19 +40,20 @@ class pc_do_h_message_file_download extends OpenPNE_Action
         // オリジナルファイル名
         $original_filename = $file['original_filename'];
         if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
-            // IE の場合のみ、ファイル名を SJIS に変換
+            // IE の場合のみ、ファイル名を SJIS に変換と、キャッシュをさせないヘッダを出力しない
             $original_filename = mb_convert_encoding($original_filename, 'SJIS', 'UTF-8');
-        }
-        $original_filename = str_replace(array("\r", "\n"), '', $original_filename);
+            $original_filename = str_replace(array("\r", "\n"), '', $original_filename);
 
-        send_nocache_headers(true);
-        header('Content-Disposition: attachment; filename="' . $original_filename . '"');
-        header('Content-Length: '. strlen($file['bin']));
-        header('Content-Type: application/octet-stream');
-        if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
-            // IE の場合のみ、ヘッダを追加
-        header('Cache-Control: public');
-        header('Pragma: public');
+            header('Content-Disposition: attachment; filename="' . $original_filename . '"');
+            header('Content-Length: '. strlen($file['bin']));
+            header('Content-Type: application/octet-stream');
+        } else {
+            $original_filename = str_replace(array("\r", "\n"), '', $original_filename);
+
+            send_nocache_headers(true);
+            header('Content-Disposition: attachment; filename="' . $original_filename . '"');
+            header('Content-Length: '. strlen($file['bin']));
+            header('Content-Type: application/octet-stream');
         }
         echo $file['bin'];
         exit;
