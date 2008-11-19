109a4cfca45a62fc4b2e9aedc6d9c25e4641d7ac
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-19 07:22:11 +0000

    #2032 revert r9139 r9068 r9064 r9063 r9062
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@9256 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/util/util.php
+++ webapp/lib/util/util.php
@@ -913,8 +913,6 @@ function util_get_c_member_config_default()
         'IS_DISPLAY_BOOKMARK_DIARY_HOME' => 1,
         'IS_DISPLAY_BOOKMARK_BLOG_HOME' => 1,
         'IS_DISPLAY_SCHEDULE_HOME' => 1,
-        'SEND_RANK_UP_MAIL_PC' => 1,
-        'SEND_RANK_UP_MAIL_KTAI' => 1,
     );
 
     return $default_config;
