5f5c83b431af171b150f95c644ab0e49be41e04b
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-20 17:15:44 +0000

    #3107 r9295～r9301の修正をtrunkに反映
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@9302 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/h_anataga_c_friend_confirm_list.php
+++ webapp/modules/ktai/page/h_anataga_c_friend_confirm_list.php
@@ -11,16 +11,9 @@ class ktai_page_h_anataga_c_friend_confirm_list extends OpenPNE_Action
         $u  = $GLOBALS['KTAI_C_MEMBER_ID'];
 
         // --- リクエスト変数
-        $target_c_member_id = $requests['target_c_member_id'];
         $direc = $requests['direc'];
         $page = $requests['page'];
         // ----------
-
-        if (!$target_c_member_id) $target_c_member_id = $u;
-
-        if (db_member_is_access_block($u, $target_c_member_id)) {
-            openpne_redirect('ktai', 'page_h_access_block');
-        }
 
         //ターゲット情報
         $this->set("target_c_member", db_member_c_member4c_member_id_LIGHT($u));
@@ -28,8 +21,8 @@ class ktai_page_h_anataga_c_friend_confirm_list extends OpenPNE_Action
         $page_size = 5;
         $page += $direc;
         //ターゲットの詳細なリスト
-        $list = db_friend_ktai_anataga_c_friend_confirm_list4c_member_id($u, $page_size, $page);
-        $total_num = db_count_c_anataga_friend_confirm($u);
+        $list = db_friend_ktai_anataga_c_friend_confirm_list4c_member_id2($u, $page_size, $page);
+        $total_num = db_friend_count_c_anataga_friend_confirm($u);
         $this->set("c_friend_confirm_list", $list[0]);
         $this->set("page", $page);
         $this->set("is_prev", $list[1]);
