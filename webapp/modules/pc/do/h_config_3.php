3608af088f08172ee5f4e1fd8283bbf478c100d6
akamine <akamine@8bbc783d-d51a-0410-adab-8fa69d43f207>
2008-11-27 04:41:39 +0000

    #2082 h_config内のUIを変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@9396 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_config_3.php
+++ webapp/modules/pc/do/h_config_3.php
@@ -106,6 +106,15 @@ class pc_do_h_config_3 extends OpenPNE_Action
 
         db_member_insert_c_access_block($u, $c_member_id_block);
 
+        // ランクアップメール通知
+        db_member_update_c_member_config($u, 'SEND_RANK_UP_MAIL_PC', $requests['SEND_RANK_UP_MAIL_PC']);
+
+        $default_config = util_get_c_member_config_default();
+        foreach ($requests as $key => $value) {
+            if (!is_null($value) && array_key_exists($key, $default_config)) {
+                db_member_update_c_member_config($u, $key, $value);
+            }
+        }
         openpne_redirect('pc', 'page_h_home');
     }
 }
