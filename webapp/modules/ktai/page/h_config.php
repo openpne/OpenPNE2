c1b1b401273e531399b9e4a96ada102288ef6d83
kiwa <kiwa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-12 08:23:26 +0000

    #3476:管理画面のメール送信設定に関わらず各メール受信設定画面へのリンクが表示されていた部分の修正を取り込み(trunk)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@10752 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/h_config.php
+++ webapp/modules/ktai/page/h_config.php
@@ -11,6 +11,10 @@ class ktai_page_h_config extends OpenPNE_Action
         $u  = $GLOBALS['KTAI_C_MEMBER_ID'];
 
         $this->set('is_unused_ashiato', util_is_unused_mail('m_ktai_ashiato'));
+        $this->set('is_unused_diary_comment', util_is_unused_mail('m_ktai_diary_comment_info'));
+        if (OPENPNE_USE_POINT_RANK) {
+            $this->set('is_unused_rank_up', util_is_unused_mail('m_ktai_rank_up'));
+        }
         return 'success';
     }
 }
