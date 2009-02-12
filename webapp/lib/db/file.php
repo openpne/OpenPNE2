d79158efefbd42ecd23a1576e4b61f1e18ce6037
kiwa <kiwa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-12 07:03:09 +0000

    #3239:db_file_c_file_listでメモリを開放するように変更(trunk)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@10748 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/db/file.php
+++ webapp/lib/db/file.php
@@ -39,6 +39,7 @@ function db_file_c_file_list($page, $page_size, &$pager)
         $c_file_list[$i] = db_get_row($sql, $params);
         $c_file_list[$i]['size'] = strlen($c_file_list[$i]['bin']);
     }
+    unset($c_file_list[$i]['bin']);
 
     $sql = 'SELECT COUNT(*) FROM c_file';
     $total_num = db_get_one($sql);
