523cd80f00900753bc89d8f9eccbf603335568ef
kawahara <kawahara@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-18 10:39:07 +0000

    #1163:「絞り込んだメンバーにメッセージ/Eメールを送る」のプロフィール項目の絞り込みに対応していなかったのを修正(2.12)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@10793 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/send_messages_search.php
+++ webapp/modules/admin/page/send_messages_search.php
@@ -9,24 +9,20 @@ class admin_page_send_messages_search extends OpenPNE_Action
 {
     function execute($requests)
     {
-        $cond_list = validate_cond($_REQUEST);
-
+        $cond_list = validate_cond($_REQUEST['cond']);
+        $profile_cond_list = validate_profile_cond($_REQUEST['profile']);
+        
         $profile_list = db_member_c_profile_list();
         $profile_value_list = array();
-
-        $special_keys = array('s_year', 'e_year',
-                              's_rank', 'e_rank',
-                              'last_login',
-                              'is_pc_address', 'is_ktai_address');
-        foreach ($cond_list as $key => $each_cond) {
-            if (in_array($key, $special_keys)) {
-                continue;
-            }
+        
+        foreach ($profile_cond_list as $key => $each_cond) {
             $c_profile_option = db_c_profile_option4c_profile_option_id($each_cond);
             $profile_value_list[$key]['value'] = $c_profile_option['value'];
             $profile_value_list[$key]['caption'] = $profile_list[$key]['caption'];
         }
+
         $v['cond_list'] = $cond_list;
+        $v['profile_cond_list'] = $profile_cond_list;
         $v['profile_value_list'] = $profile_value_list;
 
         //絞り込みのための最終ログイン時間
