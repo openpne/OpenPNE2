338630169403a34ea9bb7506c3a37159335408a8
kiwa <kiwa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-02-26 05:27:23 +0000

    #3655:不要な関数を削除
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@10875 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/h_com_find_all.php
+++ webapp/modules/pc/page/h_com_find_all.php
@@ -57,7 +57,7 @@ class pc_page_h_com_find_all extends OpenPNE_Action
         $this->set('c_commu_category_is_create_commu', db_commu_c_commu_category4is_create_commu());
 
         // 作成可コミュニティの有無
-        $c_commu_category4createflag_num = count(db_commu_c_commu_category4createflag());
+        $c_commu_category4createflag_num = count(db_commu_c_commu_category4is_create_commu());
         $is_created_commu = ( $c_commu_category4createflag_num > 0 ) ? true : false;
         $this->set('is_created_commu', $is_created_commu);
 
