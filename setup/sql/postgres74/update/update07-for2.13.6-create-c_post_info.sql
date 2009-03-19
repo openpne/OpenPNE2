2c8a26708701540d83b43825580ce7a6cb195364
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-19 11:59:00 +0000

    #3731:連続投稿のテーブル削除と使用テーブル変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11072 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/postgres74/update/update07-for2.13.6-create-c_post_info.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE c_post_info (
-  c_post_info_id serial NOT NULL,
-  c_member_id int4 NOT NULL default '0',
-  last_post_time int4 NOT NULL default '0',
-  last_post_info int4 NOT NULL default '0',
-  r_datetime timestamp NOT NULL default '0000-01-01 00:00:00',
-  PRIMARY KEY  (c_post_info_id)
-);
-CREATE INDEX c_post_info_c_member_id on c_post_info (c_member_id);
