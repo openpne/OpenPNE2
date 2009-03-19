a3eb31496d438d057977e791291e33d1a15ad0dc
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-19 10:19:40 +0000

    #3723:メンバーIDが存在しなかった場合にエラーメッセージを表示するようにし、遷移先画面を変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11070 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/delete_c_member_write_all.php
+++ webapp/modules/admin/do/delete_c_member_write_all.php
@@ -9,9 +9,13 @@ class admin_do_delete_c_member_write_all extends OpenPNE_Action
 {
     function execute($requests)
     {
+        if (!db_member_c_member4c_member_id_LIGHT($requests['target_c_member_id'])) {
+            admin_client_redirect('list_c_member', '指定されたメンバーは存在しません');
+        }
+
         db_common_delete_c_member_write_all($requests['target_c_member_id']);
 
-        admin_client_redirect('top', 'メンバーの書き込みを全て削除しました');
+        admin_client_redirect('list_c_member', 'メンバーの書き込みを全て削除しました');
     }
 }
 
