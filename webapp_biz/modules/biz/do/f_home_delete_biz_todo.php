6628cbc4d30161b0295b3264394b8ea22d3c3122
kiwa <kiwa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-03-19 07:39:25 +0000

    #3205:非公開の共有Todoをh系にのみ表示するようにした(2.12)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@11061 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp_biz/modules/biz/do/f_home_delete_biz_todo.php
+++ webapp_biz/modules/biz/do/f_home_delete_biz_todo.php
@@ -15,7 +15,7 @@ class biz_do_f_home_delete_biz_todo extends OpenPNE_Action
         $id = $requests['target_id'];
         // ----------
 
-        if (!biz_isPermissionTodo($u, $delid)) {
+        if (!biz_isPermissionTodo($u, $delid, $id)) {
             handle_kengen_error();
         }
         biz_deleteTodo($delid);
