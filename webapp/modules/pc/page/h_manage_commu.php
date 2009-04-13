8067b45c0b3994be5dde4d6ae0301df930874a8e
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-13 08:52:43 +0000

    #3653:クラス名が修正されていなかったのを修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11277 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/h_manage_commu.php
+++ webapp/modules/pc/page/h_manage_commu.php
@@ -4,7 +4,7 @@
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 
-class pc_page_h_manage_com extends OpenPNE_Action
+class pc_page_h_manage_commu extends OpenPNE_Action
 {
     function execute($requests)
     {
