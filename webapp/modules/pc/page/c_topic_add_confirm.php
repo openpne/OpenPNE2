cce32f9d7ad94490bbd459a472e906c602df9960
urabe <urabe@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-17 12:48:46 +0000

    #3773 をトランクに追加
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11351 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/c_topic_add_confirm.php
+++ webapp/modules/pc/page/c_topic_add_confirm.php
@@ -24,15 +24,23 @@ class pc_page_c_topic_add_confirm extends OpenPNE_Action
         // ----------
 
         //--- 権限チェック
-        if (!db_commu_is_c_commu_view4c_commu_idAc_member_id($c_commu_id, $u)) {
-            handle_kengen_error();
-        }
-
         $c_commu = db_commu_c_commu4c_commu_id2($c_commu_id);
-
-        //トピック作成権限チェック
-        if ($c_commu['topic_authority'] == 'admin_only' && !db_commu_is_c_commu_admin($c_commu_id, $u)) {
-            handle_kengen_error();
+        
+        switch ($c_commu['is_topic']) {
+            case 'public' :
+                //誰でも作成可能
+                break;
+            case 'member' :
+                if (!db_commu_is_c_commu_view4c_commu_idAc_member_id($c_commu_id, $u)) {
+                    handle_kengen_error();
+                }
+                break;
+            case 'admin_only' :
+                //トピック作成権限チェック
+                if (!db_commu_is_c_commu_admin($c_commu_id, $u)) {
+                    handle_kengen_error();
+                }
+                break;
         }
 
         //画像ファイル
