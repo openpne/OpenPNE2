aeab977c211337d413fa0108ea84868d85b906f9
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-21 07:57:08 +0000

    #3833 メッセージお知らせメールでメッセージへのリンクを表示するようにした
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11378 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/util/mail_send.php
+++ webapp/lib/util/mail_send.php
@@ -520,7 +520,7 @@ function do_common_send_ashiato_mail($c_member_to, $c_member_from)
 }
 
 //◆メッセージ受信メール
-function do_common_send_message_mail_send($c_member_id_to, $c_member_id_from, $subject = '', $body = '')
+function do_common_send_message_mail_send($c_member_id_to, $c_member_id_from, $c_message_id)
 {
     $c_member_to = db_member_c_member4c_member_id($c_member_id_to, true);
     $pc_address = $c_member_to['secure']['pc_address'];
@@ -529,8 +529,7 @@ function do_common_send_message_mail_send($c_member_id_to, $c_member_id_from, $s
     $params = array(
         "c_member_to"   => db_member_c_member4c_member_id($c_member_id_to),
         "c_member_from" => db_member_c_member4c_member_id($c_member_id_from),
-        'subject' => $subject,
-        'body' => $body,
+		"c_message_id" => $c_message_id,
     );
     return fetch_send_mail($pc_address, 'm_pc_message_zyushin', $params, $is_receive_mail);
 }
