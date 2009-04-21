1bfe074084e465ef5c8167df1656f5e3ccf0939f
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-21 14:11:21 +0000

    #3784 コードのスタイルを共通化と重複コードの削除(2.12.x)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@11397 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/h_message_send.php
+++ webapp/modules/ktai/page/h_message_send.php
@@ -11,7 +11,7 @@ class ktai_page_h_message_send extends OpenPNE_Action
         $u  = $GLOBALS['KTAI_C_MEMBER_ID'];
 
         //ランダム５０人のフレンド
-        $this->set("c_friend_list", db_friend_ktai_c_friend_list_random4c_member_id($u));
+        $this->set('c_friend_list', db_friend_ktai_c_friend_list_random4c_member_id($u));
 
         // --- リクエスト変数
         $target_c_member_id = $requests['target_c_member_id'];
@@ -23,7 +23,7 @@ class ktai_page_h_message_send extends OpenPNE_Action
         $form_val['subject'] = $subject;
         $form_val['body'] = $body;
         $form_val['target_c_member_id'] = $target_c_member_id;
-        $this->set("form_val", $form_val);
+        $this->set('form_val', $form_val);
 
         return 'success';
     }
