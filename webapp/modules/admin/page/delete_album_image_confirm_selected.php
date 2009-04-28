fd890453fd99dfa353580517fcba5de50b1546b7
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-28 09:15:08 +0000

    #2717 インデントがタブになっていたので、半角スペースに変換
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11519 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/delete_album_image_confirm_selected.php
+++ webapp/modules/admin/page/delete_album_image_confirm_selected.php
@@ -16,26 +16,26 @@ class admin_page_delete_album_image_confirm_selected extends OpenPNE_Action
 
         $target_c_album_image_ids = $requests['target_c_album_image_ids'];
 
-		// アルバム写真が選択されていない場合はエラー
-		if (!$target_c_album_image_ids) {
-        	admin_client_redirect('edit_album_image_list', "アルバム写真が選択されていません");
-		}
+        // アルバム写真が選択されていない場合はエラー
+        if (!$target_c_album_image_ids) {
+            admin_client_redirect('edit_album_image_list', "アルバム写真が選択されていません");
+        }
 
-		$id_ary = split(":", $target_c_album_image_ids);
+        $id_ary = split(":", $target_c_album_image_ids);
 
-		$album_image_list = array();
-		foreach ($id_ary as $id) {
-        	$album_image = db_album_image_get_c_album_image4id($id);
+        $album_image_list = array();
+        foreach ($id_ary as $id) {
+            $album_image = db_album_image_get_c_album_image4id($id);
 
-        	if (!$album_image) {
-        	    admin_client_redirect('edit_album_image_list', '指定されたアルバムは存在しません');
-        	}
+            if (!$album_image) {
+                admin_client_redirect('edit_album_image_list', '指定されたアルバムは存在しません');
+            }
 
-        	$member = db_member_c_member4c_member_id($album_image['c_member_id']);
-        	$album_image['c_member'] = $member;
+            $member = db_member_c_member4c_member_id($album_image['c_member_id']);
+            $album_image['c_member'] = $member;
 
-			array_push($album_image_list, $album_image);
-		}
+            array_push($album_image_list, $album_image);
+        }
         $this->set('album_image_list', $album_image_list);
         $this->set('target_c_album_image_ids', $target_c_album_image_ids);
 
