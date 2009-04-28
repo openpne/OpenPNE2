9f0a71b79aa41921b8186588fc0fbd95467a1c3e
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-28 09:15:08 +0000

    #2717 インデントがタブになっていたので、半角スペースに変換
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11519 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/do/delete_kakikomi_c_diary_selected.php
+++ webapp/modules/admin/do/delete_kakikomi_c_diary_selected.php
@@ -9,19 +9,19 @@ class admin_do_delete_kakikomi_c_diary_selected extends OpenPNE_Action
 {
     function execute($requests)
     {
-		$id_ary = split(":", $requests['target_c_diary_ids']);
+        $id_ary = split(":", $requests['target_c_diary_ids']);
 
-		foreach ($id_ary as $id) {
-        	$diary = db_diary_get_c_diary4id($id);
+        foreach ($id_ary as $id) {
+            $diary = db_diary_get_c_diary4id($id);
 
-        	if (!$diary) {
-        	    admin_client_redirect('diary_list', '指定された' . WORD_DIARY . 'は存在しません');
-        	}
-		}
+            if (!$diary) {
+                admin_client_redirect('diary_list', '指定された' . WORD_DIARY . 'は存在しません');
+            }
+        }
 
-		foreach ($id_ary as $id) {
-        	db_diary_delete_c_diary($id);
-		}
+        foreach ($id_ary as $id) {
+            db_diary_delete_c_diary($id);
+        }
 
         admin_client_redirect('diary_list', WORD_DIARY . 'を削除しました');
     }
