2e152cfa4e3c3770efe9fcd41cc165dae7ce6496
kiwa <kiwa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-28 05:31:28 +0000

    #3811:ハードタブをソフトタブに変更、インデントを調整
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11512 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/do/h_message_send_insert_message.php
+++ webapp/modules/ktai/do/h_message_send_insert_message.php
@@ -86,7 +86,7 @@ class ktai_do_h_message_send_insert_message extends OpenPNE_Action
                 }
             }
 
-	     $p = array('box' => 'outbox');
+            $p = array('box' => 'outbox');
         } else {
             if (!isset($target_c_message_id)) {
                 // 下書きメッセージ新規保存
