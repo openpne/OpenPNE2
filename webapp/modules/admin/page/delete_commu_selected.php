ec35edc7d5b0c270c81e462ffe08216ee050eb75
fukamachi <fukamachi@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-04-28 09:15:08 +0000

    #2717 インデントがタブになっていたので、半角スペースに変換
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11519 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/admin/page/delete_commu_selected.php
+++ webapp/modules/admin/page/delete_commu_selected.php
@@ -14,29 +14,29 @@ class admin_page_delete_commu_selected extends OpenPNE_Action
 
         $target_c_commu_ids = $requests['target_c_commu_ids'];
 
-		// コミュニティが選択されていない場合はエラー
-		if (!$target_c_commu_ids) {
-        	admin_client_redirect('commu_list', WORD_COMMUNITY . "が選択されていません");
-		}
+        // コミュニティが選択されていない場合はエラー
+        if (!$target_c_commu_ids) {
+            admin_client_redirect('commu_list', WORD_COMMUNITY . "が選択されていません");
+        }
 
-		$id_ary = split(":", $target_c_commu_ids);
+        $id_ary = split(":", $target_c_commu_ids);
 
         $v = array();
 
-		$commu_list = array();
-		foreach ($id_ary as $id) {
-        	$commu = db_commu_c_commu4c_commu_id($id);
+        $commu_list = array();
+        foreach ($id_ary as $id) {
+            $commu = db_commu_c_commu4c_commu_id($id);
 
-        	if (!$commu) {
-        	    admin_client_redirect('commu_list', '指定された' . WORD_COMMUNITY . 'は存在しません');
-        	}
+            if (!$commu) {
+                admin_client_redirect('commu_list', '指定された' . WORD_COMMUNITY . 'は存在しません');
+            }
 
-        	$commu['c_member'] = db_member_c_member4c_member_id($commu['c_member_id_admin']);
+            $commu['c_member'] = db_member_c_member4c_member_id($commu['c_member_id_admin']);
 
-			array_push($commu_list, $commu);
-		}
+            array_push($commu_list, $commu);
+        }
         $this->set('commu_list', $commu_list);
-		$this->set('target_c_commu_ids', $target_c_commu_ids);
+        $this->set('target_c_commu_ids', $target_c_commu_ids);
 
         $v['SNS_NAME'] = SNS_NAME;
         $v['OPENPNE_VERSION'] = OPENPNE_VERSION;
