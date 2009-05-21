c267335799890f1c8286c5635bc274fece26926e
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-05-21 13:39:40 +0000

    * 不要ファイルの削除  * 連番変更  * 記述を一部修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@11730 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ setup/sql/mysql41/update/update15-for2.14beta1-alter-c_member-add-column-is_search_result.sql
@@ -0,0 +1,1 @@
+ALTER TABLE c_member ADD COLUMN is_search_result TINYINT(1) NOT NULL default 1;
--- setup/sql/postgres74/update/update11-for2.13.7-alter-c_tmp_image-add-column-filesize.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE c_tmp_image ADD COLUMN filesize int4;
-ALTER TABLE c_tmp_image ALTER COLUMN filesize SET DEFAULT 0;
-ALTER TABLE c_tmp_image ALTER COLUMN filesize SET NOT NULL;
-
