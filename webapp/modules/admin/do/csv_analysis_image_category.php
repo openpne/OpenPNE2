218c45d15cd9b28bdbb28a82901146add0483af3
shintani <shintani@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-06-02 02:25:46 +0000

    #3900 統計情報のDL機能でカテゴリ別画像表示ページが未対応 2.14開発版にマージしました.
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.14.x@11806 8bbc783d-d51a-0410-adab-8fa69d43f207

---
+++ webapp/modules/admin/do/csv_analysis_image_category.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @copyright 2005-2008 OpenPNE Project
+ * @license   http://www.php.net/license/3_01.txt PHP License 3.01
+ */
+
+class admin_do_csv_analysis_image_category extends OpenPNE_Action
+{
+    function isSecure()
+    {
+        session_cache_limiter('public');
+        return true;
+    }
+
+    function handleError($errors)
+    {
+        admin_client_redirect('image_analysis_category', array_shift($errors));
+    }
+
+    function execute($requests)
+    {
+        $csv = '"カテゴリ","画像容量,平均容量(1人当り)"' . "\n";
+        $csv = mb_convert_encoding($csv, 'SJIS', 'UTF-8');
+
+        $analysis_image_category = get_analysis_image_category();
+        foreach ($analysis_image_category as $key => $value) {
+            $category = $analysis_image_category[$key]['category'];
+            $category = mb_convert_encoding($category, 'SJIS', 'UTF-8');
+            $sum = $analysis_image_category[$key]['sum'];
+            $avg = $analysis_image_category[$key]['avg'];
+            $csv .= sprintf("\"%s\",%01.2f,%01.2f\n", $category, $sum, $avg);
+        }
+        $analysis_image_category_sum = get_analysis_image_category_sum();
+        $category_sum = $analysis_image_category_sum['category'];
+        $category_sum = mb_convert_encoding($category_sum, 'SJIS', 'UTF-8');
+        $sum_all = $analysis_image_category_sum['sum'];
+        $avg_all = $analysis_image_category_sum['avg'];
+        $csv .= sprintf("\"%s\",%01.2f,%01.2f\n", $category_sum, $sum_all, $avg_all);
+
+        //IE以外の場合、キャッシュをさせないヘッダを出力
+        if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') === false) {
+            send_nocache_headers(true);
+        }
+        header("Content-Type: application/octet-stream");
+        header("Content-Disposition: attachment; filename=analysis_image_category.csv");
+        echo $csv;
+        exit;
+    }
+}
+
+?>
