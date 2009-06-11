ebd493cca2703b94c0e8848594967c0e236ed93f
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-06-11 02:16:22 +0000

    #3972:設定値を追加した
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.14.x@11899 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/OpenPNE/Config.php
+++ webapp/lib/OpenPNE/Config.php
@@ -250,7 +250,7 @@ class OpenPNE_Config
             'CHECK_IMG_AUTH' => false,
             'OPENPNE_IS_OPENID_SERVER' => false,
             'OPENPNE_DB_ERROR_LOG' => true,
-            'IS_PASSWORD_QUERY_ANSWER' => true,
+            'IS_PASSWORD_QUERY_ANSWER' => 2,
             'OPENPNE_POST_INTERVAL_UNFAIR_SECOND' => 10,
             'OPENPNE_POST_INTERVAL_UNFAIR_COUNT' => 10,
             'OPENPNE_POST_INTERVAL_UNFAIR_COUNT_RESET_SECOND' => 60*60,
