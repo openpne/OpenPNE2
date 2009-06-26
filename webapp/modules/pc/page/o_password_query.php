b95c4525966bda99130ea6c5d3cdc4d532c748ba
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-06-26 13:05:14 +0000

    #4033:秘密の質問を省略した場合の悪意のある操作を防止するための機能の追加(2.14.x)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.14.x@12039 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/o_password_query.php
+++ webapp/modules/pc/page/o_password_query.php
@@ -20,6 +20,7 @@ class pc_page_o_password_query extends OpenPNE_Action
         $this->set('inc_page_header', fetch_inc_page_header('public'));
 
         $this->set('c_password_query_list', p_common_c_password_query4null());
+        $this->set('is_send', $requests['is_send']);
 
         return 'success';
     }
