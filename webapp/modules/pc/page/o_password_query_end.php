a6ce2d94239d71a0528538f8d8d3d00a7863854c
nagasawa <nagasawa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-01 08:47:52 +0000

    #4033:r12049,r12073,r12074,r12075,r12076の変更をマージ(2.14.x)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.14.x@12079 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/o_password_query_end.php
+++ webapp/modules/pc/page/o_password_query_end.php
@@ -4,7 +4,7 @@
  * @license   http://www.php.net/license/3_01.txt PHP License 3.01
  */
 
-class pc_page_o_password_query extends OpenPNE_Action
+class pc_page_o_password_query_end extends OpenPNE_Action
 {
     function isSecure()
     {
@@ -18,8 +18,6 @@ class pc_page_o_password_query extends OpenPNE_Action
 
         //---- inc_ テンプレート用 変数 ----//
         $this->set('inc_page_header', fetch_inc_page_header('public'));
-
-        $this->set('c_password_query_list', p_common_c_password_query4null());
 
         return 'success';
     }
