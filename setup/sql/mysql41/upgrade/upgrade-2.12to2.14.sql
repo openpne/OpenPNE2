4307ad01eb0af933abd6e19d93abb2d9a8c96de6
urabe <urabe@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-07-07 04:00:36 +0000

    #4064 is_admit カラムの値を修正するSQLを作成 (stable2.14.x)
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.14.x@12154 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/sql/mysql41/upgrade/upgrade-2.12to2.14.sql
+++ setup/sql/mysql41/upgrade/upgrade-2.12to2.14.sql
@@ -59,9 +59,9 @@ ALTER TABLE c_commu ADD COLUMN is_topic enum('member','admin_only','public') NOT
 ALTER TABLE c_commu ADD COLUMN is_comment enum('member','public') NOT NULL default 'member';
 
 UPDATE c_commu SET is_admit='public',is_open='public',is_comment='member' WHERE public_flag='public';
-UPDATE c_commu SET is_admit='admit',is_open='public',is_comment='member' WHERE public_flag='auth_public';
-UPDATE c_commu SET is_admit='admit',is_open='public',is_comment='member' WHERE public_flag='auth_sns';
-UPDATE c_commu SET is_admit='admit',is_open='member',is_comment='member' WHERE public_flag='auth_commu_member';
+UPDATE c_commu SET is_admit='auth',is_open='public',is_comment='member' WHERE public_flag='auth_public';
+UPDATE c_commu SET is_admit='auth',is_open='public',is_comment='member' WHERE public_flag='auth_sns';
+UPDATE c_commu SET is_admit='auth',is_open='member',is_comment='member' WHERE public_flag='auth_commu_member';
 
 UPDATE c_commu SET is_topic='member' WHERE topic_authority='public';
 UPDATE c_commu SET is_topic='admin_only' WHERE topic_authority='admin_only';
