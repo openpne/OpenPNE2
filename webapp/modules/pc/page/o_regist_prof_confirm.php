f4945546dba2dd7c334da765bf45d20785687856
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-08 08:04:33 +0000

    #4236:SESSIONで値を引き回すように変更
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/trunk@12563 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/page/o_regist_prof_confirm.php
+++ webapp/modules/pc/page/o_regist_prof_confirm.php
@@ -30,15 +30,18 @@ class pc_page_o_regist_prof_confirm extends OpenPNE_Action
 
         $pre = db_member_c_member_pre4sid($sid);
 
+        session_name('OpenPNEpcregist');
+        @session_start();
+        $prof = $_SESSION['prof'];
+        $c_member_profile_list = db_member_check_profile($prof['profile'], $public_flag_list);
+        $prof['profile'] = $c_member_profile_list;
         $this->set('inc_page_header', fetch_inc_page_header('regist'));
 
         $this->set('sid', $sid);
-        session_name('OpenPNEpcregist');
-        @session_start();
-        $this->set('prof', $_REQUEST['prof']);
+        $this->set('prof', $prof);
         $this->set('pc_address', $pre['pc_address']);
         $query_list = p_common_c_password_query4null();
-        $query_id = $_REQUEST['c_password_query_id'];
+        $query_id = $prof['c_password_query_id'];
         $this->set('password_query_name', $query_list[$query_id]);
 
         $this->set('profile_list', db_member_c_profile_list4null());
