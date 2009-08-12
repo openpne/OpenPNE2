c4e34a1a70e65e1eafb1a3f1996f96e8e7945fb2
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-08-12 01:46:42 +0000

    #4175:コーディング規約にあわせた
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.14.x@12606 8bbc783d-d51a-0410-adab-8fa69d43f207

--- setup/script/update/update01-for2.13.8-insert_image_size.php
+++ setup/script/update/update01-for2.13.8-insert_image_size.php
@@ -1,10 +1,11 @@
 <?php
 require_once './config.inc.php';
 chdir(OPENPNE_PUBLIC_HTML_DIR);
-require_once(OPENPNE_WEBAPP_DIR . "/init.inc");
+require_once OPENPNE_WEBAPP_DIR . '/init.inc';
 
-//----------
-//- 登録済み画像のファイルサイズを求める
+/**
+ * 登録済み画像のファイルサイズを求める
+ */
 function get_image_size($category, $params, $handle)
 {
     // request
@@ -13,7 +14,7 @@ function get_image_size($category, $params, $handle)
     $c_member_id = $params[2];
 
     // c_image 参照先
-    $image_dsn = array('main','image');
+    $image_dsn = array('main', 'image');
     foreach ($image_dsn as $key => $dsn) {
         if (!isset($GLOBALS['_OPENPNE_DSN_LIST'][$dsn])) {
             continue;
@@ -21,27 +22,24 @@ function get_image_size($category, $params, $handle)
         $imagedb =& db_get_instance($dsn);
 
         // 取り出し
-        $sql  = "SELECT ";
-        if ($table <> "biz_shisetsu") {
-            $sql .= " $c_member_id as c_member_id, ";
+        $sql  = 'SELECT ';
+        if ($table <> 'biz_shisetsu') {
+            $sql .= $c_member_id . ' as c_member_id, ';
         }
-        $sql .= "$filename as image_filename ";
-        if ($table == "c_member") {
-            $sql .= ",r_date ";
-        } elseif ($table <> "biz_group" && $table <> "biz_shisetsu") {
-            $sql .= ",r_datetime ";
+        $sql .= $filename . ' as image_filename ';
+        if ($table == 'c_member') {
+            $sql .= ',r_date ';
+        } elseif ($table <> 'biz_group' && $table <> 'biz_shisetsu') {
+            $sql .= ',r_datetime ';
         }
-        $sql .= "FROM ";
-        $sql .= " $table ";
-        $sql .= "WHERE ";
-        $sql .= " $filename <> '' ";
-        $sql .= "AND ";
-        $sql .= " $filename is not NULL";
+        $sql .= 'FROM ' . $table;
+        $sql .= ' WHERE ' . $filename . ' <> ""';
+        $sql .= ' AND ' . $filename . ' is not NULL';
         $data_list = db_get_all($sql);
 
         $lines = 0;
         if ($data_list) {
-            fwrite($handle, "INSERT INTO c_image_size VALUES");
+            fwrite($handle, 'INSERT INTO c_image_size VALUES');
         }
         foreach($data_list as $data) {
             // get image size
@@ -53,28 +51,28 @@ function get_image_size($category, $params, $handle)
                 $filesize = strlen(base64_decode($c_image['bin']));
             }
 
-            $ins_data = "";
+            $ins_data = '';
             if ($lines) {
-                $ins_data .= ",";
+                $ins_data .= ',';
             }
-            $ins_data .= "(";
+            $ins_data .= '(';
             if ($GLOBALS['_OPENPNE_DSN_LIST']['main']['dsn']['phptype'] == 'pgsql') {
                 $ins_data .= " nextval('c_image_size_c_image_size_id_seq')";
             } else {
-                $ins_data .= " null";
+                $ins_data .= ' null';
             }
             $ins_data .= ",'" . $data['image_filename'] . "'";
             $ins_data .= ",'" . $data['c_member_id'] . "'";
             $ins_data .= ",'" . $filesize . "'";
             $ins_data .= ",'" . $category . "'";
-            if ($table == "biz_group" || $table == "biz_shisetsu") {
+            if ($table == 'biz_group' || $table == 'biz_shisetsu') {
                 $ins_data .= ",'" . db_now() . "'";
-            } elseif ($table == "c_member") {
+            } elseif ($table == 'c_member') {
                 $ins_data .= ",'" . $data['r_date'] . "'";
             } else {
                 $ins_data .= ",'" . $data['r_datetime'] . "'";
             }
-            $ins_data .= ")";
+            $ins_data .= ')';
             fwrite($handle, $ins_data);
             $lines ++;
         }
@@ -85,12 +83,13 @@ function get_image_size($category, $params, $handle)
     
 }
 
-//----------
-//- テンポラリファイルを作成
+/**
+ * テンポラリファイルを作成
+ */
 function open_temp_file($mode, $filename = '')
 {
     if (!$filename) {
-        $w_filename = tempnam(OPENPNE_VAR_DIR . "/tmp", "image_size");
+        $w_filename = tempnam(OPENPNE_VAR_DIR . '/tmp', 'image_size');
     } else {
         $w_filename = $filename;
     }
@@ -99,11 +98,12 @@ function open_temp_file($mode, $filename = '')
     return array($w_filename, $handle);
 }
 
-//----------
-//- load data infile
+/**
+ * load data infile
+ */
 function load_data_infile($filename)
 {
-    list($r_filename, $r_handle) = open_temp_file("r", $filename);
+    list($r_filename, $r_handle) = open_temp_file('r', $filename);
     while (!feof($r_handle)) {
         $sql = fgets($r_handle);
         if ($sql) db_query($sql);
@@ -111,8 +111,9 @@ function load_data_infile($filename)
     close_temp_file($filename, $r_handle);
 }
 
-//----------
-//- テンポラリを閉じる
+/**
+ * テンポラリを閉じる
+ */
 function close_temp_file($filename, $handle)
 {
     fclose($handle);
@@ -120,46 +121,44 @@ function close_temp_file($filename, $handle)
 }
 
 
-//----------
-//- 処理本体
-//----------
-//  biz_shisetsuは、member_idを持っていないため、設定不可
-
-// array([category] => array([table],[filename field],[c_member_id field]))
-
+/**
+ * 処理本体
+ * biz_shisetsuは、member_idを持っていないため、設定不可
+ * array([category] => array([table],[filename field],[c_member_id field])
+ */
 $category_list = array(
-          'album' => array(
-                         array('c_album_image', 'image_filename', 'c_member_id')
-                          ),
-          'commu' => array(
-                         array('c_commu_topic_comment', 'image_filename1', 'c_member_id')
-                        ,array('c_commu_topic_comment', 'image_filename2', 'c_member_id')
-                        ,array('c_commu_topic_comment', 'image_filename3', 'c_member_id')
-                          ),
-          'diary' => array(
-                         array('c_diary', 'image_filename_1', 'c_member_id')
-                        ,array('c_diary', 'image_filename_2', 'c_member_id')
-                        ,array('c_diary', 'image_filename_3', 'c_member_id')
-                        ,array('c_diary_comment', 'image_filename_1', 'c_member_id')
-                        ,array('c_diary_comment', 'image_filename_2', 'c_member_id')
-                        ,array('c_diary_comment', 'image_filename_3', 'c_member_id')
-                        ,array('c_album', 'album_cover_image', 'c_member_id')
-                          ),
-           'other' => array(
-                         array('c_commu', 'image_filename', 'c_member_id_admin')
-                        ,array('biz_group', 'image_filename', 'admin_id')
-                        ,array('biz_shisetsu', 'image_filename', 'c_member_id')
-                        ,array('c_member', 'image_filename_1', 'c_member_id')
-                        ,array('c_member', 'image_filename_2', 'c_member_id')
-                        ,array('c_member', 'image_filename_3', 'c_member_id')
-                        ,array('c_message', 'image_filename_1', 'c_member_id_from')
-                        ,array('c_message', 'image_filename_2', 'c_member_id_from')
-                        ,array('c_message', 'image_filename_3', 'c_member_id_from')
-                          )
-                       );
+    'album' => array(
+        array('c_album_image', 'image_filename', 'c_member_id')
+    ),
+    'commu' => array(
+        array('c_commu_topic_comment', 'image_filename1', 'c_member_id'),
+        array('c_commu_topic_comment', 'image_filename2', 'c_member_id'),
+        array('c_commu_topic_comment', 'image_filename3', 'c_member_id'),
+    ),
+    'diary' => array(
+        array('c_diary', 'image_filename_1', 'c_member_id'),
+        array('c_diary', 'image_filename_2', 'c_member_id'),
+        array('c_diary', 'image_filename_3', 'c_member_id'),
+        array('c_diary_comment', 'image_filename_1', 'c_member_id'),
+        array('c_diary_comment', 'image_filename_2', 'c_member_id'),
+        array('c_diary_comment', 'image_filename_3', 'c_member_id'),
+        array('c_album', 'album_cover_image', 'c_member_id'),
+    ),
+     'other' => array(
+        array('c_commu', 'image_filename', 'c_member_id_admin'),
+        array('biz_group', 'image_filename', 'admin_id'),
+        array('biz_shisetsu', 'image_filename', 'c_member_id'),
+        array('c_member', 'image_filename_1', 'c_member_id'),
+        array('c_member', 'image_filename_2', 'c_member_id'),
+        array('c_member', 'image_filename_3', 'c_member_id'),
+        array('c_message', 'image_filename_1', 'c_member_id_from'),
+        array('c_message', 'image_filename_2', 'c_member_id_from'),
+        array('c_message', 'image_filename_3', 'c_member_id_from'),
+    ),
+);
 
 // Insert格納用テンポラリファイル作成
-list($filename, $handle) = open_temp_file("w");
+list($filename, $handle) = open_temp_file('w');
 
 //テーブル分ループ
 foreach ($category_list as $category => $table_list) {
@@ -176,9 +175,6 @@ close_temp_file($filename, $handle);
 
 // Insert実行
 load_data_infile($filename);
-
-// テンポラリファイル削除
-//unlink($filename);
 
 echo 'Complete!';
 ?>
