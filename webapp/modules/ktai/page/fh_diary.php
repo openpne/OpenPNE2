531003fe0615f0affa97b201888fbf9630a83464
imamura623 <imamura623@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-10-15 06:37:06 +0000

    #4084:$WORD_DIARY_HALFの追加と権限チェックの修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@12998 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/fh_diary.php
+++ webapp/modules/ktai/page/fh_diary.php
@@ -23,7 +23,7 @@ class ktai_page_fh_diary extends OpenPNE_Action
         if ($u != $target_c_member_id) {
             // check public_flag
             if (!pne_check_diary_public_flag($target_c_diary_id, $u)) {
-                ktai_display_error('この' . WORD_DIARY_HALF . 'にはアクセスできません');
+                openpne_redirect('ktai', 'page_h_err_diary_access');
             }
             //アクセスブロック設定
             if (db_member_is_access_block($u, $target_c_member_id)) {
