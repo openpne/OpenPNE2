aab86a5edfcdb473df9dffdb5b558bfbced1c507
shingo <shingo@8bbc783d-d51a-0410-adab-8fa69d43f207>
2009-10-28 07:12:58 +0000

    #4355:r13053の変更をマージ
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@13054 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/pc/do/h_diary_add_insert_c_diary.php
+++ webapp/modules/pc/do/h_diary_add_insert_c_diary.php
@@ -29,7 +29,7 @@ class pc_do_h_diary_add_insert_c_diary extends OpenPNE_Action
         $tmpfile_1 = $requests['tmpfile_1'];
         $tmpfile_2 = $requests['tmpfile_2'];
         $tmpfile_3 = $requests['tmpfile_3'];
-        $category = preg_split('/\s+/', $requests['category']);
+        $category = array_unique(preg_split('/\s+/', $requests['category']));
         // ----------
 
         $sessid = session_id();
