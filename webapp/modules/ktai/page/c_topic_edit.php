d7e4d84570e92c46653f3368ecaff50c95286ba6
imamura623 <imamura623@8bbc783d-d51a-0410-adab-8fa69d43f207>
2010-05-17 10:43:01 +0000

    #4357:イベント及びトピックの編集権限修正
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.14.x@13518 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/modules/ktai/page/c_topic_edit.php
+++ webapp/modules/ktai/page/c_topic_edit.php
@@ -29,6 +29,15 @@ class ktai_page_c_topic_edit extends OpenPNE_Action
             !db_commu_is_c_commu_admin($c_commu_id, $u)) {
             handle_kengen_error();
         }
+        $c_commu = db_commu_c_commu4c_commu_id2($c_commu_id);
+        if ($c_commu['is_topic'] == 'admin_only' &&
+            !db_commu_is_c_commu_admin($c_commu_id, $u)) {
+            handle_kengen_error();
+        }
+        if ($c_commu['is_topic'] == 'member' &&
+            !db_commu_is_c_commu_member($c_commu_id, $u)) {
+            handle_kengen_error();
+        }
         //---
 
         if ($c_topic['event_flag']) {
