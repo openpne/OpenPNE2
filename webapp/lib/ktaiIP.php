e0fd8fcd94fb9e5d1f2de7f12b96ec4dfc172c75
kiwa <kiwa@8bbc783d-d51a-0410-adab-8fa69d43f207>
2010-08-05 08:17:15 +0000

    #4449 携帯電話IPアドレス帯域リストを最新にする
    git-svn-id: https://trac.openpne.jp/svn/OpenPNE/branches/stable-2.12.x@13643 8bbc783d-d51a-0410-adab-8fa69d43f207

--- webapp/lib/ktaiIP.php
+++ webapp/lib/ktaiIP.php
@@ -4,15 +4,18 @@
 // 'IP Adress/BitMask' の形式で入力してください
 $GLOBALS['_OPENPNE_KTAI_IP_LIST'] = array(
 
-// DoCoMo (2008/09/17更新)
+// DoCoMo (2010/06/24更新)
 // http://www.nttdocomo.co.jp/service/imode/make/content/ip/index.html
 '210.153.84.0/24',
 '210.136.161.0/24',
 '210.153.86.0/24',
 '124.146.174.0/24',
 '124.146.175.0/24',
+'202.229.176.0/24',
+'202.229.177.0/24',
+'202.229.178.0/24',
 
-// au (2010/03/10更新)
+// au (2010/06/24更新)
 // http://www.au.kddi.com/ezfactory/tec/spec/ezsava_ip.html
 '210.230.128.224/28',
 '121.111.227.160/27',
@@ -39,6 +42,7 @@ $GLOBALS['_OPENPNE_KTAI_IP_LIST'] = array(
 '111.86.141.64/26',
 '111.86.141.128/26',
 '111.86.141.192/26',
+'118.159.133.192/26',
 
 // SoftBank (2010/4/23更新)
 // http://creation.mb.softbank.jp/web/web_ip.html
